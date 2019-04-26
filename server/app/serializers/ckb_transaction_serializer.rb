eeb458651a0b468b6b54f4c175db070365fcad67
shaojunda <shaojunda@gmail.com>
2019-04-26 15:58:58 +0800

    chore(server): use shannon on transaction fee

--- server/app/serializers/ckb_transaction_serializer.rb
+++ server/app/serializers/ckb_transaction_serializer.rb
@@ -1,9 +1,13 @@
 class CkbTransactionSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :block_number, :block_timestamp, :transaction_fee, :version, :display_inputs, :display_outputs
+  attributes :block_number, :block_timestamp, :version, :display_inputs, :display_outputs
 
   attribute :transaction_hash do |object|
     object.tx_hash
+  end
+
+  attribute :transaction_fee do |object|
+    Shannon.new(object.transaction_fee).to_ckb
   end
 end
