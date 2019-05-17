46fe65562ab8fede3e4dee420fad6624368e9fb5
shaojunda <shaojunda@gmail.com>
2019-05-17 21:28:42 +0800

    chore(server): remove node switch

--- server/lib/ckb_inauthentic_sync.rb
+++ server/lib/ckb_inauthentic_sync.rb
@@ -3,7 +3,7 @@ require_relative "../config/environment"
 Rails.cache.delete("current_inauthentic_sync_round")
 
 loop do
-  return if Sidekiq::Queue.new("inauthentic_sync").size > 1000
+  return if Sidekiq::Queue.new("inauthentic_sync").size > 4000
 
   CkbSync::InauthenticSync.sync_node_data
 
