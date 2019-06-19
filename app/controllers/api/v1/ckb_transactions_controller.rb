94fb1cdda75a1eb1148183b6126c74034251ae9d
shaojunda <shaojunda@gmail.com>
2019-06-19 18:05:00 +0800

    perf: cache ckb transaction

--- app/controllers/api/v1/ckb_transactions_controller.rb
+++ app/controllers/api/v1/ckb_transactions_controller.rb
@@ -4,11 +4,11 @@ module Api
       before_action :validate_query_params, only: :show
 
       def show
-        ckb_transaction = CkbTransaction.where(tx_hash: params[:id]).available.take!
+        ckb_transaction = CkbTransaction.cached_find(params[:id])
+
+        raise Api::V1::Exceptions::CkbTransactionNotFoundError if ckb_transaction.blank?
 
         render json: CkbTransactionSerializer.new(ckb_transaction)
-      rescue ActiveRecord::RecordNotFound
-        raise Api::V1::Exceptions::CkbTransactionNotFoundError
       end
 
       private
