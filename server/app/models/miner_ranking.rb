782b6a0b3ee11b622dec7fa802552280e3e47bf6
shaojunda <shaojunda@gmail.com>
2019-06-21 09:14:53 +0800

    chore(server): only use the first output as miner

--- server/app/models/miner_ranking.rb
+++ server/app/models/miner_ranking.rb
@@ -15,8 +15,14 @@ class MinerRanking
   end
 
   def ranking_infos
-    ckb_transactions = CkbTransaction.available.where("block_timestamp >= ? and block_timestamp <= ?", STARTED_AT_TIMESTAMP, ENDED_AT_TIMESTAMP).where(is_cellbase: true)
-    address_ids = AccountBook.where(ckb_transaction: ckb_transactions).select("address_id").distinct
+    ckb_transactions = CkbTransaction.available.where("block_timestamp >= ? and block_timestamp <= ?", STARTED_AT_TIMESTAMP, ENDED_AT_TIMESTAMP).where(is_cellbase: true).where.not(block_number: 0)
+    only_one_cell_output_txs = CellOutput.where(ckb_transaction: ckb_transactions).group(:ckb_transaction_id).having("count(*) = 1").select("ckb_transaction_id")
+    address_ids = AccountBook.where(ckb_transaction: only_one_cell_output_txs).select(:address_id).distinct
+    more_than_one_cell_output_txs = CellOutput.where(ckb_transaction: ckb_transactions).group(:ckb_transaction_id).having("count(*) > 1").select("ckb_transaction_id")
+    if more_than_one_cell_output_txs.present?
+      address_ids = address_ids.pluck(:address_id)
+      address_ids += CkbTransaction.where(id: more_than_one_cell_output_txs).map { |ckb_transaction| ckb_transaction.cell_outputs.first.address_id }
+    end
     addresses = Address.where(id: address_ids)
     ranking_infos = []
     addresses.find_each do |address|
