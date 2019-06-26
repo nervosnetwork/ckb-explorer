d456377a8a1b91070cfe9511b80c9f1e0a305607
shaojunda <shaojunda@gmail.com>
2019-06-26 19:29:29 +0800

    feat: add new attributes to BlockSerializer
    reward_status received_tx_fee received_tx_fee_status

--- app/serializers/block_serializer.rb
+++ app/serializers/block_serializer.rb
@@ -1,7 +1,10 @@
 class BlockSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :block_hash, :number, :proposals_count, :uncles_count, :uncle_block_hashes, :reward, :total_transaction_fee, :cell_consumed, :total_cell_capacity, :miner_hash, :timestamp, :difficulty, :version, :epoch, :start_number, :length, :transactions_root, :witnesses_root
+  attributes :block_hash, :number, :proposals_count, :uncles_count, :uncle_block_hashes, :reward,
+             :total_transaction_fee, :cell_consumed, :total_cell_capacity, :miner_hash, :timestamp,
+             :difficulty, :version, :epoch, :start_number, :length, :transactions_root, :witnesses_root, :reward_status,
+             :received_tx_fee, :received_tx_fee_status
 
   attribute :nonce do |object|
     object.seal["nonce"]
