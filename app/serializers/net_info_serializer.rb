322fe55bd0310067b35a9f3891f8cb68c41c217a
shaojunda <shaojunda@gmail.com>
2019-07-16 13:29:36 +0800

    feat: add net info

---
+++ app/serializers/net_info_serializer.rb
@@ -0,0 +1,27 @@
+class NetInfoSerializer
+  include FastJsonapi::ObjectSerializer
+
+  attribute :addresses, if: Proc.new { |_record, params|
+    params && params[:info_name] == "addresses"
+  }
+
+  attribute :is_outbound, if: Proc.new { |_record, params|
+    params && params[:info_name] == "is_outbound"
+  }
+
+  attribute :node_id, if: Proc.new { |_record, params|
+    params && params[:info_name] == "node_id"
+  }
+
+  attribute :version, if: Proc.new { |_record, params|
+    params && params[:info_name] == "version"
+  }
+
+  attribute :local_node_info, if: Proc.new { |_record, params|
+    params && params[:info_name] == "local_node_info"
+  }
+
+  attribute :blockchain_info, if: Proc.new { |_record, params|
+    params && params[:info_name] == "blockchain_info"
+  }
+end
