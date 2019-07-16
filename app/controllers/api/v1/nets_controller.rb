af0ffb3ca2f572fb121fc02091f172fab573c0ec
shaojunda <shaojunda@gmail.com>
2019-07-16 13:29:50 +0800

    feat: implement index

--- app/controllers/api/v1/nets_controller.rb
+++ app/controllers/api/v1/nets_controller.rb
@@ -2,7 +2,8 @@ module Api
   module V1
     class NetsController < ApplicationController
       def index
-        render json: {}
+        net_info = NetInfo.new
+        render json: NetInfoSerializer.new(net_info, params: { info_name: "local_node_info" })
       end
 
       def show
