fd23d7a41560a7c1f1b152e90866d1bb90c7d87e
shaojunda <shaojunda@gmail.com>
2019-07-17 17:20:31 +0800

    chore: use cache

--- app/models/statistic_info_chart.rb
+++ app/models/statistic_info_chart.rb
@@ -1,36 +1,76 @@
 class StatisticInfoChart
-  def initialize
-    @max_block_number = Block.maximum(:number)
-    @last_epoch0_block_number = Block.available.where(epoch: 0).recent.first.number
-  end
-
   def id
     Time.current.to_i
   end
 
   def hash_rate
-    result =
-      (@last_epoch0_block_number + 1).step(@max_block_number, 100).map do |number|
-        blocks = Block.where("number <= ?", number).available.recent.includes(:uncle_blocks).limit(100)
-        next if blocks.blank?
-
-        total_difficulties = blocks.flat_map { |block| [block, *block.uncle_blocks] }.reduce(0) { |sum, block| sum + block.difficulty.hex }
-        total_time = blocks.first.timestamp - blocks.last.timestamp
-        hash_rate = total_difficulties.to_d / total_time / cycle_rate
-        { block_number: number, hash_rate: hash_rate }
-      end
-
-    result.compact
+    to = Rails.cache.read("hash_rate_to")
+    Rails.cache.fetch("hash_rate_chart_data_#{to}")
   end
 
   def difficulty
-    block_numbers = (@last_epoch0_block_number + 1).step(@max_block_number, 100).to_a
+    max_block_number = Block.available.maximum(:number)
+    last_epoch0_block_number = Block.available.where(epoch: 0).recent.first.number
+    block_numbers = (last_epoch0_block_number + 1).step(max_block_number, 100).to_a
     Block.where(number: block_numbers).available.order(:number).select(:number, :difficulty).map do |block|
       { block_number: block.number, difficulty: block.difficulty.hex }
     end
   end
 
+  def calculate_hash_rate
+    max_block_number = Block.available.maximum(:number)
+    last_epoch0_block_number = Block.available.where(epoch: 0).recent.first.number
+    from = Rails.cache.fetch("hash_rate_from") { last_epoch0_block_number }
+    to = Rails.cache.fetch("hash_rate_to") { max_block_number }
+    if from == to
+      Rails.cache.read("hash_rate_chart_data_#{to}")
+    else
+      prev_cached_data = []
+      if Rails.cache.read("hash_rate_chart_data_#{to}").present?
+        from = to
+        prev_cached_data = Rails.cache.read("hash_rate_chart_data_#{to}")
+      end
+      unless hash_data_valid?(prev_cached_data.last(11))
+        from = last_epoch0_block_number
+        Rails.cache.delete_matched("hash_rate_chart_data_*")
+      end
+      to = max_block_number
+      result =
+        (from + 1).step(to, 100).map do |number|
+          blocks = Block.where("number <= ?", number).available.recent.includes(:uncle_blocks).limit(100)
+          next if blocks.blank?
+
+          hash_rate_for_block(blocks, number)
+        end
+
+      Rails.cache.write("hash_rate_from", from)
+      Rails.cache.write("hash_rate_to", to)
+      Rails.cache.write("hash_rate_chart_data_#{to}", prev_cached_data.concat(result.compact))
+    end
+  end
+
   private
+
+  def hash_rate_for_block(blocks, number)
+    total_difficulties = blocks.flat_map { |block| [block, *block.uncle_blocks] }.reduce(0) { |sum, block| sum + block.difficulty.hex }
+    total_time = blocks.first.timestamp - blocks.last.timestamp
+    hash_rate = total_difficulties.to_d / total_time / cycle_rate
+    { block_number: number, hash_rate: hash_rate }
+  end
+
+  def hash_data_valid?(last11_hash_rate)
+    return true if last11_hash_rate.blank?
+
+    from = last11_hash_rate.first.number
+    to = last11_hash_rate.last.number
+    result =
+      (from).step(to, 100).map do |number|
+        blocks = Block.where("number <= ?", number).available.recent.includes(:uncle_blocks).limit(100)
+        hash_rate_for_block(blocks, number)
+      end
+
+    last11_hash_rate == result
+  end
 
   def n_l(n, l)
     ((n - l + 1)..n).reduce(1, :*)
