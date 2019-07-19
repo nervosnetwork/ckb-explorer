bf2946646745bc7b454781090c68fa5202c01da2
shaojunda <shaojunda@gmail.com>
2019-07-19 17:09:36 +0800

    chore: add more point to difficulty

--- app/models/statistic_info_chart.rb
+++ app/models/statistic_info_chart.rb
@@ -10,9 +10,14 @@ class StatisticInfoChart
 
   def difficulty
     current_epoch_number = CkbSync::Api.instance.get_current_epoch.number
+
     Rails.cache.fetch("statistic_info_difficulty_#{current_epoch_number}", expires_in: 10.minutes, race_condition_ttl: 10.seconds) do
+      from = Block.available.where(epoch: 0).recent.first.number.to_i
+      to = Block.available.maximum(:number).to_i
+      hash_rate_block_numbers = (from + 1).step(to, 100).to_a
+      hash_rate_blocks = Block.available.where(number: hash_rate_block_numbers).order(:timestamp)
       blocks = Block.available.order(:epoch, :timestamp).select("distinct on (epoch) *")
-      blocks.map do |block|
+      (blocks + hash_rate_blocks).uniq.map do |block|
         { epoch_number: block.epoch.to_i, block_number: block.number.to_i, difficulty: block.difficulty.hex }
       end
     end
@@ -23,29 +28,25 @@ class StatisticInfoChart
     last_epoch0_block_number = Block.available.where(epoch: 0).recent.first.number.to_i
     from = Rails.cache.fetch("hash_rate_from") { last_epoch0_block_number }
     to = Rails.cache.fetch("hash_rate_to") { max_block_number }
-    if from == to
-      Rails.cache.read("hash_rate_chart_data_#{to}")
-    else
-      prev_cached_data = []
-      if Rails.cache.read("hash_rate_chart_data_#{to}").present?
-        from = to
-        prev_cached_data = Rails.cache.read("hash_rate_chart_data_#{to}")
+    prev_cached_data = []
+    if Rails.cache.read("hash_rate_chart_data_#{to}").present?
+      from = to
+      prev_cached_data = Rails.cache.read("hash_rate_chart_data_#{to}")
+    end
+
+    to = max_block_number
+    epoch_first_block_numbers = Block.available.order(:epoch, :timestamp).select("distinct on (epoch) number").to_a.pluck(:number)
+    result =
+      (from + 1).step(to, 100).to_a.concat(epoch_first_block_numbers).uniq.sort.map do |number|
+        blocks = Block.where("number <= ?", number).available.recent.includes(:uncle_blocks).limit(100)
+        next if blocks.blank?
+
+        hash_rate_for_block(blocks, number)
       end
 
-      to = max_block_number
-      epoch_first_block_numbers = Block.available.order(:epoch, :timestamp).select("distinct on (epoch) number").to_a.pluck(:number)
-      result =
-        (from + 1).step(to, 100).to_a.concat(epoch_first_block_numbers).uniq.sort.map do |number|
-          blocks = Block.where("number <= ?", number).available.recent.includes(:uncle_blocks).limit(100)
-          next if blocks.blank?
-
-          hash_rate_for_block(blocks, number)
-        end
-
-      Rails.cache.write("hash_rate_from", from)
-      Rails.cache.write("hash_rate_to", to)
-      Rails.cache.write("hash_rate_chart_data_#{to}", prev_cached_data.concat(result.compact))
-    end
+    Rails.cache.write("hash_rate_from", from)
+    Rails.cache.write("hash_rate_to", to)
+    Rails.cache.write("hash_rate_chart_data_#{to}", prev_cached_data.concat(result.compact).uniq)
   end
 
   private
@@ -54,7 +55,7 @@ class StatisticInfoChart
     total_difficulties = blocks.flat_map { |block| [block, *block.uncle_blocks] }.reduce(0) { |sum, block| sum + block.difficulty.hex }
     total_time = blocks.first.timestamp - blocks.last.timestamp
     hash_rate = total_difficulties.to_d / total_time / cycle_rate
-    { block_number: number.to_i, hash_rate: hash_rate }
+    { block_number: number.to_i, hash_rate: hash_rate.infinite? ? 0 : hash_rate }
   end
 
   def n_l(n, l)
