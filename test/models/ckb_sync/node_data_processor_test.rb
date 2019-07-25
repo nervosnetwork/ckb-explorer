cf623f65a98c78fdc9b8027183d1bd354079ae67
shaojunda <shaojunda@gmail.com>
2019-07-25 15:07:47 +0800

    test: node data processor should create uncle blocks

--- test/models/ckb_sync/node_data_processor_test.rb
+++ test/models/ckb_sync/node_data_processor_test.rb
@@ -105,6 +105,57 @@ module CkbSync
       end
     end
 
+    test "#call should create uncle_blocks" do
+      VCR.use_cassette("blocks/10") do
+        node_block = CkbSync::Api.instance.get_block(DEFAULT_NODE_BLOCK_HASH)
+        node_block_uncle_blocks = node_block.uncles
+
+        assert_difference -> { UncleBlock.count }, node_block_uncle_blocks.size do
+          node_data_processor.call(node_block)
+        end
+      end
+    end
+
+    test "#call created uncle_block's attribute value should equal with the node uncle_block's attribute value" do
+      VCR.use_cassette("blocks/10") do
+        node_block = CkbSync::Api.instance.get_block(DEFAULT_NODE_BLOCK_HASH)
+        node_uncle_blocks = node_block.uncles.map { |uncle| uncle.to_h.deep_stringify_keys }
+        formatted_node_uncle_blocks = node_uncle_blocks.map { |uncle_block| format_node_block(uncle_block).sort }
+
+        local_block = node_data_processor.call(node_block)
+        local_uncle_blocks =
+          local_block.uncle_blocks.map do |uncle_block|
+            uncle_block =
+              uncle_block.attributes.select do |attribute|
+                attribute.in?(%w(difficulty block_hash number parent_hash seal timestamp transactions_root proposals_hash uncles_count uncles_hash version proposals witnesses_root epoch dao))
+              end
+            uncle_block["hash"] = uncle_block.delete("block_hash")
+            uncle_block["epoch"] = uncle_block["epoch"].to_s
+            uncle_block["number"] = uncle_block["number"].to_s
+            uncle_block["timestamp"] = uncle_block["timestamp"].to_s
+            uncle_block["version"] = uncle_block["version"].to_s
+            uncle_block["uncles_count"] = uncle_block["uncles_count"].to_s
+            uncle_block.sort
+          end
+
+        assert_equal formatted_node_uncle_blocks.sort, local_uncle_blocks.sort
+      end
+    end
+
+    test "#call created unlce_block's proposals_count should equal with the node uncle_block's proposals size" do
+      VCR.use_cassette("blocks/10") do
+        node_block = CkbSync::Api.instance.get_block(DEFAULT_NODE_BLOCK_HASH)
+        node_uncle_blocks = node_block.uncles
+        node_uncle_blocks_count = node_uncle_blocks.reduce(0) { |memo, uncle_block| memo + uncle_block.proposals.size }
+
+        local_block = node_data_processor.call(node_block)
+        local_uncle_blocks = local_block.uncle_blocks
+        local_uncle_blocks_count = local_uncle_blocks.reduce(0) { |memo, uncle_block| memo + uncle_block.proposals_count }
+
+        assert_equal node_uncle_blocks_count, local_uncle_blocks_count
+      end
+    end
+
     private
 
     def node_data_processor
