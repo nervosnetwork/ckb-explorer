72edc8340e24d1bbc40856caf820914abddbda64
shaojunda <shaojunda@gmail.com>
2019-08-15 14:11:04 +0800

    feat: block transaction api should return up to ten display info

--- app/serializers/ckb_transaction_serializer.rb
+++ app/serializers/ckb_transaction_serializer.rb
@@ -4,4 +4,12 @@ class CkbTransactionSerializer
   attributes :block_number, :block_timestamp, :transaction_fee, :version, :display_inputs, :display_outputs, :is_cellbase
 
   attribute :transaction_hash, &:tx_hash
+
+  attribute :display_inputs do |object, params|
+    params && params[:previews] ? object.display_inputs(previews: true) : object.display_inputs
+  end
+
+  attribute :display_outputs do |object, params|
+    params && params[:previews] ? object.display_outputs(previews: true) : object.display_outputs
+  end
 end
