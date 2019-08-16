98cf07de2ee65328da06e3ecf19ab528069e82aa
shaojunda <shaojunda@gmail.com>
2019-08-16 17:28:20 +0800

    chore: remove status from blocks

--- db/schema.rb
+++ db/schema.rb
@@ -54,7 +54,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_090938) do
     t.integer "proposals_count"
     t.decimal "cell_consumed", precision: 30
     t.binary "miner_hash"
-    t.integer "status", limit: 2
     t.decimal "reward", precision: 30
     t.decimal "total_transaction_fee", precision: 30
     t.decimal "ckb_transactions_count", precision: 30, default: "0"
