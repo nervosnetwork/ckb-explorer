80df01b41952dd54d7a609a5b3ea92a11ee53e5b
shaojunda <shaojunda@gmail.com>
2019-08-24 11:36:38 +0800

    feat: save nonce

--- app/serializers/block_serializer.rb
+++ app/serializers/block_serializer.rb
@@ -4,15 +4,8 @@ class BlockSerializer
   attributes :block_hash, :number, :proposals_count, :uncles_count, :uncle_block_hashes, :reward,
              :total_transaction_fee, :cell_consumed, :total_cell_capacity, :miner_hash, :timestamp,
              :difficulty, :version, :epoch, :start_number, :length, :transactions_root, :witnesses_root, :reward_status,
-             :received_tx_fee, :received_tx_fee_status
+             :received_tx_fee, :received_tx_fee_status, :nonce
 
-  attribute :nonce do |object|
-    object.seal["nonce"]
-  end
-
-  attribute :proof do |object|
-    object.seal["proof"]
-  end
 
   attribute :transactions_count, &:ckb_transactions_count
 end
