9e6943979d8cb523e2ef9a9a58dcd301ce4174ba
shaojunda <shaojunda@gmail.com>
2019-10-12 14:30:08 +0800

    test: should decrease address deposit

--- test/models/ckb_sync/node_data_processor_test.rb
+++ test/models/ckb_sync/node_data_processor_test.rb
@@ -690,6 +690,21 @@ module CkbSync
       end
     end
 
+    test "#process_block should decrease address deposit when previous output is a dao cell" do
+      CkbSync::Api.any_instance.stubs(:calculate_dao_maximum_withdraw).returns("0x174876ebe8")
+      node_block = fake_node_block("0x3307186493c5da8b91917924253a5ffd35231151649d0c7e2941aa8801815063")
+      VCR.use_cassette("blocks/#{DEFAULT_NODE_BLOCK_NUMBER}") do
+        tx = fake_dao_withdraw_transaction(node_block)
+        output = tx.cell_outputs.first
+        address = output.address
+        address.update(dao_deposit: output.capacity)
+
+        assert_difference -> { address.reload.dao_deposit }, -output.capacity do
+          node_data_processor.process_block(node_block)
+        end
+      end
+    end
+
     test "#process_block should update cell status" do
       VCR.use_cassette("blocks/#{DEFAULT_NODE_BLOCK_NUMBER}", record: :new_episodes) do
         node_block = fake_node_block("0x3307186493c5da8b91917924253a5ffd35231151649d0c7e2941aa8801815063")
