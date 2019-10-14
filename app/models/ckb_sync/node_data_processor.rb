6c8abeed6232f4ac0a4ebca9fd902dd95044917c
shaojunda <shaojunda@gmail.com>
2019-10-14 15:11:44 +0800

    feat: decrease dao contract subsidy_granted when block invalid

--- app/models/ckb_sync/node_data_processor.rb
+++ app/models/ckb_sync/node_data_processor.rb
@@ -171,6 +171,12 @@ module CkbSync
           event.reverted!
         end
 
+        issue_subsidy_dao_events = dao_events.where(event_type: "issue_subsidy")
+        issue_subsidy_dao_events.each do |event|
+          dao_contract.decrement!(:subsidy_granted, event.value)
+          event.reverted!
+        end
+
         local_tip_block.invalid!
         local_tip_block.contained_addresses.each(&method(:update_address_balance_and_ckb_transactions_count))
         revert_block_rewards(local_tip_block)
