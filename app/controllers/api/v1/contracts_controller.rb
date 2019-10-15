fccdb4a7434c0a1c470259b441fad88955e12ef9
shaojunda <shaojunda@gmail.com>
2019-10-15 11:24:03 +0800

    feat: implement show action

--- app/controllers/api/v1/contracts_controller.rb
+++ app/controllers/api/v1/contracts_controller.rb
@@ -2,7 +2,9 @@ module Api
   module V1
     class ContractsController < ApplicationController
       def show
-        render json: {}
+        raise Api::V1::Exceptions::ContractNotFoundError if params[:id] != "dao_contract"
+
+        render json: DaoContractSerializer.new(DaoContract.default_contract)
       end
     end
   end
