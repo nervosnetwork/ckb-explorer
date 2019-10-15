87ab983f4af96d5c97572c7b152502e01eab711d
shaojunda <shaojunda@gmail.com>
2019-10-15 14:05:59 +0800

    test: content-type and media type

---
+++ test/controllers/api/v1/contract_transactions_controller_test.rb
@@ -0,0 +1,44 @@
+require "test_helper"
+
+module Api
+  module V1
+    class ContractTransactionsControllerTest < ActionDispatch::IntegrationTest
+      test "should set right content type when call index" do
+        valid_get api_v1_contract_transaction_url("dao_contract")
+
+        assert_equal "application/vnd.api+json", response.media_type
+      end
+
+      test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
+        get api_v1_contract_transaction_url("dao_contract"), headers: { "Content-Type": "text/plain" }
+
+        assert_equal 415, response.status
+      end
+
+      test "should respond with error object when Content-Type is wrong" do
+        error_object = Api::V1::Exceptions::WrongContentTypeError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_contract_transaction_url("dao_contract"), headers: { "Content-Type": "text/plain" }
+
+        assert_equal response_json, response.body
+      end
+
+      test "should respond with 406 Not Acceptable when Accept is wrong" do
+        get api_v1_contract_transaction_url("dao_contract"), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal 406, response.status
+      end
+
+      test "should respond with error object when Accept is wrong" do
+        error_object = Api::V1::Exceptions::WrongAcceptError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_contract_transaction_url("dao_contract"), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal response_json, response.body
+      end
+    end
+  end
+end
+
