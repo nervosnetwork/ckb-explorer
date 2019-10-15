02b6f2fec04e49bfbc9cc4a09cc929428ab7e7fc
shaojunda <shaojunda@gmail.com>
2019-10-15 18:05:23 +0800

    feat: return empty json on show action

--- test/controllers/api/v1/dao_contract_transactions_controller_test.rb
+++ test/controllers/api/v1/dao_contract_transactions_controller_test.rb
@@ -3,37 +3,46 @@ module Api
   module V1
     class DaoContractTransactionsControllerTest < ActionDispatch::IntegrationTest
       test "should set right content type when call index" do
-        valid_get api_v1_dao_contract_transaction_url("dao_contract")
+        ckb_transaction = create(:ckb_transaction)
+
+        valid_get api_v1_dao_contract_transaction_url(ckb_transaction.tx_hash)
 
         assert_equal "application/vnd.api+json", response.media_type
       end
 
       test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
-        get api_v1_dao_contract_transaction_url("dao_contract"), headers: { "Content-Type": "text/plain" }
+        ckb_transaction = create(:ckb_transaction)
+
+        get api_v1_dao_contract_transaction_url(ckb_transaction.tx_hash), headers: { "Content-Type": "text/plain" }
 
         assert_equal 415, response.status
       end
 
       test "should respond with error object when Content-Type is wrong" do
+        ckb_transaction = create(:ckb_transaction)
+
         error_object = Api::V1::Exceptions::WrongContentTypeError.new
         response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
-        get api_v1_dao_contract_transaction_url("dao_contract"), headers: { "Content-Type": "text/plain" }
+        get api_v1_dao_contract_transaction_url(ckb_transaction.tx_hash), headers: { "Content-Type": "text/plain" }
 
         assert_equal response_json, response.body
       end
 
       test "should respond with 406 Not Acceptable when Accept is wrong" do
-        get api_v1_dao_contract_transaction_url("dao_contract"), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+        ckb_transaction = create(:ckb_transaction)
+
+        get api_v1_dao_contract_transaction_url(ckb_transaction.tx_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
 
         assert_equal 406, response.status
       end
 
       test "should respond with error object when Accept is wrong" do
+        ckb_transaction = create(:ckb_transaction)
         error_object = Api::V1::Exceptions::WrongAcceptError.new
         response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
-        get api_v1_dao_contract_transaction_url("dao_contract"), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+        get api_v1_dao_contract_transaction_url(ckb_transaction.tx_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
 
         assert_equal response_json, response.body
       end
