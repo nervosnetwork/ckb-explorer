f527e55adc0c00eb9306b33a0f450dfa2202426f
shaojunda <shaojunda@gmail.com>
2019-10-16 14:41:24 +0800

    feat: implement address dao transactions show action

--- app/controllers/api/v1/address_dao_transactions_controller.rb
+++ app/controllers/api/v1/address_dao_transactions_controller.rb
@@ -1,8 +1,34 @@
 module Api
   module V1
     class AddressDaoTransactionsController < ApplicationController
+      before_action :validate_query_params
+      before_action :validate_pagination_params, :pagination_params
+
       def show
-        render json: {}
+        address = Address.find_address!(params[:id])
+        presented_address = AddressPresenter.new(address)
+        ckb_dao_transactions = presented_address.ckb_dao_transactions.recent.distinct.page(@page).per(@page_size)
+        options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: ckb_dao_transactions, page: @page, page_size: @page_size).call
+
+        render json: CkbTransactionSerializer.new(ckb_dao_transactions, options.merge({ params: { previews: true } }))
+      end
+
+      private
+
+      def validate_query_params
+        validator = Validations::Address.new(params)
+
+        if validator.invalid?
+          errors = validator.error_object[:errors]
+          status = validator.error_object[:status]
+
+          render json: errors, status: status
+        end
+      end
+
+      def pagination_params
+        @page = params[:page] || 1
+        @page_size = params[:page_size] || CkbTransaction.default_per_page
       end
     end
   end
