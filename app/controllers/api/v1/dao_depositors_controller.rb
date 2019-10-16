d6a230543f54e71a8f75d4cefc9182be30619b62
shaojunda <shaojunda@gmail.com>
2019-10-16 20:45:13 +0800

    feat: implement dao depositors index action

--- app/controllers/api/v1/dao_depositors_controller.rb
+++ app/controllers/api/v1/dao_depositors_controller.rb
@@ -2,7 +2,9 @@ module Api
   module V1
     class DaoDepositorsController < ApplicationController
       def index
-        render json: []
+        addresses = Address.where("dao_deposit > 0").order(dao_deposit: :desc).limit(100)
+
+        render json: DaoDepositorSerializer.new(addresses)
       end
     end
   end
