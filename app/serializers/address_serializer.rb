ad72bc09c3fc6c2f44e9836caf2d5284eb883740
shaojunda <shaojunda@gmail.com>
2019-11-13 13:59:38 +0800

    feat: add since parser

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -17,4 +17,22 @@ class AddressSerializer
   attribute :interest do |object|
     object.interest.to_s
   end
+  attribute :lock_info do |object|
+    parsed_result = CkbUtils.parse_address(object.address_hash)
+    bin_args = CKB::Utils.hex_to_bin(parsed_result.script.args)
+    if parsed_result.address_type == "FULL" && bin_args.bytesize == 28
+      since = CKB::Utils.bin_to_hex(bin_args[-8..-1]).delete_prefix("0x")
+      begin
+        since_value = SinceParser.new(since).parse
+        if since_value.present?
+          tip_epoch_number = CkbSync::Api.instance.get_current_epoch.number
+          lock_status = tip_epoch_number > since_value.number ? "locked" : "unlocked"
+
+          { status: lock_status, epoch_number: since_value.number.to_s, epoch_index: since_value.index.to_s }
+        end
+      ensure SinceParser::IncorrectSinceFlagsError
+        nil
+      end
+    end
+  end
 end
