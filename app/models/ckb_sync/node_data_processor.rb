af5524e86d250a28d55553cc45989ff1de0e7d48
shaojunda <shaojunda@gmail.com>
2019-11-14 11:18:49 +0800

    perf: update address ckb transactions count speed

--- app/models/ckb_sync/node_data_processor.rb
+++ app/models/ckb_sync/node_data_processor.rb
@@ -485,7 +485,7 @@ module CkbSync
 
     def update_address_balance_and_ckb_transactions_count(address)
       address.balance = address.cell_outputs.live.sum(:capacity)
-      address.ckb_transactions_count = address.ckb_transactions.distinct.count
+      address.ckb_transactions_count = AccountBook.where(address: address).select(:ckb_transaction_id).distinct.count
       address.save!
     end
 
