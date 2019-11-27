5360c919d246d54d86e7eb4d25f5332e1c9279cc
shaojunda <shaojunda@gmail.com>
2019-11-27 11:44:10 +0800

    feat: add background job related gems

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   require "sidekiq/web"
   require "sidekiq_unique_jobs/web"
+  require "sidekiq/cron/web"
   mount Sidekiq::Web => "/sidekiq"
 
   root "application#homepage"
