cf243aa02f4cc6dc8ad46cecb4de44d1764160bb
shaojunda <shaojunda@gmail.com>
2019-11-29 07:54:37 +0800

    refactor: skip check header info for market data api

--- app/controllers/api/v1/market_data_controller.rb
+++ app/controllers/api/v1/market_data_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class MarketDataController < ApplicationController
+      skip_before_action :check_header_info
+
       def show
         return if params[:id] != "circulating_supply"
 
--- test/controllers/api/v1/market_data_controller_test.rb
+++ test/controllers/api/v1/market_data_controller_test.rb
@@ -10,36 +10,6 @@ module Api
         assert_equal "application/vnd.api+json", response.media_type
       end
 
-      test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
-        get api_v1_market_datum_url("circulating_supply"), headers: { "Content-Type": "text/plain" }
-
-        assert_equal 415, response.status
-      end
-
-      test "should respond with error object when Content-Type is wrong" do
-        error_object = Api::V1::Exceptions::WrongContentTypeError.new
-        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
-
-        get api_v1_market_datum_url("circulating_supply"), headers: { "Content-Type": "text/plain" }
-
-        assert_equal response_json, response.body
-      end
-
-      test "should respond with 406 Not Acceptable when Accept is wrong" do
-        get api_v1_market_datum_url("circulating_supply"), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
-
-        assert_equal 406, response.status
-      end
-
-      test "should respond with error object when Accept is wrong" do
-        error_object = Api::V1::Exceptions::WrongAcceptError.new
-        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
-
-        get api_v1_market_datum_url("circulating_supply"), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
-
-        assert_equal response_json, response.body
-      end
-
       test "should return current circulating supply" do
         create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
         valid_get api_v1_market_datum_url("circulating_supply")
