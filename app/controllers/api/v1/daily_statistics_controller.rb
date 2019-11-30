67b86c27d642b550c591e691c3990fc1fc4e18cd
shaojunda <shaojunda@gmail.com>
2019-11-30 13:58:00 +0800

    feat: implement daily statistic show action

--- app/controllers/api/v1/daily_statistics_controller.rb
+++ app/controllers/api/v1/daily_statistics_controller.rb
@@ -2,6 +2,8 @@ module Api
   module V1
     class DailyStatisticsController < ApplicationController
       def show
+        daily_statistics = DailyStatistic.limit(365)
+        render json: DailyStatisticSerializer.new(daily_statistics, { params: { indicator: params[:id] } })
       end
     end
   end
