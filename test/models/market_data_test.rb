0d30929f8df4332a3ceaf75fbf7ba9ab57688c3d
shaojunda <shaojunda@gmail.com>
2019-11-30 11:50:20 +0800

    test: ecosystem locked

--- test/models/market_data_test.rb
+++ test/models/market_data_test.rb
@@ -22,4 +22,32 @@ class MarketDataTest < ActiveSupport::TestCase
 
     assert_equal expected_circulating_supply, MarketData.new.send(:circulating_supply)
   end
+
+  test "ecosystem_locked should return 97% of ecosystem quota when current time is before first released time" do
+    create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
+    MarketData.any_instance.stubs(:current_time).returns(Time.zone.parse("2019-11-30"))
+
+    assert_equal MarketData::ECOSYSTEM_QUOTA * 0.97, MarketData.new.send(:ecosystem_locked)
+  end
+
+  test "ecosystem_locked should return 75% of ecosystem quota when current time is after first released time but before second released time" do
+    create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
+    MarketData.any_instance.stubs(:current_time).returns(Time.zone.parse("2020-08-02"))
+
+    assert_equal MarketData::ECOSYSTEM_QUOTA * 0.75, MarketData.new.send(:ecosystem_locked)
+  end
+
+  test "ecosystem_locked should return 50% of ecosystem quota when current time is after second released time but before third released time" do
+    create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
+    MarketData.any_instance.stubs(:current_time).returns(Time.zone.parse("2021-08-02"))
+
+    assert_equal MarketData::ECOSYSTEM_QUOTA * 0.5, MarketData.new.send(:ecosystem_locked)
+  end
+
+  test "ecosystem_locked should return zero when current time is after third released time" do
+    create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
+    MarketData.any_instance.stubs(:current_time).returns(Time.zone.parse("2023-08-02"))
+
+    assert_equal 0, MarketData.new.send(:ecosystem_locked)
+  end
 end
