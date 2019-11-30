cef81a5c5f5071e46572ec66286b9a1e6fb9a06a
shaojunda <shaojunda@gmail.com>
2019-11-30 11:49:52 +0800

    test: new circulating supply

--- test/controllers/api/v1/market_data_controller_test.rb
+++ test/controllers/api/v1/market_data_controller_test.rb
@@ -10,15 +10,23 @@ module Api
         assert_equal "application/vnd.api+json", response.media_type
       end
 
-      test "should return current circulating supply" do
+      test "should return current total supply" do
         create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
-        valid_get api_v1_market_datum_url("circulating_supply")
+        valid_get api_v1_market_datum_url("total_supply")
         latest_dao = Block.recent.pick(:dao)
         parsed_dao = CkbUtils.parse_dao(latest_dao)
         result = parsed_dao.c_i - (336 * 10**16 * 0.25).to_d
-        expected_circulating_supply = (result / 10**8).to_s
+        expected_total_supply = (result / 10**8).to_s
 
-        assert_equal expected_circulating_supply, json
+        assert_equal expected_total_supply, json
+      end
+
+      test "should return current circulating supply" do
+        create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
+        valid_get api_v1_market_datum_url("circulating_supply")
+        MarketData.new("circulating_supply").call
+
+        assert_equal MarketData.new("circulating_supply").call.to_s, json
       end
     end
   end
--- test/models/market_data_test.rb
+++ test/models/market_data_test.rb
@@ -1,13 +1,25 @@
 require "test_helper"
 
 class MarketDataTest < ActiveSupport::TestCase
-  test "circulating_supply should return right value" do
+  test "total_supply should return right value" do
     create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
     latest_dao = Block.recent.pick(:dao)
     parsed_dao = CkbUtils.parse_dao(latest_dao)
     result = parsed_dao.c_i - (336 * 10**16 * 0.25).to_d
     expected_circulating_supply = result / 10**8
 
-    assert_equal expected_circulating_supply, MarketData.new.circulating_supply
+    assert_equal expected_circulating_supply, MarketData.new.send(:total_supply)
+  end
+
+  test "circulating_supply should return right value" do
+    create(:block, dao: "0x80d6ccc02604d52ebc30325a84902300e7d511536bb20a00002b5625ba150007")
+    MarketData.any_instance.stubs(:current_time).returns(Time.zone.parse("2019-11-30"))
+    latest_dao = Block.recent.pick(:dao)
+    parsed_dao = CkbUtils.parse_dao(latest_dao)
+    result = parsed_dao.c_i - MarketData::BURN_QUOTA - MarketData::ECOSYSTEM_QUOTA * 0.97 -
+      MarketData::TEAM_QUOTA * (2 / 3.to_d) - MarketData::PRIVATE_SALE_QUOTA * (1 / 3.to_d) - MarketData::FOUNDING_PARTNER_QUOTA - MarketData::FOUNDATION_RESERVE_QUOTA
+    expected_circulating_supply = (result / 10**8).truncate(8)
+
+    assert_equal expected_circulating_supply, MarketData.new.send(:circulating_supply)
   end
 end
