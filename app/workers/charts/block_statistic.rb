0dc3101668c4aacf0b64418a559327597c753e8c
shaojunda <shaojunda@gmail.com>
2019-12-01 19:48:27 +0800

    feat: add epoch statistic worker

--- app/workers/charts/block_statistic.rb
+++ app/workers/charts/block_statistic.rb
@@ -6,16 +6,17 @@ module Charts
     sidekiq_options queue: "critical"
 
     def perform
-      latest_block_statistic = BlockStatistic.order(:id).last
-      target_block = Block.find_by(number: latest_block_statistic.block_number + 100)
+      latest_block_statistic = ::BlockStatistic.order(:id).last
+      target_block_number = latest_block_statistic.block_number.to_i + 100
+      target_block = Block.find_by(number: target_block_number)
       return if target_block.blank?
 
       statistic_info = StatisticInfo.new
-      hash_rate = statistic_info.hash_rate(block.number)
+      hash_rate = statistic_info.hash_rate(target_block_number)
       live_cell_count = CellOutput.live.count
       dead_cell_count = CellOutput.dead.count
 
-      BlockStatistic.create(block_number: target_block.number, difficulty: target_block.difficulty, hash_rate: hash_rate, live_cell_count: live_cell_count, dead_cell_count: dead_cell_count)
+      ::BlockStatistic.create(block_number: target_block.number, difficulty: target_block.difficulty, hash_rate: hash_rate, live_cell_count: live_cell_count, dead_cell_count: dead_cell_count)
     end
   end
 end
