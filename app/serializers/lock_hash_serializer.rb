e96d1194ba7051e468f67030fe63f1a4a248d178
shaojunda <shaojunda@gmail.com>
2019-12-16 14:58:41 +0800

    feat: add new attributes to lock hash serializer

--- app/serializers/lock_hash_serializer.rb
+++ app/serializers/lock_hash_serializer.rb
@@ -5,21 +5,37 @@ class LockHashSerializer
 
   attributes :address_hash, :lock_hash
 
+  attribute :lock_info do |object|
+    object.lock_script.lock_info
+  end
   attribute :lock_script do |object|
     object.lock_script.to_node_lock
   end
-
+  attribute :balance do |object|
+    object.balance.to_s
+  end
   attribute :transactions_count do |object|
     object.ckb_transactions_count.to_s
   end
-
-  attribute :balance do |object|
-    object.balance.to_s
+  attribute :pending_reward_blocks_count do |object|
+    object.pending_reward_blocks_count.to_s
   end
   attribute :dao_deposit do |object|
     object.dao_deposit.to_s
   end
   attribute :interest do |object|
     object.interest.to_s
+  end
+  attribute :is_special do |object|
+    object.special?.to_s
+  end
+  attribute :special_address, if: Proc.new { |record| record.special? } do |object|
+    Settings.special_addresses[object.address_hash]
+  end
+  attribute :live_cells_count do |object|
+    object.live_cells_count.to_s
+  end
+  attribute :mined_blocks_count do |object|
+    object.mined_blocks_count.to_s
   end
 end
