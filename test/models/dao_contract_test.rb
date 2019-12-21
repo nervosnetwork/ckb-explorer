87f2f1a88d43ce72964d7d675301f120084daa01
shaojunda <shaojunda@gmail.com>
2019-12-21 13:58:35 +0800

    test: estimated apc

--- test/models/dao_contract_test.rb
+++ test/models/dao_contract_test.rb
@@ -27,4 +27,27 @@ class DaoContractTest < ActiveSupport::TestCase
     expected_attributes = %w(created_at deposit_transactions_count depositors_count id interest_granted total_deposit total_depositors_count updated_at withdraw_transactions_count)
     assert_equal expected_attributes, dao_contract.attributes.keys.sort
   end
+
+  test "estimated apc when deposit period is less than one year" do
+    dao_contract = DaoContract.default_contract
+    expected_estimated_apc = 3.7
+    deposit_epoch = OpenStruct.new(number: 0, index:0, length: 1800)
+    assert_equal expected_estimated_apc, dao_contract.estimated_apc(deposit_epoch, 2190 * 0.19).round(2)
+  end
+
+  test "estimated apc when deposit period is one year cross period" do
+    dao_contract = DaoContract.default_contract
+    expected_estimated_apc = 2.44
+    deposit_epoch = OpenStruct.new(number: 2190 * 3.5, index:0, length: 1800)
+
+    assert_equal expected_estimated_apc, dao_contract.estimated_apc(deposit_epoch).round(2)
+  end
+
+  test "estimated apc when deposit period is more than four year" do
+    dao_contract = DaoContract.default_contract
+    expected_estimated_apc = 2.94
+    deposit_epoch = OpenStruct.new(number: 0, index:0, length: 1800)
+
+    assert_equal expected_estimated_apc, dao_contract.estimated_apc(deposit_epoch, 2190 * 5.5).round(2)
+  end
 end
