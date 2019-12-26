48be8e428ceee100ac650e5c9a1c9de420d9eac1
shaojunda <shaojunda@gmail.com>
2019-12-26 16:52:00 +0800

    feat: save hash rate on epoch statistic worker

--- app/workers/charts/epoch_statistic.rb
+++ app/workers/charts/epoch_statistic.rb
@@ -14,8 +14,12 @@ module Charts
       uncles_count = Block.where(epoch: target_epoch_number).sum(:uncles_count)
       uncle_rate = uncles_count / blocks_count.to_d
       difficulty = Block.where(epoch: target_epoch_number).first.difficulty
+      first_block_in_epoch = Block.where(epoch: target_epoch_number).order(:number).first
+      last_lock_in_epoch = Block.where(epoch: target_epoch_number).order(:number).last
+      block_time = last_lock_in_epoch.timestamp - first_block_in_epoch.timestamp
+      hash_rate = difficulty * first_block_in_epoch.length / block_time
 
-      ::EpochStatistic.create(epoch_number: target_epoch_number, difficulty: difficulty, uncle_rate: uncle_rate)
+      ::EpochStatistic.create(epoch_number: target_epoch_number, difficulty: difficulty, uncle_rate: uncle_rate, hash_rate: hash_rate)
     end
   end
 end
