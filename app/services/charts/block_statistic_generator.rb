db9161492318dd8a61bbf239e7cc2b0000a86bca
shaojunda <shaojunda@gmail.com>
2019-12-30 11:26:40 +0800

    feat: add block statistic generator service

---
+++ app/services/charts/block_statistic_generator.rb
@@ -0,0 +1,22 @@
+module Charts
+  class BlockStatisticGenerator
+    def initialize(block_number)
+      @block_number = block_number
+    end
+
+    def call
+      statistic_info = StatisticInfo.new
+      hash_rate = statistic_info.hash_rate(block_number)
+      live_cells_count = CellOutput.live.count
+      dead_cells_count = CellOutput.dead.count
+      target_block = Block.find_by(number: block_number)
+      return if target_block.blank?
+
+      block_statistic = ::BlockStatistic.find_or_create_by(block_number: block_number)
+      block_statistic.update(difficulty: target_block.difficulty, hash_rate: hash_rate, live_cell_count: live_cells_count, dead_cell_count: dead_cells_count)
+    end
+
+    private
+    attr_reader :block_number
+  end
+end
