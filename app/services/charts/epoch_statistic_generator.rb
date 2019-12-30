8d003f12b9dab039135108f25f891b83557848b5
shaojunda <shaojunda@gmail.com>
2019-12-30 11:26:09 +0800

    chore: use epoch statistic generator service on epoch statistic worker

--- app/services/charts/epoch_statistic_generator.rb
+++ app/services/charts/epoch_statistic_generator.rb
@@ -1,22 +1,27 @@
 module Charts
   class EpochStatisticGenerator
-    def initialize(epoch_number)
-      @epoch_number = epoch_number
+    def initialize(target_epoch_number)
+      @target_epoch_number = target_epoch_number
     end
 
     def call
-      return if Block.where(epoch: epoch_number + 1).blank?
+      return if Block.where(epoch: target_epoch_number).blank?
 
       blocks_count = Block.where(epoch: epoch_number).count
-      uncles_count = Block.where(epoch: epoch_number).sum(:uncles_count)
+      uncles_count = Block.where(epoch: target_epoch_number).sum(:uncles_count)
       uncle_rate = uncles_count / blocks_count.to_d
-      difficulty = Block.where(epoch: epoch_number).first.difficulty
+      difficulty = Block.where(epoch: target_epoch_number).first.difficulty
+      first_block_in_epoch = Block.where(epoch: target_epoch_number).order(:number).first
+      last_lock_in_epoch = Block.where(epoch: target_epoch_number).order(:number).last
+      block_time = last_lock_in_epoch.timestamp - first_block_in_epoch.timestamp
+      epoch_length = Block.where(epoch: target_epoch_number).count
+      hash_rate = difficulty * epoch_length / block_time
 
-      epoch_statistic = ::EpochStatistic.find_or_create_by(epoch_number: epoch_number)
-      epoch_statistic.update(difficulty: difficulty, uncle_rate: uncle_rate)
+      epoch_statistic = ::EpochStatistic.find_or_create_by(epoch_number: target_epoch_number)
+      epoch_statistic.update(difficulty: difficulty, uncle_rate: uncle_rate, hash_rate: hash_rate)
     end
 
     private
-    attr_reader :epoch_number
+    attr_reader :target_epoch_number
   end
 end
--- app/workers/charts/epoch_statistic.rb
+++ app/workers/charts/epoch_statistic.rb
@@ -10,17 +10,7 @@ module Charts
       target_epoch_number = latest_epoch_number + 1
       return if Block.where(epoch: target_epoch_number + 1).blank? || ::EpochStatistic.where(epoch_number: target_epoch_number).exists?
 
-      blocks_count = Block.where(epoch: target_epoch_number).count
-      uncles_count = Block.where(epoch: target_epoch_number).sum(:uncles_count)
-      uncle_rate = uncles_count / blocks_count.to_d
-      difficulty = Block.where(epoch: target_epoch_number).first.difficulty
-      first_block_in_epoch = Block.where(epoch: target_epoch_number).order(:number).first
-      last_lock_in_epoch = Block.where(epoch: target_epoch_number).order(:number).last
-      block_time = last_lock_in_epoch.timestamp - first_block_in_epoch.timestamp
-      epoch_length = Block.where(epoch: target_epoch_number).count
-      hash_rate = difficulty * epoch_length / block_time
-
-      ::EpochStatistic.create(epoch_number: target_epoch_number, difficulty: difficulty, uncle_rate: uncle_rate, hash_rate: hash_rate)
+      Charts::EpochStatisticGenerator.new(target_epoch_number).call
     end
   end
 end
