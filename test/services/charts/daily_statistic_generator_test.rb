ffde82c2e989a16d02e0ff7b90e4cf33329dd11c
shaojunda <shaojunda@gmail.com>
2020-01-04 14:28:36 +0800

    chore: adjust test

--- test/services/charts/daily_statistic_generator_test.rb
+++ test/services/charts/daily_statistic_generator_test.rb
@@ -5,7 +5,7 @@ module Charts
     test "should create daily statistic record" do
       block = create(:block, dao: "0xaff1568bbe49672f8a02516252ab2300df8c9e15dad428000035a1d671700007")
       tx = create(:ckb_transaction, block: block)
-      create(:cell_output, cell_type: "nervos_dao_deposit", generated_by: tx, ckb_transaction: tx, block: block, capacity: 10**8 * 1000, block_timestamp: (DateTime.now - 1.day).end_of_day.strftime("%Q"))
+      create(:cell_output, cell_type: "nervos_dao_deposit", generated_by: tx, ckb_transaction: tx, block: block, capacity: 10**8 * 1000, block_timestamp: (Time.current - 1.day).end_of_day.strftime("%Q"))
       assert_difference -> { ::DailyStatistic.count }, 1 do
         Charts::DailyStatisticGenerator.new.call
       end
