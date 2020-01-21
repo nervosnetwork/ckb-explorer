ba0e1057059a461325951e25bd7ba82e2241731c
shaojunda <shaojunda@gmail.com>
2020-01-21 17:49:33 +0800

    feat: add unlaimed compenstaion generator worker

---
+++ app/workers/dao_contract_unclaimed_compensation_generator.rb
@@ -0,0 +1,29 @@
+class DaoContractUnclaimedCompensationGenerator
+  include Sidekiq::Worker
+
+  def perform
+    DaoContract.default_contract.update(unclaimed_compensation: cal_unclaimed_compensation)
+  end
+
+  private
+
+  def cal_unclaimed_compensation
+    phase1_dao_interests + unmade_dao_interests
+  end
+
+  def phase1_dao_interests
+    CellOutput.nervos_dao_withdrawing.generated_before(ended_at).unconsumed_at(ended_at).reduce(0) do |memo, nervos_dao_withdrawing_cell|
+      memo + CkbUtils.dao_interest(nervos_dao_withdrawing_cell)
+    end
+  end
+
+  def unmade_dao_interests
+    CellOutput.nervos_dao_deposit.generated_before(ended_at).unconsumed_at(ended_at).reduce(0) do |memo, cell_output|
+      dao = cell_output.block.dao
+      tip_dao = current_tip_block.dao
+      parse_dao = CkbUtils.parse_dao(dao)
+      tip_parse_dao = CkbUtils.parse_dao(tip_dao)
+      memo + (cell_output.capacity * tip_parse_dao.ar_i / parse_dao.ar_i) - cell_output.capacity
+    end
+  end
+end
