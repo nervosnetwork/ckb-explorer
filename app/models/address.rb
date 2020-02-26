37bc65f1264e2d425caafd69ffb58730f160cae3
shaojunda <shaojunda@gmail.com>
2020-02-26 17:50:31 +0800

    chore: relation between address and udt account

--- app/models/address.rb
+++ app/models/address.rb
@@ -6,6 +6,7 @@ class Address < ApplicationRecord
   has_many :account_books, dependent: :destroy
   has_many :ckb_transactions, through: :account_books
   has_many :mining_infos
+  has_many :udt_accounts
   validates :balance, :cell_consumed, :ckb_transactions_count, :interest, :dao_deposit, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
 
   scope :visible, -> { where(visible: true) }
