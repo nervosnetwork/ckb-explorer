2152b9211b6ba3f397ae2cf3768412bbb1268dcd
shaojunda <shaojunda@gmail.com>
2020-03-04 13:30:16 +0800

    chore: complete udt show action

--- app/controllers/api/v1/udts_controller.rb
+++ app/controllers/api/v1/udts_controller.rb
@@ -1,4 +1,24 @@
 class Api::V1::UdtsController < ApplicationController
+  before_action :validate_query_params, only: :show
+
   def show
+    udt = Udt.find_by!(type_hash: params[:id])
+
+    render json: UdtSerializer.new(udt)
+  rescue ActiveRecord::RecordNotFound
+    raise Api::V1::Exceptions::UdtNotFoundError
+  end
+
+  private
+
+  def validate_query_params
+    validator = Validations::Udt.new(params)
+
+    if validator.invalid?
+      errors = validator.error_object[:errors]
+      status = validator.error_object[:status]
+
+      render json: errors, status: status
+    end
   end
 end
