6a8ac319346ccb2df2341d624cbeedcb0413f886
shaojunda <shaojunda@gmail.com>
2020-03-04 18:45:22 +0800

    test: udt transaction show action

--- test/controllers/api/v1/udt_transactions_controller_test.rb
+++ test/controllers/api/v1/udt_transactions_controller_test.rb
@@ -10,7 +10,7 @@ class Api::V1::UdtTransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should set right content type when call show" do
-    udt = create(:udt)
+    udt = create(:udt, published: true)
 
     valid_get api_v1_udt_transaction_url(udt.type_hash)
 
@@ -18,7 +18,7 @@ class Api::V1::UdtTransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
-    udt = create(:udt)
+    udt = create(:udt, published: true)
 
     get api_v1_udt_transaction_url(udt.type_hash), headers: { "Content-Type": "text/plain" }
 
@@ -26,7 +26,7 @@ class Api::V1::UdtTransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should respond with error object when Content-Type is wrong" do
-    udt = create(:udt)
+    udt = create(:udt, published: true)
     error_object = Api::V1::Exceptions::InvalidContentTypeError.new
     response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
@@ -36,7 +36,7 @@ class Api::V1::UdtTransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should respond with 406 Not Acceptable when Accept is wrong" do
-    udt = create(:udt)
+    udt = create(:udt, published: true)
 
     get api_v1_udt_transaction_url(udt.type_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
 
@@ -44,12 +44,161 @@ class Api::V1::UdtTransactionsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should respond with error object when Accept is wrong" do
-    udt = create(:udt)
+    udt = create(:udt, published: true)
     error_object = Api::V1::Exceptions::InvalidAcceptError.new
     response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
     get api_v1_udt_transaction_url(udt.type_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
 
     assert_equal response_json, response.body
+  end
+
+  test "should return error object when id is not a type hash" do
+    error_object = Api::V1::Exceptions::TypeHashInvalidError.new
+    response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+    valid_get api_v1_udt_transaction_url("9034fwefwef")
+
+    assert_equal response_json, response.body
+  end
+
+  test "should return corresponding ckb transactions with given type hash" do
+    page = 1
+    page_size = 10
+    udt = create(:udt, :with_transactions, published: true)
+    ckb_transactions = udt.ckb_transactions.order(block_timestamp: :desc).page(page).per(page_size)
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash)
+
+    options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: ckb_transactions, page: page, page_size: page_size).call
+
+    assert_equal CkbTransactionSerializer.new(ckb_transactions, options.merge({ params: { previews: true } })).serialized_json, response.body
+  end
+
+  test "should contain right keys in the serialized object when call show" do
+    udt = create(:udt, :with_transactions, published: true)
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash)
+
+    response_tx_transaction = json["data"].first
+
+    assert_equal %w(block_number transaction_hash block_timestamp transaction_fee version display_inputs display_outputs is_cellbase income witnesses cell_deps header_deps).sort, response_tx_transaction["attributes"].keys.sort
+  end
+
+  test "should return error object when no records found by id" do
+    error_object = Api::V1::Exceptions::UdtNotFoundError.new
+    response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+    valid_get api_v1_udt_transaction_url("0x5c5069eb0857efc65e1bca0c07df34c31663b3622fd3876c876320fc9634e2a8")
+
+    assert_equal response_json, response.body
+  end
+
+  test "should return error object when target udt is not published" do
+    udt = create(:udt, :with_transactions)
+    error_object = Api::V1::Exceptions::UdtNotFoundError.new
+    response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash)
+
+    assert_equal response_json, response.body
+  end
+
+  test "should return error object when page param is invalid" do
+    udt = create(:udt, :with_transactions, published: true)
+    error_object = Api::V1::Exceptions::PageParamError.new
+    response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash), params: { page: "aaa" }
+
+    assert_equal response_json, response.body
+  end
+
+  test "should return error object when page size param is invalid" do
+    udt = create(:udt, :with_transactions, published: true)
+    error_object = Api::V1::Exceptions::PageSizeParamError.new
+    response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash), params: { page_size: "aaa" }
+
+    assert_equal response_json, response.body
+  end
+
+  test "should return error object when page and page size param are invalid" do
+    errors = []
+    udt = create(:udt, :with_transactions, published: true)
+    errors << Api::V1::Exceptions::PageParamError.new
+    errors << Api::V1::Exceptions::PageSizeParamError.new
+    response_json = RequestErrorSerializer.new(errors, message: errors.first.title).serialized_json
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash), params: { page: "bbb", page_size: "aaa" }
+
+    assert_equal response_json, response.body
+  end
+
+  test "should return 10 records when page and page_size are not set" do
+    udt = create(:udt, :with_transactions, published: true)
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash)
+
+    assert_equal 10, json["data"].size
+  end
+
+  test "should return corresponding page's records when page is set and page_size is not set" do
+    page = 2
+    page_size = 10
+    udt = create(:udt, :with_transactions, published: true)
+    udt_ckb_transactions = udt.ckb_transactions.order(block_timestamp: :desc).page(page).per(page_size)
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash), params: { page: page }
+
+    options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: udt_ckb_transactions, page: page, page_size: page_size).call
+    response_transaction = CkbTransactionSerializer.new(udt_ckb_transactions, options.merge({ params: { previews: true } })).serialized_json
+
+    assert_equal response_transaction, response.body
+    assert_equal page_size, json["data"].size
+  end
+
+  test "should return the corresponding transactions under the udt when page is not set and page_size is set" do
+    page = 1
+    page_size = 12
+    udt = create(:udt, :with_transactions, published: true)
+    udt_ckb_transactions = udt.ckb_transactions.order(block_timestamp: :desc).page(page).per(page_size)
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash), params: { page_size: page_size }
+
+    options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: udt_ckb_transactions, page: page, page_size: page_size).call
+    response_transaction = CkbTransactionSerializer.new(udt_ckb_transactions, options.merge({ params: { previews: true } })).serialized_json
+
+    assert_equal response_transaction, response.body
+    assert_equal page_size, json["data"].size
+  end
+
+  test "should return the corresponding transactions when page and page_size are set" do
+    page = 2
+    page_size = 5
+    udt = create(:udt, :with_transactions, published: true)
+    udt_ckb_transactions = udt.ckb_transactions.order(block_timestamp: :desc).page(page).per(page_size)
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash), params: { page: page, page_size: page_size }
+    options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: udt_ckb_transactions, page: page, page_size: page_size).call
+    response_transaction = CkbTransactionSerializer.new(udt_ckb_transactions, options.merge({ params: { previews: true } })).serialized_json
+
+    assert_equal response_transaction, response.body
+  end
+
+  test "should return empty array when there is no record under the udt" do
+    page = 5
+    page_size = 10
+    udt = create(:udt, :with_transactions, published: true)
+    udt_ckb_transactions = udt.ckb_transactions.order(block_timestamp: :desc).page(page).per(page_size)
+
+    valid_get api_v1_udt_transaction_url(udt.type_hash), params: { page: page, page_size: page_size }
+
+    options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: udt_ckb_transactions, page: page, page_size: page_size).call
+    response_transaction = CkbTransactionSerializer.new(udt_ckb_transactions, options.merge({ params: { previews: true } })).serialized_json
+
+    assert_equal [], json["data"]
+    assert_equal response_transaction, response.body
   end
 end
--- test/factories/udt.rb
+++ test/factories/udt.rb
@@ -8,5 +8,16 @@ FactoryBot.define do
     full_name { "kingdom fat coin" }
     symbol { "kfc" }
     decimal { 6 }
+
+    trait :with_transactions do
+      after(:create) do |udt, _evaluator|
+        20.times do
+          block = create(:block, :with_block_hash)
+          transaction = create(:ckb_transaction, block: block)
+          transaction1 = create(:ckb_transaction, block: block)
+          create(:cell_output, block: block, ckb_transaction: transaction, generated_by: transaction, consumed_by: transaction1, type_hash: udt.type_hash, cell_type: "udt", data: "0x000050ad321ea12e0000000000000000")
+        end
+      end
+    end
   end
 end
