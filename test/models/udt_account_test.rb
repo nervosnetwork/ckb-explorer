ae1354f4d2982540bccec32fc752ebe133489bfc
shaojunda <shaojunda@gmail.com>
2020-03-12 21:34:45 +0800

    chore: adjust tests

--- test/models/udt_account_test.rb
+++ test/models/udt_account_test.rb
@@ -6,12 +6,10 @@ class UdtAccountTest < ActiveSupport::TestCase
   end
 
   context "validations" do
-    should validate_presence_of(:full_name)
-    should validate_presence_of(:symbol)
-    should validate_presence_of(:decimal)
     should validate_presence_of(:amount)
-    should validate_numericality_of(:decimal).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(39)
+    should validate_numericality_of(:decimal).allow_nil.is_greater_than_or_equal_to(0).is_less_than_or_equal_to(39)
     should validate_numericality_of(:amount).is_greater_than_or_equal_to(0)
-    should validate_length_of(:symbol).is_at_least(1).is_at_most(16)
+    should validate_length_of(:symbol).allow_nil.is_at_least(1).is_at_most(16)
+    should validate_length_of(:full_name).allow_nil.is_at_least(1).is_at_most(32)
   end
 end
--- test/models/udt_test.rb
+++ test/models/udt_test.rb
@@ -2,13 +2,10 @@ require "test_helper"
 
 class UdtTest < ActiveSupport::TestCase
   context "validations" do
-    should validate_presence_of(:full_name)
-    should validate_presence_of(:symbol)
-    should validate_presence_of(:decimal)
     should validate_presence_of(:total_amount)
-    should validate_numericality_of(:decimal).is_greater_than_or_equal_to(0).is_less_than_or_equal_to(39)
+    should validate_numericality_of(:decimal).allow_nil.is_greater_than_or_equal_to(0).is_less_than_or_equal_to(39)
     should validate_numericality_of(:total_amount).is_greater_than_or_equal_to(0)
-    should validate_length_of(:symbol).is_at_least(1).is_at_most(16)
-    should validate_length_of(:full_name).is_at_least(1).is_at_most(32)
+    should validate_length_of(:symbol).allow_nil.is_at_least(1).is_at_most(16)
+    should validate_length_of(:full_name).allow_nil.is_at_least(1).is_at_most(32)
   end
 end
