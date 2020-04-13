ee600dcdd31e598372632d2708c7050adc287ffb
shaojunda <shaojunda@gmail.com>
2020-04-13 17:00:58 +0800

    chore: check indicator name

--- app/controllers/api/v1/distribution_data_controller.rb
+++ app/controllers/api/v1/distribution_data_controller.rb
@@ -1,9 +1,23 @@
 module Api
   module V1
     class DistributionDataController < ApplicationController
+      before_action :validate_query_params, only: :show
       def show
         distribution_data = DistributionData.new
         render json: DistributionDataSerializer.new(distribution_data, { params: { indicator: params[:id] }})
+      end
+
+      private
+
+      def validate_query_params
+        validator = Validations::DistributionData.new(params)
+
+        if validator.invalid?
+          errors = validator.error_object[:errors]
+          status = validator.error_object[:status]
+
+          render json: errors, status: status
+        end
       end
     end
   end
