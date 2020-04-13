871aefe4813045d20d20d641cddaa4a350695cc6
shaojunda <shaojunda@gmail.com>
2020-04-13 11:25:07 +0800

    feat: add new column to daily statistics
    address_balance_distribution

--- app/models/daily_statistic.rb
+++ app/models/daily_statistic.rb
@@ -6,26 +6,27 @@ end
 #
 # Table name: daily_statistics
 #
-#  id                       :bigint           not null, primary key
-#  transactions_count       :string           default("0")
-#  addresses_count          :string           default("0")
-#  total_dao_deposit        :string           default("0.0")
-#  block_timestamp          :decimal(30, )
-#  created_at_unixtimestamp :integer
-#  created_at               :datetime         not null
-#  updated_at               :datetime         not null
-#  dao_depositors_count     :string           default("0")
-#  unclaimed_compensation   :string           default("0")
-#  claimed_compensation     :string           default("0")
-#  average_deposit_time     :string           default("0")
-#  estimated_apc            :string           default("0")
-#  mining_reward            :string           default("0")
-#  deposit_compensation     :string           default("0")
-#  treasury_amount          :string           default("0")
-#  live_cells_count         :string           default("0")
-#  dead_cells_count         :string           default("0")
-#  avg_hash_rate            :string           default("0")
-#  avg_difficulty           :string           default("0")
-#  uncle_rate               :string           default("0")
-#  total_depositors_count   :string           default("0")
+#  id                           :bigint           not null, primary key
+#  transactions_count           :string           default("0")
+#  addresses_count              :string           default("0")
+#  total_dao_deposit            :string           default("0.0")
+#  block_timestamp              :decimal(30, )
+#  created_at_unixtimestamp     :integer
+#  created_at                   :datetime         not null
+#  updated_at                   :datetime         not null
+#  dao_depositors_count         :string           default("0")
+#  unclaimed_compensation       :string           default("0")
+#  claimed_compensation         :string           default("0")
+#  average_deposit_time         :string           default("0")
+#  estimated_apc                :string           default("0")
+#  mining_reward                :string           default("0")
+#  deposit_compensation         :string           default("0")
+#  treasury_amount              :string           default("0")
+#  live_cells_count             :string           default("0")
+#  dead_cells_count             :string           default("0")
+#  avg_hash_rate                :string           default("0")
+#  avg_difficulty               :string           default("0")
+#  uncle_rate                   :string           default("0")
+#  total_depositors_count       :string           default("0")
+#  address_balance_distribution :jsonb
 #
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_060907) do
+ActiveRecord::Schema.define(version: 2020_04_13_030841) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -179,6 +179,7 @@ ActiveRecord::Schema.define(version: 2020_01_22_060907) do
     t.string "avg_difficulty", default: "0"
     t.string "uncle_rate", default: "0"
     t.string "total_depositors_count", default: "0"
+    t.jsonb "address_balance_distribution"
   end
 
   create_table "dao_contracts", force: :cascade do |t|
