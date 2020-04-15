9a696f350b114b6adc1516ad4a6baab36ce0d991
shaojunda <shaojunda@gmail.com>
2020-04-15 11:26:28 +0800

    chore: generate block_time_distribution

--- app/services/charts/epoch_statistic_generator.rb
+++ app/services/charts/epoch_statistic_generator.rb
@@ -18,11 +18,26 @@ module Charts
       hash_rate = difficulty * epoch_length / block_time
 
       epoch_statistic = ::EpochStatistic.find_or_create_by(epoch_number: target_epoch_number)
-      epoch_statistic.update(difficulty: difficulty, uncle_rate: uncle_rate, hash_rate: hash_rate)
+      epoch_statistic.update(difficulty: difficulty, uncle_rate: uncle_rate, hash_rate: hash_rate, block_time_distribution: block_time_distribution)
     end
 
     private
 
     attr_reader :target_epoch_number
+
+    def block_time_distribution
+      max_n = 19
+      epoch_interval = 99
+      start_epoch_number = [target_epoch_number - epoch_interval, 0].max
+      epoch_numbers = (start_epoch_number..target_epoch_number).to_a
+      ranges = (0..max_n).map { |n| [n, n + 1] }
+
+      ranges.map do |range|
+        millisecond_start = range[0] * 1000
+        millisecond_end = range[1] * 1000
+        block_count = Block.where(epoch: epoch_numbers).where("block_time > ? and block_time < ?", millisecond_start, millisecond_end).count
+        [range[1], block_count]
+      end
+    end
   end
 end
