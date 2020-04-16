18903eda52b6175ebc1b822e349766cd6ee700f5
shaojunda <shaojunda@gmail.com>
2020-04-16 10:22:07 +0800

    chore: filter indicator

--- app/serializers/distribution_data_serializer.rb
+++ app/serializers/distribution_data_serializer.rb
@@ -1,11 +1,15 @@
 class DistributionDataSerializer
   include FastJsonapi::ObjectSerializer
 
-  attribute :address_balance_distribution do |object|
+  attribute :address_balance_distribution, if: Proc.new { |_record, params|
+     params && params[:indicator].include?("address_balance_distribution")
+  } do |object|
     object.address_balance_distribution.map { |distribution| distribution.map(&:to_s) }
   end
 
-  attribute :block_time_distribution do |object|
+  attribute :block_time_distribution, if: Proc.new { |_record, params|
+    params && params[:indicator].include?("block_time_distribution")
+  } do |object|
     object.block_time_distribution.map { |distribution| distribution.map(&:to_s) }
   end
 end
