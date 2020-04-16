b6315a1f4213a93b844a1a7b4fa6c9098f610c54
shaojunda <shaojunda@gmail.com>
2020-04-16 18:33:17 +0800

    chore: adjust test

--- test/services/charts/daily_statistic_generator_test.rb
+++ test/services/charts/daily_statistic_generator_test.rb
@@ -6,6 +6,7 @@ module Charts
       block = create(:block, dao: "0xaff1568bbe49672f8a02516252ab2300df8c9e15dad428000035a1d671700007", timestamp: (Time.current - 1.day).end_of_day.to_i * 1000)
       tx = create(:ckb_transaction, block: block, block_timestamp: block.timestamp)
       create(:cell_output, cell_type: "nervos_dao_deposit", generated_by: tx, ckb_transaction: tx, block: block, capacity: 10**8 * 1000, block_timestamp: (Time.current - 1.day).end_of_day.to_i * 1000)
+      create(:daily_statistic, created_at_unixtimestamp: Time.current.yesterday.yesterday.beginning_of_day.to_i)
       assert_difference -> { ::DailyStatistic.count }, 1 do
         Charts::DailyStatisticGenerator.new.call
       end
