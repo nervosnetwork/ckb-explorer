6886ff457a0b4cfb051ddc885336044135afcb08
shaojunda <shaojunda@gmail.com>
2020-04-28 16:27:29 +0800

    chore: remove block time distribution
    from epoch statistics

--- app/services/charts/epoch_statistic_generator.rb
+++ app/services/charts/epoch_statistic_generator.rb
@@ -20,8 +20,9 @@ module Charts
       hash_rate = difficulty * epoch_length / epoch_time
 
       epoch_statistic = ::EpochStatistic.find_or_create_by(epoch_number: target_epoch_number)
-      epoch_statistic.update(difficulty: difficulty, uncle_rate: uncle_rate, hash_rate: hash_rate, block_time_distribution: block_time_distribution,
-                             epoch_time: epoch_time, epoch_time_distribution: epoch_time_distribution, epoch_length: epoch_length, epoch_length_distribution: epoch_length_distribution)
+      epoch_statistic.update(difficulty: difficulty, uncle_rate: uncle_rate, hash_rate: hash_rate,
+                             epoch_time: epoch_time, epoch_time_distribution: epoch_time_distribution,
+                             epoch_length: epoch_length, epoch_length_distribution: epoch_length_distribution)
     end
 
     private
@@ -60,21 +61,6 @@ module Charts
 
         [range[1], epoch_count]
       }.compact
-    end
-
-    def block_time_distribution
-      max_n = 19
-      epoch_interval = 99
-      start_epoch_number = [target_epoch_number - epoch_interval, 0].max
-      epoch_numbers = (start_epoch_number..target_epoch_number).to_a
-      ranges = (0..max_n).map { |n| [n, n + 1] }
-
-      ranges.map do |range|
-        millisecond_start = range[0] * 1000
-        millisecond_end = range[1] * 1000
-        block_count = Block.where(epoch: epoch_numbers).where("block_time > ? and block_time <= ?", millisecond_start, millisecond_end).count
-        [range[1], block_count]
-      end
     end
   end
 end
--- lib/tasks/migration/fill_epoch_distribution_info_to_epoch_statistics.rake
+++ lib/tasks/migration/fill_epoch_distribution_info_to_epoch_statistics.rake
@@ -8,11 +8,10 @@ class EpochDistributionInfoGenerator
         latest_epoch_statistic = EpochStatistic.order(epoch_number: :desc)[0]
         epoch_statistic_generator = Charts::EpochStatisticGenerator.new(latest_epoch_statistic.epoch_number)
 
-        block_time_distribution = epoch_statistic_generator.send(:block_time_distribution)
         epoch_time_distribution = epoch_statistic_generator.send(:epoch_time_distribution)
         epoch_length_distribution = epoch_statistic_generator.send(:epoch_length_distribution)
 
-        latest_epoch_statistic.update(block_time_distribution: block_time_distribution, epoch_time_distribution: epoch_time_distribution, epoch_length_distribution: epoch_length_distribution)
+        latest_epoch_statistic.update(epoch_time_distribution: epoch_time_distribution, epoch_length_distribution: epoch_length_distribution)
         puts "done"
       end
     end
