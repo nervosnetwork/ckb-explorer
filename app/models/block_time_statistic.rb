210421c509e393068bad57a0e903c87cade7e461
shaojunda <shaojunda@gmail.com>
2020-04-30 20:04:51 +0800

    chore: generate hour average block time

--- app/models/block_time_statistic.rb
+++ app/models/block_time_statistic.rb
@@ -1,11 +1,38 @@
 class BlockTimeStatistic < ApplicationRecord
-  def generate
-    started_at = time_in_milliseconds(30.days.ago.beginning_of_day)
-    ended_at = time_in_milliseconds(30.days.ago.end_of_day) - 1
+  def generate_monthly
+    started_at = 30.days.ago.beginning_of_day
+    ended_at = 30.days.ago.end_of_day
+
+    generate(started_at, ended_at)
+  end
+
+  def generate_daily
+    started_at = Time.current.yesterday.beginning_of_day
+    ended_at = Time.current.yesterday.end_of_day
+
+    generate(started_at, ended_at)
+  end
+
+  private
+
+  def generate(started_at, ended_at)
+    values =
+      (started_at..ended_at).map do |datetime|
+        BlockTimeStatistic.connection.select_all(avg_block_time_sql).to_a.map do |item|
+          item["created_at"] = Time.current
+          item["updated_at"] = Time.current
+          item
+        end
+      end
+
+    BlockTimeStatistic.upsert_all(values, unique_by: :stat_timestamp)
+  end
+
+  def avg_block_time_sql(started_at, ended_at)
     <<-SQL
       select
-        date_trunc('hour', to_timestamp(timestamp/1000.0)) datetime,
-        avg(block_time) avg_time
+        date_trunc('hour', to_timestamp(timestamp/1000.0)) stat_timestamp,
+        avg(block_time) avg_block_time_per_hour
       from blocks
       where timestamp >= #{started_at} and timestamp <= #{ended_at}
       group by 1
