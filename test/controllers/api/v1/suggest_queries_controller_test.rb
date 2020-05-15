a0c797dedd85551346baec0a33f9a8368144949f
shaojunda <shaojunda@gmail.com>
2020-05-15 15:49:42 +0800

    chore: adjust tests

--- test/controllers/api/v1/suggest_queries_controller_test.rb
+++ test/controllers/api/v1/suggest_queries_controller_test.rb
@@ -111,8 +111,7 @@ module Api
       test "should return address when query key is a exist address hash" do
         address = create(:address, :with_lock_script)
         address.query_address = address.address_hash
-        presented_address = AddressPresenter.new(address)
-        response_json = AddressSerializer.new(presented_address).serialized_json
+        response_json = AddressSerializer.new(address).serialized_json
 
         valid_get api_v1_suggest_queries_url, params: { q: address.address_hash }
 
@@ -152,20 +151,18 @@ module Api
         address = create(:address, :with_lock_script, address_hash: "ckb1qyqt8xaupvm8837nv3gtc9x0ekkj64vud3jqfwyw5v")
         query_key = "ckb1qjda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xw3vumhs9nvu786dj9p0q5elx66t24n3kxgj53qks"
         address.query_address = query_key
-        presented_address = AddressPresenter.new(address)
         valid_get api_v1_suggest_queries_url, params: { q: query_key }
 
-        assert_equal AddressSerializer.new(presented_address).serialized_json, response.body
+        assert_equal AddressSerializer.new(address).serialized_json, response.body
       end
 
       test "should support short address query when full address's lock script exists" do
         address = create(:address, :with_lock_script, address_hash: "ckb1qjda0cr08m85hc8jlnfp3zer7xulejywt49kt2rr0vthywaa50xw3vumhs9nvu786dj9p0q5elx66t24n3kxgj53qks")
         query_key = "ckb1qyqt8xaupvm8837nv3gtc9x0ekkj64vud3jqfwyw5v"
         address.query_address = query_key
-        presented_address = AddressPresenter.new(address)
         valid_get api_v1_address_url(query_key)
 
-        assert_equal AddressSerializer.new(presented_address).serialized_json, response.body
+        assert_equal AddressSerializer.new(address).serialized_json, response.body
       end
 
       test "should return a udt when query key is a exist udt type hash" do
