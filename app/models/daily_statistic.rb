c1e0bf968289b53777a141a96153ca5ca5ffcd65
shaojunda <shaojunda@gmail.com>
2020-05-25 18:11:48 +0800

    feat: add locked_capacity to daily statistics

--- app/models/daily_statistic.rb
+++ app/models/daily_statistic.rb
@@ -2,8 +2,12 @@ class DailyStatistic < ApplicationRecord
   VALID_INDICATORS = %w(
     transactions_count addresses_count total_dao_deposit live_cells_count dead_cells_count avg_hash_rate avg_difficulty uncle_rate
     total_depositors_count address_balance_distribution total_tx_fee occupied_capacity daily_dao_deposit daily_dao_depositors_count
-    circulation_ratio daily_dao_withdraw nodes_count circulating_supply
+    circulation_ratio daily_dao_withdraw nodes_count circulating_supply burnt
   ).freeze
+
+  def burnt
+    treasury_amount + MarketData::BURN_QUOTA
+  end
 end
 
 # == Schema Information
@@ -47,4 +51,5 @@ end
 #  average_block_time           :jsonb
 #  nodes_distribution           :jsonb
 #  nodes_count                  :integer
+#  locked_capacity              :decimal(30, )
 #
