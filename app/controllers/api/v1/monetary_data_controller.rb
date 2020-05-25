ed143229ed8b89280b110bd67c929dda79e610c9
shaojunda <shaojunda@gmail.com>
2020-05-25 13:08:55 +0800

    feat: add monetary data validator

--- app/controllers/api/v1/monetary_data_controller.rb
+++ app/controllers/api/v1/monetary_data_controller.rb
@@ -1,10 +1,23 @@
 module Api
   module V1
     class MonetaryDataController < ApplicationController
+      before_action :validate_query_params, only: :show
+
       def show
         monetary_data = MonetaryData.new
 
         render json: MonetaryDataSerializer.new(monetary_data, params: { indicator: params[:id] })
+      end
+
+      def validate_query_params
+        validator = Validations::DistributionData.new(params)
+
+        if validator.invalid?
+          errors = validator.error_object[:errors]
+          status = validator.error_object[:status]
+
+          render json: errors, status: status
+        end
       end
     end
   end
