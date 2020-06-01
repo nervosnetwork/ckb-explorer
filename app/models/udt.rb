7e451ee641ac0f523a9ab430bded5ebb1d641bae
shaojunda <shaojunda@gmail.com>
2020-06-01 20:24:45 +0800

    feat: add block_timestamp to udt

--- app/models/udt.rb
+++ app/models/udt.rb
@@ -36,6 +36,7 @@ end
 #  published        :boolean          default(FALSE)
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
+#  block_timestamp  :decimal(30, )
 #
 # Indexes
 #
