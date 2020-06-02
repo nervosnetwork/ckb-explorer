59b68d5c4b15b0af49183cd6589f57d3b23572ff
shaojunda <shaojunda@gmail.com>
2020-06-02 18:29:54 +0800

    chore: set checkpoint to miner distribution

--- app/models/distribution_data.rb
+++ app/models/distribution_data.rb
@@ -83,8 +83,9 @@ class DistributionData
 
     Rails.cache.realize("miner_address_distribution_#{checkpoint}", expires_in: 1.day) do
       result = Block.where("timestamp >= ?", CkbUtils.time_in_milliseconds(checkpoint.days.ago.to_i)).group(:miner_hash).order("count(miner_hash) desc").count.to_a
+      cut_off_point = (result.count * 0.7).ceil
       if result.present?
-        (result[0..9].map{ |item| [item[0], item[1].to_s] } + [["other", result[10..-1].map { |item| item[1] }.reduce(:+).to_s]]).to_h
+        (result[0..cut_off_point].map{ |item| [item[0], item[1].to_s] } + [["other", result[(cut_off_point + 1)..-1].map { |item| item[1] }.reduce(:+).to_s]]).to_h
       else
         result
       end
