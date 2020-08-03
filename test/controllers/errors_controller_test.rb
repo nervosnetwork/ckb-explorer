ea1798af3b05efa039a39a4212ffcbeae8f0476c
shaojunda <shaojunda@gmail.com>
2020-08-03 22:40:19 +0800

    test: routing error

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,7 +1,16 @@
-require 'test_helper'
+require "test_helper"
 
 class ErrorsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should return 404 status code when path is not exist" do
+    post "/"
+
+    assert_response :not_found
+  end
+
+  test "should return Not Found message when path is not exist" do
+    post "/oop"
+
+    assert_response :not_found
+    assert_equal "Not Found", json["message"]
+  end
 end
