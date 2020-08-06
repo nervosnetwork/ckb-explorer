4231860fee2be275ba8752a12db4e11a4f42baac
shaojunda <shaojunda@gmail.com>
2020-08-06 14:01:48 +0800

    feat: add ckb_transactions_count to dao_contracts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_05_093044) do
+ActiveRecord::Schema.define(version: 2020_08_06_060029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -244,6 +244,7 @@ ActiveRecord::Schema.define(version: 2020_08_05_093044) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.decimal "unclaimed_compensation", precision: 30
+    t.decimal "ckb_transactions_count", precision: 30, default: "0"
   end
 
   create_table "dao_events", force: :cascade do |t|
