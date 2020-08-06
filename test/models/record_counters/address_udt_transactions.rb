6d1ada3f08e22e47460aa8e435c2e27c0c736bea
shaojunda <shaojunda@gmail.com>
2020-08-07 00:14:08 +0800

    test: address udt transactions counter

---
+++ test/models/record_counters/address_udt_transactions.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class AddressUdtTransactionsTest < ActiveSupport::TestCase
+  test "should respond to total_count" do
+    address = create(:address)
+    udt = create(:udt, published: true)
+    address_udt_transactions_counter = RecordCounters::AddressUdtTransactions.new(address, udt.id)
+    assert_respond_to address_udt_transactions_counter, :total_count
+  end
+
+  test "total_count should return dao_contract ckb transactions count" do
+    address = create(:address)
+    udt = create(:udt, published: true)
+    address_udt_transactions_counter = RecordCounters::AddressUdtTransactions.new(address, udt.id)
+    assert_equal address.ckb_udt_transactions(udt.id).count, address_udt_transactions_counter.total_count
+  end
+end
