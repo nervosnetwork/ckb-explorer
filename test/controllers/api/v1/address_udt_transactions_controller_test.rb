026e4a04069a1163e153e6c638c569ce3e90771f
shaojunda <shaojunda@gmail.com>
2020-10-27 16:30:56 +0800

    chore: update test

--- test/controllers/api/v1/address_udt_transactions_controller_test.rb
+++ test/controllers/api/v1/address_udt_transactions_controller_test.rb
@@ -168,7 +168,7 @@ module Api
         udt = create(:udt, published: true)
         address = create(:address, :with_udt_transactions, transactions_count: 30, udt: udt)
 
-        address_udt_transactions = address.ckb_udt_transactions(udt.id).order(block_timestamp: :desc).page(page).per(page_size)
+        address_udt_transactions = address.ckb_udt_transactions(udt.id).order(block_timestamp: :desc).recent.page(page).per(page_size)
 
         valid_get api_v1_address_udt_transaction_url(address.address_hash, type_hash: udt.type_hash), params: { page: page }
 
