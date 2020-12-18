922408563b7ea72f9b0d0c8b3e3ce29f0a993458
shaojunda <shaojunda@gmail.com>
2020-12-18 15:40:15 +0800

    perf: use cache on daily statistic controller

--- app/controllers/api/v1/daily_statistics_controller.rb
+++ app/controllers/api/v1/daily_statistics_controller.rb
@@ -5,7 +5,11 @@ module Api
 
       def show
         daily_statistics = DailyStatistic.order(:id).valid_indicators
-        render json: DailyStatisticSerializer.new(daily_statistics, { params: { indicator: params[:id] } })
+        json =
+          Rails.cache.realize(daily_statistics.cache_key, version: daily_statistics.cache_version, race_condition_ttl: 3.seconds) do
+            DailyStatisticSerializer.new(daily_statistics, { params: { indicator: params[:id] } })
+          end
+        render json: json
       end
 
       private
