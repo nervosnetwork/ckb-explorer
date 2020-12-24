4e1ef7841342e507dff6131c4c7c94cfde747b0a
shaojunda <shaojunda@gmail.com>
2020-12-25 01:44:00 +0800

    chore: add task to genereate tx display infos

---
+++ lib/tasks/migration/generate_tx_display_infos.rake.rb
@@ -0,0 +1,16 @@
+namespace :migration do
+	desc "Usage: RAILS_ENV=production bundle exec rake migration:generate_tx_display_infos"
+	task generate_tx_display_infos: :environment do
+		progress_bar = ProgressBar.create({ total: CkbTransaction.count, format: "%e %B %p%% %c/%C" })
+		CkbTransaction.find_in_batches(batch_size: 3000).each do |txs|
+			value =
+				txs.map do |tx|
+					progress_bar.increment
+					{ ckb_transaction_id: tx_id, inputs: tx.display_inputs, outputs: tx.display_outputs, created_at: Time.current, updated_at: Time.current }
+				end
+			if value.present?
+				TxDisplayInfo.upsert_all(value)
+			end
+		end
+	end
+end
