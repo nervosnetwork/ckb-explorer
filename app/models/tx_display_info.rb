56031ddd5fbaa5551b25a4652aaa0c8179b872b2
shaojunda <shaojunda@gmail.com>
2020-12-24 15:53:40 +0800

    feat: add tx_display_infos table

---
+++ app/models/tx_display_info.rb
@@ -0,0 +1,13 @@
+class TxDisplayInfo < ApplicationRecord
+end
+
+# == Schema Information
+#
+# Table name: tx_display_infos
+#
+#  ckb_transaction_id :bigint           not null, primary key
+#  inputs             :jsonb
+#  outputs            :jsonb
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#
