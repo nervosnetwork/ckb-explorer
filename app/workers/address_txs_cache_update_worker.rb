7e7c9fdd644eb53d0072b526aed46f5031c762f8
shaojunda <shaojunda@gmail.com>
2020-12-25 15:15:06 +0800

    perf: reduce memory use

--- app/workers/address_txs_cache_update_worker.rb
+++ app/workers/address_txs_cache_update_worker.rb
@@ -9,19 +9,21 @@ class AddressTxsCacheUpdateWorker
 
     already_exist_pairs.each do |k, v|
       score_member_pairs =
-        CkbTransaction.where(id: v).select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at).map do |tx|
+        CkbTransaction.where(id: v).select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at).find_each.map do |tx|
           [tx.id, tx.to_json]
         end
 
       service.write("Address/txs/#{k}", score_member_pairs, CkbTransaction)
     end
 
-    new_pairs.each do |k, _|
-      key = "Address/txs/#{k}"
-      service.fetch(key, 1, CkbTransaction::DEFAULT_PAGINATES_PER, CkbTransaction) do
-        CkbTransaction.where("contained_address_ids @> array[?]::bigint[]", [k]).select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at).recent
+    ActiveRecord::Base.uncached do
+      new_pairs.each do |k, _|
+        key = "Address/txs/#{k}"
+        service.fetch(key, 1, CkbTransaction::DEFAULT_PAGINATES_PER, CkbTransaction) do
+          CkbTransaction.where("contained_address_ids @> array[?]::bigint[]", [k]).select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at).recent
+        end
+        service.expire(key, 30)
       end
-      service.expire(key, 30)
     end
   end
 end
