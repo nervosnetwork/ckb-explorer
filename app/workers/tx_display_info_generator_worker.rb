41dc2feb375c5d99e3abfa810d95dff46081287a
shaojunda <shaojunda@gmail.com>
2020-12-27 20:53:23 +0800

    chore: check tx if present before update display info

--- app/workers/tx_display_info_generator_worker.rb
+++ app/workers/tx_display_info_generator_worker.rb
@@ -4,11 +4,13 @@ class TxDisplayInfoGeneratorWorker
 	def perform(tx_ids)
 		value =
 			tx_ids.map do |tx_id|
-				tx = CkbTransaction.find(tx_id)
-				{ ckb_transaction_id: tx_id, inputs: tx.display_inputs, outputs: tx.display_outputs, created_at: Time.current, updated_at: Time.current }
+				tx = CkbTransaction.find(id: tx_id)
+				if tx.present?
+					{ ckb_transaction_id: tx_id, inputs: tx.display_inputs, outputs: tx.display_outputs, created_at: Time.current, updated_at: Time.current }
+				end
 			end
 
-		if value.present?
+		if value.compact.present?
 			TxDisplayInfo.upsert_all(value)
 		end
 	end
