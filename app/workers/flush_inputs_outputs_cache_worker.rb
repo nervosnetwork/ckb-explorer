2c370a0759dc0ad2a2a065c5fd3767aeb9b8fa19
shaojunda <shaojunda@gmail.com>
2021-01-29 16:59:23 +0800

    refactor: make some operations to asynchronous

---
+++ app/workers/flush_inputs_outputs_cache_worker.rb
@@ -0,0 +1,18 @@
+class FlushInputsOutputsCacheWorker
+	include Sidekiq::Worker
+
+	def perform(block_id)
+		block = Block.find_by(id: block_id)
+		return if block.blank?
+
+		block.cell_inputs.select(:id).find_in_batches do |cell_inputs|
+			cache_keys = []
+			cell_inputs.each do |cell_input|
+				cache_keys << cell_input.cache_keys
+			end
+			$redis.pipelined do
+				$redis.del(*cache_keys)
+			end
+		end
+	end
+end
---
+++ app/workers/remove_tx_display_info_worker.rb
@@ -0,0 +1,8 @@
+class RemoveTxDisplayInfoWorker
+	include Sidekiq::Worker
+
+	def perform(block_id)
+		tx_ids = CellOutput.where(id: CellInput.where(block_id: block_id).pluck(:previous_cell_output_id)).pluck(:generated_by_id)
+		TxDisplayInfo.where(ckb_transaction_id: tx_ids).delete_all
+	end
+end
