4ddd2a7114a51b4dee55e911ff06966d5480fc64
shaojunda <shaojunda@gmail.com>
2021-02-25 20:49:12 +0800

    chore: remove deps from ckb_transactions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_22_102120) do
+ActiveRecord::Schema.define(version: 2021_02_25_124705) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -173,7 +173,6 @@ ActiveRecord::Schema.define(version: 2021_02_22_102120) do
 
   create_table "ckb_transactions", force: :cascade do |t|
     t.binary "tx_hash"
-    t.jsonb "deps"
     t.bigint "block_id"
     t.decimal "block_number", precision: 30
     t.decimal "block_timestamp", precision: 30
