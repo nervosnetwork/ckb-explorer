17a9818d33fd9d64eaeb73a5afb3adc8fbcec9d6
shaojunda <shaojunda@gmail.com>
2021-03-30 05:01:00 +0800

    chore: use worker to update block contained addresses info

--- app/workers/update_address_info_worker.rb
+++ app/workers/update_address_info_worker.rb
@@ -1,8 +1,14 @@
 class UpdateAddressInfoWorker
   include Sidekiq::Worker
 
-  def perform(address_id)
-    addr = Address.where(id: address_id).select(:id).first
-    addr.update_columns(ckb_transactions_count: addr.custom_ckb_transactions.count, live_cells_count: addr.cell_outputs.live.count, dao_transactions_count: addr.ckb_dao_transactions.count)
+  def perform(block_number)
+    address_attributes = []
+    block = Block.find_by(number: block_number)
+    block.contained_addresses.select(:id, :created_at).each do |addr|
+      address_attributes << { id: addr.id, balance: addr.cell_outputs.live.sum(:capacity), ckb_transactions_count: addr.custom_ckb_transactions.count,
+                              live_cells_count: addr.cell_outputs.live.count, dao_transactions_count: addr.ckb_dao_transactions.count,
+                              created_at: addr.created_at, updated_at: Time.current }
+    end
+    Address.upsert_all(address_attributes) if address_attributes.present?
   end
 end
