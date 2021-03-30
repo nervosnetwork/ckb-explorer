1a803a2b13d1ebdb50735b2ae3f9f0b228578558
shaojunda <shaojunda@gmail.com>
2021-03-30 17:40:40 +0800

    chore: remove seach script by script_hash

--- app/models/ckb_sync/new_node_data_processor.rb
+++ app/models/ckb_sync/new_node_data_processor.rb
@@ -582,7 +582,7 @@ module CkbSync
           unless Rails.cache.read("NodeData/#{block_number}/Lock/#{output.lock.code_hash}-#{output.lock.hash_type}-#{output.lock.args}")
             script_hash = output.lock.compute_hash
             # TODO remove search by code_hash, hash_type and args query after script_hash has been filled
-            unless LockScript.where(script_hash: script_hash).exists? && LockScript.where(code_hash: output.lock.code_hash, hash_type: output.lock.hash_type, args: output.lock.args).exists?
+            unless LockScript.where(code_hash: output.lock.code_hash, hash_type: output.lock.hash_type, args: output.lock.args).exists?
               lock_attrs = script_attributes(output.lock, script_hash)
               Rails.cache.write("NodeData/#{block_number}/Lock/#{output.lock.code_hash}-#{output.lock.hash_type}-#{output.lock.args}", true, expires_in: 5.seconds)
             end
@@ -591,7 +591,7 @@ module CkbSync
             unless Rails.cache.read("NodeData/#{block_number}/Type/#{output.type.code_hash}-#{output.type.hash_type}-#{output.type.args}")
               script_hash = output.type.compute_hash
               # TODO remove search by code_hash, hash_type and args query after script_hash has been filled
-              unless TypeScript.where(script_hash: script_hash).exists? && TypeScript.where(code_hash: output.type.code_hash, hash_type: output.type.hash_type, args: output.type.args).exists?
+              unless TypeScript.where(code_hash: output.type.code_hash, hash_type: output.type.hash_type, args: output.type.args).exists?
                 type_attrs = script_attributes(output.type, script_hash)
                 Rails.cache.write("NodeData/#{block_number}/Type/#{output.type.code_hash}-#{output.type.hash_type}-#{output.type.args}", true, expires_in: 5.seconds)
               end
