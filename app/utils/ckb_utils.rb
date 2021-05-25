6ddfa1ec11c9da9c802e6dff6e06091e0f4fa176
shaojunda <shaojunda@gmail.com>
2021-05-25 18:36:19 +0800

    feat: add m_nft related parser

--- app/utils/ckb_utils.rb
+++ app/utils/ckb_utils.rb
@@ -309,4 +309,48 @@ class CkbUtils
       "normal"
     end
   end
+
+  def self.parse_issuer_data(data)
+    data = data.delete_prefix("0x")
+    version = data[0..1].to_i(16)
+    class_count = data[2..9].to_i(16)
+    set_count = data[10..17].to_i(16)
+    info_size = data[18..21].to_i(16)
+    info = JSON.parse([data[22..-1]].pack("H*").force_encoding("utf-8"))
+
+    OpenStruct.new(version: version, class_count: class_count, set_count: set_count, info_size: info_size, info: info)
+  end
+
+  def self.parse_token_class_data(data)
+    data = data.delete_prefix("0x")
+    version = data[0..1].to_i(16)
+    total = data[2..9].to_i(16)
+    issued = data[10..17].to_i(16)
+    configure = data[18..19].to_i(16)
+    name_size = data[20..23].to_i(16)
+    name_end_index = (24 + name_size * 2 - 1)
+    name = [data[24..name_end_index]].pack("H*").force_encoding("utf-8")
+    description_size_start_index = name_end_index + 1
+    description_size_end_index = description_size_start_index + 4 - 1
+    description_size = data[description_size_start_index..description_size_end_index].to_i(16)
+    description_start_index = description_size_end_index + 1
+    description_end_index = description_start_index + description_size * 2 - 1
+    description = [data[description_start_index..description_end_index]].pack("H*").force_encoding("utf-8")
+    renderer_size_start_index = description_end_index + 1
+    renderer_size_end_index = renderer_size_start_index + 4 - 1
+    renderer_size = data[renderer_size_start_index..renderer_size_end_index].to_i(16)
+    renderer_start_index = renderer_size_end_index + 1
+    renderer_end_index = renderer_start_index + renderer_size * 2 - 1
+    renderer = [data[renderer_start_index, renderer_end_index]].pack("H*").force_encoding("utf-8")
+    OpenStruct.new(version: version, total: total, issued: issued, configure: configure, name: name, description: description, renderer: renderer)
+  end
+
+  def self.parse_token_data(data)
+    data = data.delete_prefix("0x")
+    version = data[0..1].to_i(16)
+    characteristic = data[2..17]
+    configure = data[18..19].to_i(16)
+    state = data[20..21].to_i(16)
+    OpenStruct.new(version: version, characteristic: characteristic, configure: configure, state: state)
+  end
 end
