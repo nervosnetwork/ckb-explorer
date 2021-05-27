67e56e1ac9fd1db6ec1a2c0493eb73dd9a2ddcb2
shaojunda <shaojunda@gmail.com>
2021-05-27 21:57:35 +0800

    refactor: adjust tests

--- test/models/lock_script_test.rb
+++ test/models/lock_script_test.rb
@@ -6,6 +6,7 @@ class LockScriptTest < ActiveSupport::TestCase
     create(:table_record_count, :ckb_transactions_counter)
     CkbSync::Api.any_instance.stubs(:get_blockchain_info).returns(OpenStruct.new(chain: "ckb_testnet"))
     GenerateStatisticsDataWorker.any_instance.stubs(:perform).returns(true)
+    CkbSync::Api.any_instance.stubs(:get_blockchain_info).returns(OpenStruct.new(chain: "ckb_testnet"))
   end
 
   context "associations" do
