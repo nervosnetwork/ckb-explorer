fcb4b990af3a09204500d0d1aae5c8208f97da73
shaojunda <shaojunda@gmail.com>
2021-07-16 13:32:28 +0800

    perf: add index on tx_status to pool_transaction_entries

--- app/models/cell_output.rb
+++ app/models/cell_output.rb
@@ -131,5 +131,6 @@ end
 #  index_cell_outputs_on_ckb_transaction_id      (ckb_transaction_id)
 #  index_cell_outputs_on_consumed_by_id          (consumed_by_id)
 #  index_cell_outputs_on_generated_by_id         (generated_by_id)
+#  index_cell_outputs_on_status                  (status)
 #  index_cell_outputs_on_tx_hash_and_cell_index  (tx_hash,cell_index)
 #
