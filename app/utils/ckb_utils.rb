599fa582d3ad23212e21e7fca6e7d7d453982bd5
shaojunda <shaojunda@gmail.com>
2021-08-10 13:20:49 +0800

    refactor: remove null char on issuer data parser

--- app/utils/ckb_utils.rb
+++ app/utils/ckb_utils.rb
@@ -318,7 +318,7 @@ class CkbUtils
     class_count = data[2..9].to_i(16)
     set_count = data[10..17].to_i(16)
     info_size = data[18..21].to_i(16)
-    info = JSON.parse([data[22..-1]].pack("H*").force_encoding("UTF-8").encode("UTF-8", invalid: :replace, undef: :replace))
+    info = JSON.parse([data[22..-1]].pack("H*").force_encoding("UTF-8").encode("UTF-8", invalid: :replace, undef: :replace).delete("\u0000"))
     OpenStruct.new(version: version, class_count: class_count, set_count: set_count, info_size: info_size, info: info)
   rescue
     OpenStruct.new(version: 0, class_count: 0, set_count: 0, info_size: 0, info: "")
