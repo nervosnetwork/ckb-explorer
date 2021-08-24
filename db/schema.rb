5bbe52e28944f0f24dbf368f6d5fa99dbc342ff4
shaojunda <shaojunda@gmail.com>
2021-08-24 21:41:50 +0800

    feat: add miner message to block

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_124921) do
+ActiveRecord::Schema.define(version: 2021_08_24_133215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2021_08_19_124921) do
     t.integer "proposals_count"
     t.decimal "cell_consumed", precision: 30
     t.binary "miner_hash"
+    t.string "miner_message"
     t.decimal "reward", precision: 30
     t.decimal "total_transaction_fee", precision: 30
     t.decimal "ckb_transactions_count", precision: 30, default: "0"
