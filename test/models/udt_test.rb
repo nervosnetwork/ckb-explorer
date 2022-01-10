642e17c3b6029bf17d163c3d7694f408ef0e315e
shaojunda <shaojunda@gmail.com>
2022-01-10 16:36:55 +0800

    refactor: adjust test

--- test/models/udt_test.rb
+++ test/models/udt_test.rb
@@ -6,7 +6,7 @@ class UdtTest < ActiveSupport::TestCase
     should validate_numericality_of(:decimal).allow_nil.is_greater_than_or_equal_to(0).is_less_than_or_equal_to(39)
     should validate_numericality_of(:total_amount).is_greater_than_or_equal_to(0)
     should validate_length_of(:symbol).allow_nil.is_at_least(1).is_at_most(16)
-    should validate_length_of(:full_name).allow_nil.is_at_least(1).is_at_most(32)
+    should validate_length_of(:full_name).allow_nil.is_at_least(1).is_at_most(100)
   end
 
   test "should return node type script when the udt is published" do
