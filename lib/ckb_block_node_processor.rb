ad1a5ed5f88b80ec0eeb882ca17fc09fc70552c8
ShiningRay <tsowly@hotmail.com>
2022-04-12 23:30:30 +0800

    lock block process to prevent conflict

--- lib/ckb_block_node_processor.rb
+++ lib/ckb_block_node_processor.rb
@@ -1,5 +1,9 @@
 require_relative "../config/environment"
 
+lock = Redis::Lock.new('CkbSync', :expiration => 300, :timeout => 30)
+
 loop do
-  CkbSync::NewNodeDataProcessor.new.call
+  lock.lock do
+    CkbSync::NewNodeDataProcessor.new.call
+  end
 end
