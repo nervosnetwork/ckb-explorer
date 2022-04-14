0a7d42a1103cb7e81ad5d0394d8dfb740e26a0a6
ShiningRay <tsowly@hotmail.com>
2022-04-15 01:24:45 +0800

    clear locks

--- lib/ckb_block_node_processor.rb
+++ lib/ckb_block_node_processor.rb
@@ -2,8 +2,14 @@ require_relative "../config/environment"
 
 lock = Redis::Lock.new('CkbSync', :expiration => 300, :timeout => 30)
 
+at_exit do 
+  lock.clear
+end
+
 loop do
   lock.lock do
     CkbSync::NewNodeDataProcessor.new.call
   end
 end
+
+
