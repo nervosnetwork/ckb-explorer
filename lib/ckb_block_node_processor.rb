33e1dfd4aef02ec2168ea16b76724be1b90905c2
ShiningRay <tsowly@hotmail.com>
2022-04-18 14:02:45 +0800

    fix

--- lib/ckb_block_node_processor.rb
+++ lib/ckb_block_node_processor.rb
@@ -11,9 +11,11 @@ puts 'start'
 loop do
   lock.lock do
     start = Time.now.to_f
-    remain = CkbSync::NewNodeDataProcessor.new.call
+    block = CkbSync::NewNodeDataProcessor.new.call
+    tip_block_number = CkbSync::Api.instance.get_tip_block_number
+    remain = tip_block_number - block.number
     duration = Time.now.to_f - start
-    sleep(1-duration) if remain > 0 && duration < 1 # only sleep when catched up with network
+    sleep(1-duration) if remain <= 0 && duration < 1 # only sleep when catched up with network
   end
 end
 
