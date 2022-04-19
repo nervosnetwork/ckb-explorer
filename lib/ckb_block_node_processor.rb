f64ba2b09d85a5f73664a92266a817e1bf6fea1c
ShiningRay <tsowly@hotmail.com>
2022-04-19 19:29:07 +0800

    fix

--- lib/ckb_block_node_processor.rb
+++ lib/ckb_block_node_processor.rb
@@ -18,10 +18,11 @@ loop do
     if block
       tip_block_number = CkbSync::Api.instance.get_tip_block_number
       remain = tip_block_number - block.number
+      duration = Time.now.to_f - start
     else
       remain = 0
+      duration = 0
     end
-    duration = Time.now.to_f - start
     sleep(1-duration) if remain <= 0 && duration < 1 # only sleep when catched up with network
   end
 end
