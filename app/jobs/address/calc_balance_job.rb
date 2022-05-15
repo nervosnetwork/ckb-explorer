c741e67559717ba97b80e697952913845e9d60fb
ShiningRay <tsowly@hotmail.com>
2022-05-15 11:50:18 +0800

    balance job

--- app/jobs/address/calc_balance_job.rb
+++ app/jobs/address/calc_balance_job.rb
@@ -1,6 +1,17 @@
 class Address::CalcBalanceJob < ApplicationJob
   def perform(address_id)
-    address =  address_id.is_a?(Address) ? address_id : Address.find(address_id)
+    address =  case address_id
+    when Address
+      address_id
+    when Number
+      Address.find(address_id)
+    when /\Ack.*\z/
+      Address.find_by address_hash: address_id
+    when /\A0x.*\z/
+      Address.find_by lock_hash: address_id
+    end
 
+    address.cal_balance!
+    address.save
   end
 end
