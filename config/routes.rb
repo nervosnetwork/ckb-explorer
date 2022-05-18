e7ef516e1a380f1ba2b0dbf17951a2631ef87842
ShiningRay <tsowly@hotmail.com>
2022-05-18 16:24:37 +0800

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -46,6 +46,10 @@ Rails.application.routes.draw do
       resources :distribution_data, only: :show
       resources :monetary_data, only: :show
     end
+
+    namespace :v2 do
+      post '/das_accounts' => 'das_accounts#query', as: :das_accounts
+    end
   end
   draw "v2"
   match "/:anything" => "errors#routing_error", via: :all, constraints: { anything: /.*/ }
