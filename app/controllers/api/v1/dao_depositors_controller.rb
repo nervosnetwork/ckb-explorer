557ea886d47571a3e779cdf7b045df32ce25da16
ShiningRay <tsowly@hotmail.com>
2022-06-29 12:21:58 +0800

    fix dao deposit address ranking

--- app/controllers/api/v1/dao_depositors_controller.rb
+++ app/controllers/api/v1/dao_depositors_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class DaoDepositorsController < ApplicationController
       def index
-        addresses = Address.select(:id, :address_hash, :dao_deposit, :average_deposit_time).where(is_depositor: true).order(dao_deposit: :desc).limit(100)
+        addresses = Address.select(:id, :address_hash, :dao_deposit, :average_deposit_time).where(is_depositor: true).where("dao_deposit > 0").order(dao_deposit: :desc).limit(100)
 
         render json: DaoDepositorSerializer.new(addresses)
       end
