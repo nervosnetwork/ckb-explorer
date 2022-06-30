1b9f156601159ddcfa2bd7167ebc51876f80f3be
ShiningRay <tsowly@hotmail.com>
2022-06-30 17:56:51 +0800

    always use dao deposit cell to calc deposit time

--- app/workers/address_average_deposit_time_generator.rb
+++ app/workers/address_average_deposit_time_generator.rb
@@ -1,4 +1,8 @@
+# Refresh average deposit time for each address
+# average_deposit_time = Average locked duration of *each CKByte* in DAO Deposit
+# so we must sum the locked duration of all each CKByte in locked and unlocked deposit cells.
 class AddressAverageDepositTimeGenerator
+  MilliSecondsInDay = BigDecimal(24 * 60 * 60 * 1000).freeze
   include Sidekiq::Worker
 
   def perform
@@ -11,28 +15,22 @@ class AddressAverageDepositTimeGenerator
 
   private
 
-  def cal_average_deposit_time(address)
-    interest_bearing_deposits = 0
-    uninterest_bearing_deposits = 0
-    milliseconds_in_day = BigDecimal(24 * 60 * 60 * 1000)
-    ended_at = CkbUtils.time_in_milliseconds(Time.current)
-    sum_interest_bearing =
-      address.cell_outputs.nervos_dao_withdrawing.unconsumed_at(ended_at).reduce(0) do |memo, nervos_dao_withdrawing_cell|
-        nervos_dao_withdrawing_cell_generated_tx = nervos_dao_withdrawing_cell.generated_by
-        nervos_dao_deposit_cell = nervos_dao_withdrawing_cell_generated_tx.cell_inputs.order(:id)[nervos_dao_withdrawing_cell.cell_index].previous_cell_output
-        interest_bearing_deposits += nervos_dao_deposit_cell.capacity
-        memo + nervos_dao_deposit_cell.capacity * (nervos_dao_withdrawing_cell.block_timestamp - nervos_dao_deposit_cell.block_timestamp) / milliseconds_in_day
-      end
-    sum_uninterest_bearing =
-      address.cell_outputs.nervos_dao_deposit.unconsumed_at(ended_at).reduce(0) do |memo, nervos_dao_deposit_cell|
-        uninterest_bearing_deposits += nervos_dao_deposit_cell.capacity
+  # return average deposit time of specific address (unit in day)
+  def cal_average_deposit_time(address, ended_at = CkbUtils.time_in_milliseconds(Time.current))
+    total_ckb_deposit_time = 0 #   unlocked CKBs
+    total_deposits = 0 # locked CKBs
 
-        memo + nervos_dao_deposit_cell.capacity * (ended_at - nervos_dao_deposit_cell.block_timestamp) / milliseconds_in_day
-      end
+    # for unlocked deposit cell
+    # we must calculate the locked duration from lock to unlock.
+    # for locking deposit cell
+    # we calculate the locked duration from lock to now.
+    address.cell_outputs.nervos_dao_deposit.each do |cell|
+      total_deposits += cell.capacity
+      total_ckb_deposit_time += cell.capacity * ((cell.consumed_by_id ? cell.consumed_block_timestamp : ended_at) - cell.block_timestamp) / MilliSecondsInDay
+    end
 
-    total_deposits = interest_bearing_deposits + uninterest_bearing_deposits
     return 0 if total_deposits.zero?
 
-    ((sum_interest_bearing + sum_uninterest_bearing) / total_deposits).truncate(6)
+    (total_ckb_deposit_time / total_deposits).truncate(6)
   end
 end
