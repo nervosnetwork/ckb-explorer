de81f2a4c55515e467c0cc1e59b0d649337c7bfd
ShiningRay <tsowly@hotmail.com>
2022-07-03 01:49:29 +0800

    owner address filter

--- app/controllers/api/v2/nft/items_controller.rb
+++ app/controllers/api/v2/nft/items_controller.rb
@@ -4,14 +4,17 @@ module Api
       before_action :find_collection
       def index
         scope = TokenItem.all
+        @owner = Address.find_address!(params[:owner]) if params[:owner]
         scope = scope.where(collection_id: @collection.id) if @collection
+
         scope = scope.where(standard) if params[:standard]
+        scope = scope.where(owner_id: @owner.id) if params[:owner]
         @pagy, @collections = pagy(scope)
-        render json: { 
+        render json: {
           data: @collections,
-          pagination: pagy_metadata(@pagy) 
+          pagination: pagy_metadata(@pagy)
         }
-      end      
+      end
 
       def show
         @collection = TokenCollection.find params[:collection_id]
@@ -19,8 +22,9 @@ module Api
 
         render json: @item.as_json.merge(collection: @item.collection.as_json)
       end
-      
+
       protected
+
       def find_collection
         @collection = TokenCollection.find_by_id params[:collection_id] if params[:collection_id].present?
       end
