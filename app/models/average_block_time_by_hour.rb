1029fe0a6f2968a03dbbb5100054dce93660a20e
代码之力 <9770+ShiningRay@users.noreply.github.com>
2022-08-03 11:57:24 +0800

    Fix/refresh materialized views (#975)
    Co-authored-by: Chen Yu <keithwhisper@gmail.com>
    Co-authored-by: ShiningRay <ShiningRay@users.noreply.github.com>

--- app/models/average_block_time_by_hour.rb
+++ app/models/average_block_time_by_hour.rb
@@ -3,7 +3,7 @@
 class AverageBlockTimeByHour < ApplicationRecord
   self.table_name = 'average_block_time_by_hour'
   def self.refresh
-    connection.execute "refresh materialized view average_block_time_by_hour CONCURRENTLY"
+    connection.execute "refresh materialized view CONCURRENTLY average_block_time_by_hour "
   end
 end
 
@@ -13,4 +13,8 @@ end
 #
 #  hour                    :datetime
 #  avg_block_time_per_hour :decimal(, )
+#
+# Indexes
+#
+#  index_average_block_time_by_hour_on_hour  (hour) UNIQUE
 #
