45870d9d2c465fd1d984fdcef3c71d0071566fa0
代码之力 <9770+ShiningRay@users.noreply.github.com>
2022-09-01 21:38:13 +0800

    feat: optimize query performance and other fix (#994)
    Co-authored-by: ShiningRay <ShiningRay@users.noreply.github.com>

--- test/workers/address_txs_cache_update_worker_test.rb
+++ test/workers/address_txs_cache_update_worker_test.rb
@@ -7,21 +7,6 @@ class AddressTxsCacheUpdateWorkerTest < ActiveSupport::TestCase
     Rails.cache.extend(CacheRealizer)
   end
 
-  test "address_txs_cache_update_worker should cache address fist 10 page txs when there are no records for the address" do
-    Sidekiq::Testing.inline!
-    block = create(:block, :with_block_hash)
-    addr = create(:address)
-    500.times.each do |i|
-      create(:ckb_transaction, :with_single_output, block: block, contained_address_ids: [addr.id], block_timestamp: Time.current.to_i + i)
-    end
-    addr.update(ckb_transactions_count: addr.custom_ckb_transactions.count)
-    AddressTxsCacheUpdateWorker.perform_async(block.id)
-    max_count = ListCacheService::MAX_CACHED_PAGE * CkbTransaction::MAX_PAGINATES_PER
-    expected_txs = addr.custom_ckb_transactions.select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at).recent.limit(max_count).map(&:to_json)
-    assert_equal 500, $redis.zcard(addr.tx_list_cache_key)
-    assert_equal expected_txs, $redis.zrevrange(addr.tx_list_cache_key, 0, -1)
-  end
-
   test "address_txs_cache_update_worker should add new cache when there are records for the address" do
     Sidekiq::Testing.inline!
     block = create(:block, :with_block_hash)
