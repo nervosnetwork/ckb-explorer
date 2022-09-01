08eca5a259db16fc772630d41a370857e46d158f
ShiningRay <ShiningRay@users.noreply.github.com>
2022-09-01 08:21:44 +0800

    use static file serve for average block time

--- app/workers/average_block_time_generator.rb
+++ app/workers/average_block_time_generator.rb
@@ -5,5 +5,22 @@ class AverageBlockTimeGenerator
   def perform
     AverageBlockTimeByHour.refresh
     RollingAvgBlockTime.refresh
+    generate_cache
+  end
+
+  def generate_cache
+    content = {
+            data: {
+              id: Time.current.to_i,
+              type: "distribution_data",
+              attributes: {
+                average_block_time: RollingAvgBlockTime.all
+              }
+            }
+          }.to_json
+    path = Rails.root.join("public/api/v1/distribution_data")
+    FileUtils.mkdir_p path
+    filename = File.join(path, 'average_block_time')
+    IO.write filename, content, mode: 'w'
   end
 end
