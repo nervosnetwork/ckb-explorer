fcf59d8dea5ad4de1e8855cd7c5397d7bbfe652d
Yates <yates@magickbase.com>
2022-10-09 17:37:47 +0800

    removed useless column: addresses.address_hash_crc

--- db/migrate/20221009072146_change_index_type_for_addresses.rb
+++ db/migrate/20221009072146_change_index_type_for_addresses.rb
@@ -2,14 +2,20 @@ class ChangeIndexTypeForAddresses < ActiveRecord::Migration[6.1]
   def self.up
     remove_index :addresses, name: "index_addresses_on_address_hash_crc"
     remove_index :addresses, name: "index_addresses_on_lock_hash"
+
     add_index :addresses, :address_hash
     add_index :addresses, :lock_hash, using: 'hash'
+
+    remove_column :addresses, :address_hash_crc
   end
 
   def self.down
-    remove_index :addresses, name: "index_addresses_on_address_hash"
-    add_index :addresses, :address_hash_crc
+    add_column :addresses, :address_hash_crc, :bigint
+
     remove_index :addresses, name: "index_addresses_on_lock_hash"
+    remove_index :addresses, name: "index_addresses_on_address_hash"
+
     add_index :addresses, :lock_hash
+    add_index :addresses, :address_hash_crc
   end
 end
