1dae85d50e364c2ea1902fa935fa561dbb6e0c3a
Yates <yates@magickbase.com>
2022-10-10 17:00:35 +0800

    added missing unique constraint(which is not support by hash index)

--- db/migrate/20221009072146_change_index_type_for_addresses.rb
+++ db/migrate/20221009072146_change_index_type_for_addresses.rb
@@ -5,13 +5,16 @@ class ChangeIndexTypeForAddresses < ActiveRecord::Migration[6.1]
 
     add_index :addresses, :address_hash
     add_index :addresses, :lock_hash, using: 'hash'
+    execute "alter table public.addresses add constraint unique_lock_hash unique (lock_hash);"
 
     remove_column :addresses, :address_hash_crc
+
   end
 
   def self.down
     add_column :addresses, :address_hash_crc, :bigint
 
+    execute "alter table public.addresses drop constraint unique_lock_hash;"
     remove_index :addresses, name: "index_addresses_on_lock_hash"
     remove_index :addresses, name: "index_addresses_on_address_hash"
 
