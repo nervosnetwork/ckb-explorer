9d8471abffb4fb1f88df2a6bf6c4d45cf26364a5
Yates <yates@magickbase.com>
2022-10-25 10:48:41 +0800

    fixed issue#86: added test, mock http yml

--- app/workers/PoolTransactionCheckWorker.rb
+++ app/workers/PoolTransactionCheckWorker.rb
@@ -1,6 +1,6 @@
 class PoolTransactionCheckWorker
   include Sidekiq::Worker
-  @latest_json_rpc_id = 0
+  @@latest_json_rpc_id = 0
 
   # currently there's no such method in Ruby SDK, so let's use HTTP post request.
   def get_failed_reason options
@@ -16,6 +16,7 @@ class PoolTransactionCheckWorker
 
     res = HTTP.post(url, json: payload)
     data = JSON.parse res.to_s
+    Rails.logger.info "== in get_failed_reason, result: #{data.inspect}"
 
     status = data['result']['tx_status']['status']
     reason = ''
@@ -24,12 +25,12 @@ class PoolTransactionCheckWorker
     else
       reason = 'good'
     end
-    return {id: json_rpc_id, reason: reason}
+    return {id: options[:json_rpc_id], reason: reason.to_s, status: status}
   end
 
   def generate_json_rpc_id
-    @latest_json_rpc_id += 1
-    return @latest_json_rpc_id
+    @@latest_json_rpc_id += 1
+    return @@latest_json_rpc_id
   end
 
   def perform
@@ -37,22 +38,25 @@ class PoolTransactionCheckWorker
     PoolTransactionEntry.pool_transaction_pending.select(:id, :inputs, :created_at, :cell_deps).each do |tx|
 
       tx.inputs.each do |input|
-        reason = JSON.parse(get_failed_reason tx_id: input["previous_output"]["tx_hash"], json_rpc_id: generate_json_rpc_id)
-        if reason['reason'] == 'rejected'
-          pool_tx_entry_attributes << { id: tx.id, tx_status: "rejected", detailed_message: reason['reason'], created_at: tx.created_at, updated_at: Time.current }
+        reason = get_failed_reason tx_id: input["previous_output"]["tx_hash"], json_rpc_id: generate_json_rpc_id
+        if reason[:status] == 'rejected'
+          pool_tx_entry_attributes << { id: tx.id, tx_status: "rejected", detailed_message: reason[:reason], created_at: tx.created_at, updated_at: Time.current }
           break
         end
       end
       tx.cell_deps.each do |input|
-        reason = JSON.parse(get_failed_reason tx_id: input["out_point"]["tx_hash"], json_rpc_id: generate_json_rpc_id)
-        if reason['reason'] == 'rejected'
-          pool_tx_entry_attributes << { id: tx.id, tx_status: "rejected", detailed_message: reason['reason'], created_at: tx.created_at, updated_at: Time.current }
+        reason = get_failed_reason tx_id: input["out_point"]["tx_hash"], json_rpc_id: generate_json_rpc_id
+        if reason[:status] == 'rejected'
+          pool_tx_entry_attributes << { id: tx.id, tx_status: "rejected", detailed_message: reason[:reason], created_at: tx.created_at, updated_at: Time.current }
           break
         end
       end
     end
 
     pool_tx_entry_attributes = pool_tx_entry_attributes.group_by {|tx| tx[:id]}.map {|_, items| items[0]}.flatten
-    PoolTransactionEntry.upsert_all(pool_tx_entry_attributes) if pool_tx_entry_attributes.present?
+    if pool_tx_entry_attributes.present?
+      Rails.logger.info "== found rejected item, upsert_all: #{pool_tx_entry_attributes}"
+      PoolTransactionEntry.upsert_all(pool_tx_entry_attributes)
+    end
   end
 end
