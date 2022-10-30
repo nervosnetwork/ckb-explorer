395a3363416a26da63f0b94c1a4e38087cb0bfa5
ShiningRay <tsowly@hotmail.com>
2022-10-30 16:21:35 +0800

    optimize checking only check transaction reject reason after we find it rejected locally

--- app/workers/pool_transaction_check_worker.rb
+++ app/workers/pool_transaction_check_worker.rb
@@ -5,6 +5,7 @@ class PoolTransactionCheckWorker
 
   # currently there's no such method in Ruby SDK, so let's use HTTP post request.
   def get_failed_reason options
+    options[:json_rpc_id] ||= generate_json_rpc_id
     payload = {
       "id": options[:json_rpc_id],
       "jsonrpc": "2.0",
@@ -35,12 +36,50 @@ class PoolTransactionCheckWorker
   end
 
   def perform
-    PoolTransactionEntry.pool_transaction_pending.select(:id, :tx_hash, :inputs, :created_at, :cell_deps).find_each do |tx|
-      reason = get_failed_reason tx_id: tx.tx_hash, json_rpc_id: generate_json_rpc_id
-      if reason[:status] == 'rejected'
-        tx.update(tx_status: "rejected", detailed_message: reason[:reason])
+    pool_tx_entry_attributes = []
+    # Because iterating over all pool transaction entry record and get tx detail from CKB Node one by one 
+    # will make heavy load to CKB node, slowing block processing, sometimes will lead to HTTP timeout
+    # So here we directly check the inputs and dependencies of the transaction locally in database
+    # If any of the input or dependency cells is used, the transaction will never be valid.
+    # Thus we can directly mark this transaction rejected without requesting to CKB Node.
+    # Only request the CKB Node for reject reason after we find the transaction is rejeceted.
+    PoolTransactionEntry.pool_transaction_pending.select(:id, :tx_hash, :inputs, :created_at, :cell_deps).each do |tx|
+      rejected = nil
+      tx.inputs.each do |input|
+        if CellOutput.where(tx_hash: input["previous_output"]["tx_hash"], cell_index: input["previous_output"]["index"], status: "dead").exists?
+          rejected = { 
+            id: tx.id, 
+            tx_status: "rejected", 
+            created_at: tx.created_at, 
+            updated_at: Time.current 
+          }
+          break
+        end
       end
-      sleep 0.1
+      unless rejected
+        tx.cell_deps.each do |input|
+          if CellOutput.where(tx_hash: input["out_point"]["tx_hash"], cell_index: input["out_point"]["index"], status: "dead").exists?
+            rejected = { 
+              id: tx.id, 
+              tx_status: "rejected", 
+              created_at: tx.created_at, 
+              updated_at: Time.current 
+            }
+            rejected = true
+            break
+          end
+        end
+      end
+      if rejected
+        reason = get_failed_reason tx_id: tx.tx_hash
+        if reason[:status] == 'rejected'
+          rejected[:detailed_message] = reason[:reason]
+          pool_tx_entry_attributes << rejected
+        end
+      end
     end
+
+    pool_tx_entry_attributes.uniq!{|i| i[:id]}
+    PoolTransactionEntry.upsert_all(pool_tx_entry_attributes) if pool_tx_entry_attributes.present?
   end
 end
