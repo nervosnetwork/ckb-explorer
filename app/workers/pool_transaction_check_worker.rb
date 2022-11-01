8852a1c8b146944b18f9288b81c85f40477a5b6c
Yates <yates@magickbase.com>
2022-11-02 07:44:29 +0800

    rename: rejected_cell_output -> rejected_transaction

--- app/workers/pool_transaction_check_worker.rb
+++ app/workers/pool_transaction_check_worker.rb
@@ -12,10 +12,10 @@ class PoolTransactionCheckWorker
     # Only request the CKB Node for reject reason after we find the transaction is rejeceted.
     PoolTransactionEntry.pool_transaction_pending.select(:id, :tx_hash, :inputs, :created_at, :cell_deps).find_each do |tx|
       is_rejected = false
-      rejected_cell_output = nil
+      rejected_transaction = nil
       tx.inputs.each do |input|
         if CellOutput.where(tx_hash: input["previous_output"]["tx_hash"], cell_index: input["previous_output"]["index"], status: "dead").exists?
-          rejected_cell_output = {
+          rejected_transaction = {
             id: tx.id,
             tx_status: "rejected",
             created_at: tx.created_at,
@@ -28,7 +28,7 @@ class PoolTransactionCheckWorker
       unless is_rejected
         tx.cell_deps.each do |input|
           if CellOutput.where(tx_hash: input["out_point"]["tx_hash"], cell_index: input["out_point"]["index"], status: "dead").exists?
-            rejected_cell_output = {
+            rejected_transaction = {
               id: tx.id,
               tx_status: "rejected",
               created_at: tx.created_at,
@@ -40,12 +40,12 @@ class PoolTransactionCheckWorker
         end
       end
       if is_rejected
-        pool_tx_entry_attributes << rejected_cell_output
+        pool_tx_entry_attributes << rejected_transaction
         response_string = CkbSync::Api.instance.directly_single_call_rpc method: 'get_transaction', params: [tx.tx_hash]
         reason = response_string['result']['tx_status']
 
         if reason['status'] == 'rejected'
-          rejected_cell_output[:detailed_message] = reason['reason']
+          rejected_transaction[:detailed_message] = reason['reason']
         end
       end
     end
