1a2828c1bdd23e59b4f69d79d897dc4e84cbaa39
yates <iamyates@sina.com>
2022-11-24 13:25:03 +0800

    add test

---
+++ test/controllers/api/v2/dao_events_controller_test.rb
@@ -0,0 +1,29 @@
+require "test_helper"
+
+module Api
+  module V2
+    class DasAccountsControllerTest < ActionDispatch::IntegrationTest
+      def setup
+        super
+        @block = create(:block)
+        @address = create(:address)
+        @ckb_transaction = create(:ckb_transaction, block: @block)
+        @dao_contract = create(:dao_contract)
+      end
+      test "should return corresponding address alias" do
+
+        create(:dao_event_with_block, block_id: @block.id, contract_id: @dao_contract.id, address_id: @address.id,
+                            ckb_transaction_id: @ckb_transaction.id,
+                            event_type: :deposit_to_dao, value: 10000
+              )
+        get api_v2_dao_events_url, params: {address: @address.address_hash}
+
+        assert_response :success
+        data = JSON.parse response.body
+        puts "data: #{data.inspect}"
+        activity = data["data"]['attributes']['activities'].first
+        assert_equal activity['type'], "deposit_to_dao"
+      end
+    end
+  end
+end
