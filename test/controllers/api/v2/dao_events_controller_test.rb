78a39583f468ce07e7d431d010b7da3071931e0c
iamyates <iamyates@sina.com>
2022-11-28 20:20:25 +0800

    fixed:  Add an API to fetch nervos dao records by a specific address … (#1071)
    * fixed:  Add an API to fetch nervos dao records by a specific address #110
    
    * add test
    
    * fixed:  Add an API to fetch nervos dao records by a specific address, add field: to
    
    * fixed failed unit test
    
    * removed from field
    
    * added pagination, fixed "from" issue
    
    * 1. removed redundant file 2. query address by lock hash 3. add more test cases

---
+++ test/controllers/api/v2/dao_events_controller_test.rb
@@ -0,0 +1,60 @@
+require "test_helper"
+
+module Api
+  module V2
+    class DaoEventsControllerTest < ActionDispatch::IntegrationTest
+      def setup
+        super
+        @block = create(:block)
+        @address = create(:address)
+        @ckb_transaction = create(:ckb_transaction, block: @block)
+        @dao_contract = create(:dao_contract)
+        @amount = 10000
+      end
+      test "should get index, deposit_to_dao" do
+
+        create(:dao_event_with_block, block_id: @block.id, contract_id: @dao_contract.id, address_id: @address.id,
+                            ckb_transaction_id: @ckb_transaction.id,
+                            event_type: :deposit_to_dao, value: @amount
+              )
+        get api_v2_dao_events_url, params: {address: @address.address_hash}
+
+        assert_response :success
+        data = JSON.parse response.body
+        activity = data["data"]['activities'].first
+        assert_equal activity['type'], "deposit_to_dao"
+        assert_equal activity['amount'], @amount.to_s
+      end
+
+      test "should get index with 'withdraw_from_dao'" do
+
+        create(:dao_event_with_block, block_id: @block.id, contract_id: @dao_contract.id, address_id: @address.id,
+                            ckb_transaction_id: @ckb_transaction.id,
+                            event_type: :withdraw_from_dao, value: @amount
+              )
+        get api_v2_dao_events_url, params: {address: @address.address_hash}
+
+        assert_response :success
+        data = JSON.parse response.body
+        activity = data["data"]['activities'].first
+        assert_equal activity['type'], "withdraw_from_dao"
+        assert_equal activity['amount'], @amount.to_s
+      end
+
+      test "should get index with 'issue_interest'" do
+
+        create(:dao_event_with_block, block_id: @block.id, contract_id: @dao_contract.id, address_id: @address.id,
+                            ckb_transaction_id: @ckb_transaction.id,
+                            event_type: :issue_interest, value: @amount
+              )
+        get api_v2_dao_events_url, params: {address: @address.address_hash}
+
+        assert_response :success
+        data = JSON.parse response.body
+        activity = data["data"]['activities'].first
+        assert_equal activity['type'], "issue_interest"
+        assert_equal activity['amount'], @amount.to_s
+      end
+    end
+  end
+end
