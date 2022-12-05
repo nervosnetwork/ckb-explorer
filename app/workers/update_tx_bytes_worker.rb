59e424e4a47aa3197e4254322f5df3faaf9a7e12
ShiningRay <tsowly@hotmail.com>
2022-12-05 09:02:21 +0800

    unify 4 bytes in db

--- app/workers/update_tx_bytes_worker.rb
+++ app/workers/update_tx_bytes_worker.rb
@@ -5,7 +5,7 @@ class UpdateTxBytesWorker
   def perform(tx_id)
     tx = CkbTransaction.find tx_id
     res = CkbSync::Api.instance.directly_single_call_rpc(method: :get_transaction, params: [tx.tx_hash, "0x0"])
-    tx.bytes = (res["result"]["transaction"].size - 2) / 2
+    tx.bytes = (res["result"]["transaction"].size - 2) / 2 + 4
     tx.save
   end
 end
