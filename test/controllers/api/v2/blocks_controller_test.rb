c3a378bd1c6b4a71a82f2cc2c714c8d10d63c1b3
Yates <yates@magickbase.com>
2022-12-31 09:55:06 +0800

    fixed failed unit test

--- test/controllers/api/v2/blocks_controller_test.rb
+++ test/controllers/api/v2/blocks_controller_test.rb
@@ -6,6 +6,8 @@ module Api
       def setup
         super
         block1 = create(:block, block_hash: '001', miner_message: "0x22302e3130332e3020286537373133386520323032322d30342d31312920346436393665363536343432373935363639363134323534343322", number: 1, timestamp: 1.days.ago.to_i * 1000)
+
+        # miner_message == '0.101.3'
         block2 = create(:block, block_hash: '002', miner_message: "0x22302e3130312e332028376338393031382d646972747920323032312d31333232643331333432393230303030303030303022", number: 2, timestamp: 1.days.ago.to_i * 1000)
         block3 = create(:block, block_hash: '003', miner_message: "0x22302e3130312e332028376338393031382d646972747920323032312d31333232643331333432393230303030303030303022", number: 3, timestamp: 1.days.ago.to_i * 1000)
         block4 = create(:block, block_hash: '004', miner_message: "0x22302e3130312e332028376338393031382d646972747920323032312d31333232643331333432393230303030303030303022", number: 4, timestamp: 1.days.ago.to_i * 1000)
@@ -17,8 +19,13 @@ module Api
         get ckb_node_versions_api_v2_blocks_url
         data = JSON.parse(response.body)['data']
         assert_equal 2, data.size
-        assert_equal data[0]['version'], '0.101.3'
-        assert_equal data[0]['blocks_count'], 4
+
+        data.each do |e|
+          if e['version'] == '0.101.3'
+            assert_equal 4, e['blocks_count']
+          end
+        end
+
       end
     end
   end
