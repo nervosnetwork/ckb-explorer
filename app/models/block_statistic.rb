4f0aed9cfa2d1881e3235e15842a54e8a86ac386
ShiningRay <tsowly@hotmail.com>
2023-02-07 00:14:03 +0800

    add more fields parsed from dao header

--- app/models/block_statistic.rb
+++ app/models/block_statistic.rb
@@ -2,6 +2,7 @@ class BlockStatistic < ApplicationRecord
   VALID_INDICATORS = %w(difficulty hash_rate live_cells_count dead_cells_count).freeze
   belongs_to :block, foreign_key: :block_number, primary_key: :number
   delegate :block_hash, to: :block
+
   CkbToShannon = 10**8
   def reset_primary_issuance
     val = get_block_economic_state&.issuance&.primary || 0
@@ -17,11 +18,40 @@ class BlockStatistic < ApplicationRecord
     @res ||= CkbSync::Api.instance.get_block_economic_state(block_hash)
   end
 
+  # has '0x' prefix
+  def dao_header
+    block.dao
+  end
+
+  def reset_total_issuance
+    self.total_issuance = (dao_header[2, 16]&.hex || 0).to_d / CkbToShannon
+  end
+
+  def reset_accumulated_rate
+    self.accumulated_rate = (dao_header[18, 16]&.hex || 0).to_d / CkbToShannon
+  end
+
+  def reset_unissued_secondary_issuance
+    self.unissued_secondary_issuance = (dao_header[34, 16]&.hex || 0).to_d / CkbToShannon
+  end
+
+  def reset_total_occupied_capacities
+    self.total_occupied_capacities = (dao_header[50, 16]&.hex || 0).to_d / CkbToShannon
+  end
+
+  def reset_all
+    reset_primary_issuance
+    reset_secondary_issuance
+    reset_total_issuance
+    reset_accumulated_rate
+    reset_unissued_secondary_issuance
+    reset_total_occupied_capacities
+  end
+
   def self.full_reset
     find_each do |s|
       puts s.block_number
-      s.reset_primary_issuance
-      s.reset_secondary_issuance
+      s.reset_all
       s.save
     end
   end
