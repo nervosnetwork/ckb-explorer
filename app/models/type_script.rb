f2461ad87d61c145dcbc9897cdcefc8c913db3ee
imyates <iamyates@sina.com>
2023-02-11 10:15:38 +0800

    fixed slow query: lock_script.ckb_transactions

--- app/models/type_script.rb
+++ app/models/type_script.rb
@@ -1,12 +1,12 @@
 class TypeScript < ActiveRecord::Base
   has_many :cell_outputs
-  has_many :ckb_transactions
 
   belongs_to :cell_output, optional: true # will remove this later
+  before_validation :generate_script_hash
+
   belongs_to :script, optional: true
   belongs_to :contract, optional: true, primary_key: "code_hash", foreign_key: "code_hash"
 
-  before_validation :generate_script_hash
   validates_presence_of :code_hash
   attribute :code_hash, :ckb_hash
 
