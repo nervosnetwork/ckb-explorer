00d1cfb8693e9a1279ff7420f9c631dfe9c5006f
imyates <iamyates@sina.com>
2023-02-20 09:04:31 +0800

    improved the CellDependency.create_from_script method

--- app/models/cell_dependency.rb
+++ app/models/cell_dependency.rb
@@ -2,24 +2,33 @@
 # referring cell
 class CellDependency < ActiveRecord::Base
 
-  belongs_to :contract
+  belongs_to :contract, optional: true
   belongs_to :ckb_transaction
   belongs_to :cell_output, foreign_key: "contract_cell_id", class_name: "CellOutput"
 
   # the_scripts:  type_scripts or lock_scripts
-  # Usage:
-  # bundle exec rails c
-  # rails > CellDependency.create_from_scripts TypeScript.all
-  # rails > CellDependency.create_from_scripts LockScript.all
+  # notice :
+  # 1. stop the syncer process
+  # 2. record the last TypeScript id(e.g. 666), and last LockScript id(e.g. 888)
+  # 3. start the latest syncer process : bundle exec ruby lib/ckb_block_node_processor.rb
+  # 3. run these methods:
+  #    CellDependency.create_from_scripts TypeScript.where('id <= ?', 666)
+  #    CellDependency.create_from_scripts LockScript.where('id <= ?', 888)
   def self.create_from_scripts the_scripts
+    CellDependency.delete_all
     the_scripts.find_each do |the_script|
-      CkbTransaction.transaction do
-        next if the_script.ckb_transactions.blank?
-        the_script.ckb_transactions.each do |ckb_transaction|
-          next if ckb_transaction.cell_outputs.blank?
-          ckb_transaction.cell_outputs.each do |cell_output|
-            CellDependency.create contract_id: the_script.script.contract_id, ckb_transaction_id: ckb_transaction.id, contract_cell_id: cell_output.id
+      Rails.logger.info "== processing the_script: #{the_script.id}"
+      next if the_script.ckb_transactions.blank?
+
+      the_script.ckb_transactions.each do |ckb_transaction|
+        next if ckb_transaction.cell_outputs.blank?
+        ckb_transaction.cell_outputs.each do |cell_output|
+          the_hash = { ckb_transaction_id: ckb_transaction.id, contract_cell_id: cell_output.id, script_id: the_script.script_id }
+          contract_id = the_script.script.contract_id
+          if contract_id.present?
+            the_hash.merge contract_id: contract_id
           end
+          CellDependency.create! the_hash
         end
       end
     end
@@ -38,4 +47,5 @@ end
 #  contract_cell_id   :bigint
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
+#  script_id          :bigint
 #
--- app/models/referring_cell.rb
+++ app/models/referring_cell.rb
@@ -30,10 +30,10 @@ end
 #
 # Table name: referring_cells
 #
-#  id                   :bigint           not null, primary key
-#  cell_output_id       :bigint
-#  contract_id          :bigint
-#  ckb_transaction_id   :bigint
-#  created_at           :datetime         not null
-#  updated_at           :datetime         not null
+#  id                 :bigint           not null, primary key
+#  cell_output_id     :bigint
+#  contract_id        :bigint
+#  ckb_transaction_id :bigint
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
 #
