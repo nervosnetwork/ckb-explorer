08fbb769db6b068eb234ddedc43370d72bb2487d
imyates <iamyates@sina.com>
2023-02-25 18:46:03 +0800

    fixed bug: migration class name is not correct

--- db/migrate/20230221025839_add_indexes_to_udt_address_transactions.rb
+++ db/migrate/20230221025839_add_indexes_to_udt_address_transactions.rb
@@ -1,4 +1,4 @@
-class AddIndexesToDaoAddressTransactionsAndUdtAddressTransactions < ActiveRecord::Migration[7.0]
+class AddIndexesToUdtAddressTransactions < ActiveRecord::Migration[7.0]
   def change
     add_index :udt_address_transactions, :ckb_transaction_id
     add_index :udt_address_transactions, [:udt_address_id, :ckb_transaction_id], name: 'index_udt_address_transactions_on_tx_id_and_udt_address_id'
