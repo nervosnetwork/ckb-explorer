4a901a6c04efb573ba759fdf799467bd973fcb81
ShiningRay <tsowly@hotmail.com>
2023-03-20 14:40:13 +0800

    remove duplicate records

--- db/migrate/20230319164714_change_cell_dependency_columns.rb
+++ db/migrate/20230319164714_change_cell_dependency_columns.rb
@@ -2,6 +2,12 @@ class ChangeCellDependencyColumns < ActiveRecord::Migration[7.0]
   def change
     change_column_null :cell_dependencies, :contract_cell_id, false
     change_column_null :cell_dependencies, :ckb_transaction_id, false
+    execute <<-SQL
+      DELETE FROM cell_dependencies
+      WHERE  id NOT IN (SELECT Min(id)
+                  FROM   cell_dependencies
+                  GROUP  BY ckb_transaction_id, contract_cell_id)
+    SQL
     add_index :cell_dependencies, [:ckb_transaction_id, :contract_cell_id], unique: true
   end
 end
