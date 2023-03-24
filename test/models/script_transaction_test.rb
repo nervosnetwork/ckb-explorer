bd81d1905fdc3817febe139cb0d6ab174dbf13d7
iamyates <iamyates@sina.com>
2023-03-24 12:32:07 +0800

    fixed failed unit test: test/models/script_transaction_test.rb

--- test/models/script_transaction_test.rb
+++ test/models/script_transaction_test.rb
@@ -27,6 +27,7 @@ class ScriptTransactionTest < ActiveSupport::TestCase
 
   test "it should create_initial_data" do
     TypeScript.delete_all
+    LockScript.delete_all
     Script.delete_all
     ScriptTransaction.delete_all
     Block.delete_all
@@ -41,7 +42,6 @@ class ScriptTransactionTest < ActiveSupport::TestCase
     CkbTransaction.where('id > ?', ckb_transaction.id).delete_all
     script = create :script
     type_script = create :type_script, code_hash: code_hash, hash_type: hash_type, script_id: script.id
-    LockScript.delete_all
     lock_script = create :lock_script, code_hash: code_hash, hash_type: hash_type, script_id: script.id
     cell_output = create :cell_output, :with_full_transaction, block_id: block.id
     cell_output.update lock_script_id: lock_script.id, type_script_id: type_script.id, ckb_transaction_id: ckb_transaction.id
@@ -49,14 +49,13 @@ class ScriptTransactionTest < ActiveSupport::TestCase
     # for the 1st time, it will create
     ScriptTransaction.create_initial_data
     @script_transaction = ScriptTransaction.first
-    assert_equal 2, ScriptTransaction.all.count
+    assert_equal 1, ScriptTransaction.all.count
     assert_equal ckb_transaction.id, @script_transaction.ckb_transaction_id
     assert_equal script.id, @script_transaction.script_id
 
     # for the 2nd time, it should NOT create new record
     ScriptTransaction.create_initial_data
-    assert_equal 4, ScriptTransaction.all.count
-
+    assert_equal 1, ScriptTransaction.all.count
   end
 
 end
