aabf703426d09242c0ba9c03b8dbe1c1165b3d93
ShiningRay <tsowly@hotmail.com>
2023-04-03 15:55:15 +0800

    fix increment id

---
+++ db/migrate/20230403052005_reset_ckb_transaction_auto_increment_value.rb
@@ -0,0 +1,6 @@
+class ResetCkbTransactionAutoIncrementValue < ActiveRecord::Migration[7.0]
+  def change
+    max_ckb_tx_id = CkbTransaction.maximum(:id)
+    ActiveRecord::Base.connection.execute("ALTER SEQUENCE ckb_transactions_id_seq RESTART WITH #{max_ckb_tx_id + 1}")
+  end
+end
