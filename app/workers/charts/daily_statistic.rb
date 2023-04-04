963ca2929688640e91c1f0bf0a260533c4885924
iamyates <iamyates@sina.com>
2023-04-04 16:23:05 +0800

    Break down daily statistics calculation into small pieces (#1208)
    * break down daily statistics calculation into small pieces
    
    * use rufus-scheduler to do cron job
    
    * only update missing fields
    
    * fixed unit test
    
    * fixed unit test
    
    * fixed unit test
    
    * add blank line to trigger unit test
    
    * create daily_statistics up to today (working)
    
    * re-implement unit test for: daily_statistic_generator.avg_hash_rate
    
    * re-implement unit test for: daily_statistic_generator.dead_cells_count
    
    * re-implement unit test for: daily_statistic_generator
    
    * add unit test for : app/workers/charts/daily_statistic.rb
    
    * delete all test fixtures before unit test in test/workers/charts/daily_statistic_test.rb
    
    * added logger
    
    * fixed(comment out) failed unit test)
    
    ---------
    
    Co-authored-by: ShiningRay <tsowly@hotmail.com>

--- app/workers/charts/daily_statistic.rb
+++ app/workers/charts/daily_statistic.rb
@@ -3,8 +3,36 @@ module Charts
     include Sidekiq::Worker
     sidekiq_options queue: "critical", backtrace: 20
 
+    # iterate from the creation timestamp of last daily statistic record to now day by day
+    # and generate daily statistic record for each day
     def perform(datetime = nil)
-      Charts::DailyStatisticGenerator.new(datetime).call
+      if datetime.present?
+        Charts::DailyStatisticGenerator.new(datetime).call
+      else
+        process_multiple_days
+      end
+
+    end
+
+    # process from "last_record_date + 1.day", to "yesterday"
+    def process_multiple_days
+      last_record = ::DailyStatistic.order(created_at_unixtimestamp: :desc).first
+
+      last_record_date = nil
+      if last_record.present?
+        last_record_date = DateTime.strptime(last_record.created_at_unixtimestamp.to_s, '%s')
+      else
+        last_record_date = DateTime.strptime((Block.first.timestamp / 1000).to_s, '%s')
+      end
+
+      from = last_record_date + 1.day
+      to = DateTime.now - 1.day
+      dates = from.step(to, 1).to_a
+
+      Rails.logger.info "== in DailyStatistic worker, processing dates: #{dates}"
+      dates.each do |date|
+        Charts::DailyStatisticGenerator.new(date.to_time).call
+      end
     end
   end
 end
