91665fafd06b8ca3c58e5d770488d38a312f8ce4
iamyates <iamyates@sina.com>
2023-04-20 10:02:02 +0800

    add fast page to api/v2 (#1239)
    * add fast page to api/v2
    
    * add fast_page to pagy
    
    * fixed failed unit tests

--- app/controllers/api/v2/nft/holders_controller.rb
+++ app/controllers/api/v2/nft/holders_controller.rb
@@ -7,7 +7,7 @@ module Api
           return head(:not_found)
         end
         @counts = @collection.items.joins(:owner).group(:address_hash).count
-        # @pagy, @addresses = pagy(scope)
+        # @pagy, @addresses = pagy(scope).fast_page
         render json: {
           data: @counts
           # pagination: pagy_metadata(@pagy)
