a04ddedc6e6ae4eea9dc9a4f411c39959b77c141
ShiningRay <tsowly@hotmail.com>
2023-04-20 09:23:33 +0800

    optimize docs

--- app/models/average_block_time_by_hour.rb
+++ app/models/average_block_time_by_hour.rb
@@ -1,7 +1,7 @@
 # This is a materialized view
 # The definition refers to db/migrate/20220705003300_create_average_block_time_by_hour_view.rb
 class AverageBlockTimeByHour < ApplicationRecord
-  self.table_name = 'average_block_time_by_hour'
+  self.table_name = "average_block_time_by_hour"
   def self.refresh
     connection.execute "refresh materialized view CONCURRENTLY average_block_time_by_hour "
   end
--- app/models/rolling_avg_block_time.rb
+++ app/models/rolling_avg_block_time.rb
@@ -1,7 +1,7 @@
 # This is a materialized view
 # The definition refers to db/migrate/20220705003300_create_average_block_time_by_hour_view.rb
 class RollingAvgBlockTime < ApplicationRecord
-  self.table_name = 'rolling_avg_block_time'
+  self.table_name = "rolling_avg_block_time"
   def self.refresh
     connection.execute "refresh materialized view CONCURRENTLY rolling_avg_block_time "
   end
