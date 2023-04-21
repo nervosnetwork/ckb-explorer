4576193dd9cd6db77c66d75af2975206aab74207
iamyates <iamyates@sina.com>
2023-04-21 12:13:39 +0800

    added sort to address transactions

--- app/controllers/api/v1/address_transactions_controller.rb
+++ app/controllers/api/v1/address_transactions_controller.rb
@@ -9,7 +9,12 @@ module Api
         raise Api::V1::Exceptions::AddressNotFoundError if @address.is_a?(NullAddress)
 
         @tx_ids = AccountBook.where(address_id: @address.id).order("ckb_transaction_id" => :desc).select("ckb_transaction_id").page(@page).per(@page_size).fast_page
-        @ckb_transactions = CkbTransaction.tx_committed.where(id: @tx_ids.map(&:ckb_transaction_id)).select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at).order(id: :desc)
+        @ckb_transactions = CkbTransaction.tx_committed.where(id: @tx_ids.map(&:ckb_transaction_id))
+          .select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at, :capacity_involved)
+        asc_or_desc = params[:asc_or_desc] || 'desc'
+        @ckb_transactions = @ckb_transactions.order(id: asc_or_desc) if params[:sort_by].blank? || params[:sort_by] == 'time'
+        @ckb_transactions = @ckb_transactions.order(capacity_involved: asc_or_desc) if params[:send_volume]
+        @ckb_transactions = @ckb_transactions.order(capacity_involved: asc_or_desc) if params[:received_volume]
         json =
           Rails.cache.realize("#{@ckb_transactions.cache_key}/#{@address.query_address}", version: @ckb_transactions.cache_version) do
             @options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: @ckb_transactions, page: @page, page_size: @page_size, records_counter: @tx_ids).call
