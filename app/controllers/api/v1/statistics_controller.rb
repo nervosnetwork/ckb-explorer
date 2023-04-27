7736e57b25cb7c31f9e102dbe1122d206ea7a67c
ShiningRay <tsowly@hotmail.com>
2023-04-27 16:28:43 +0800

    optimize query for frequently used query

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -2,9 +2,9 @@ module Api
   module V1
     class StatisticsController < ApplicationController
       before_action :validate_query_params, only: :show
-
+      NOT_USED_COLUMNS = %w(last_n_days_transaction_fee_rates pending_transaction_fee_rates transaction_fee_rates)
       def index
-        statistic_info = StatisticInfo.default
+        statistic_info = StatisticInfo.select(StatisticInfo.column_names - NOT_USED_COLUMNS).default
         if stale?(public: true)
           expires_in 15.seconds, public: true, must_revalidate: true, stale_while_revalidate: 5.seconds
           render json: IndexStatisticSerializer.new(statistic_info)
@@ -12,7 +12,7 @@ module Api
       end
 
       def show
-        statistic_info = StatisticInfo.default
+        statistic_info = StatisticInfo.select(StatisticInfo.column_names - NOT_USED_COLUMNS).default
         render json: StatisticSerializer.new(statistic_info, params: { info_name: params[:id] })
       end
 
