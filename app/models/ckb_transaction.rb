27f293558ad84e057c8fff69bfd9cbb78b22e2eb
ShiningRay <tsowly@hotmail.com>
2023-05-18 22:24:37 +0800

    fix

--- app/models/ckb_transaction.rb
+++ app/models/ckb_transaction.rb
@@ -259,10 +259,15 @@ class CkbTransaction < ApplicationRecord
       cell_outputs_for_display = cell_outputs_for_display[0, 10]
     end
     cell_outputs_for_display.map do |output|
-      consumed_tx_hash = output.live? ? nil : output.consumed_by.tx_hash
+      consumed_tx_hash = output.live? ? nil : output.consumed_by&.tx_hash
       display_output = {
-        id: output.id, capacity: output.capacity, address_hash: output.address_hash,
-        status: output.status, consumed_tx_hash: consumed_tx_hash, cell_type: output.cell_type }
+        id: output.id,
+        capacity: output.capacity,
+        address_hash: output.address_hash,
+        status: output.status,
+        consumed_tx_hash: consumed_tx_hash,
+        cell_type: output.cell_type
+      }
       display_output.merge!(attributes_for_udt_cell(output)) if output.udt?
       display_output.merge!(attributes_for_m_nft_cell(output)) if output.cell_type.in?(%w(
                                                                                          m_nft_issuer m_nft_class
