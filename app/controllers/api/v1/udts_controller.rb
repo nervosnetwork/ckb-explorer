b8bef1b41745e06812bc8b9345a301e11e47f222
iamyates <iamyates@sina.com>
2023-05-29 09:50:18 +0800

    improved code

--- app/controllers/api/v1/udts_controller.rb
+++ app/controllers/api/v1/udts_controller.rb
@@ -9,8 +9,11 @@ class Api::V1::UdtsController < ApplicationController
     udts = udts.order(addresses_count: :asc) if params[:addresses_count_asc].present?
     udts = udts.order(h24_ckb_transactions_count: :asc) if params[:h24_ckb_transactions_count_asc].present?
     udts = udts.order(h24_ckb_transactions_count: :desc) if params[:h24_ckb_transactions_count_desc].present?
-    udts = udts.order(block_timestamp: :asc) if params[:created_at_asc].present?
-    udts = udts.order(block_timestamp: :desc) if !params[:created_at_asc].present?
+    if params[:created_at_asc].present?
+      udts = udts.order(block_timestamp: :asc)
+    else
+      udts = udts.order(block_timestamp: :desc)
+    end
     udts = udts.page(@page).per(@page_size).fast_page
     options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: udts, page: @page, page_size: @page_size).call
     render json: UdtSerializer.new(udts, options)
