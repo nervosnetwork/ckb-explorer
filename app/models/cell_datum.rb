5e8583a2dab236633806b47cdfd5cce73dc6144a
ShiningRay <tsowly@hotmail.com>
2023-06-15 11:52:34 +0800

    fix

--- app/models/cell_datum.rb
+++ app/models/cell_datum.rb
@@ -1,16 +1,15 @@
 class CellDatum < ApplicationRecord
   self.primary_key = :cell_output_id
   belongs_to :cell_output, primary_key: :id, inverse_of: :cell_datum
-  validates :data, presence: true, length: { minimum: 1 }
+  validates :data, length: { minimum: 1 }
 
-  after_save :update_data_hash, :update_data_size
+  after_save :update_cell_data_hash_and_size
 
-  def update_data_hash
-    cell_output.update(data_hash: CKB::Utils.bin_to_hex(CKB::Blake2b.digest(data)))
-  end
-
-  def update_data_size
-    cell_output.update(data_size: data.bytesize)
+  def update_cell_data_hash_and_size
+    cell_output.update_columns(
+      data_hash: CKB::Utils.bin_to_hex(CKB::Blake2b.digest(data)),
+      data_size: data.bytesize
+    )
   end
 
   def hex_data
--- db/migrate/20230603124843_copy_cell_data.rb
+++ db/migrate/20230603124843_copy_cell_data.rb
@@ -1,8 +1,9 @@
 class CopyCellData < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
   def change
-    CellOutput.where.not(data: nil).where.not(data: "0x").find_each do |c|
-      d = c.cell_datum
-      d.update data: CKB::Utils.hex_to_bin(c.data)
+    CellOutput.where("data is not null and length(data) > 2").find_each do |c|
+      d = c.cell_datum || c.build_cell_datum
+      d.update! data: CKB::Utils.hex_to_bin(c[:data])
     end
   end
 end
