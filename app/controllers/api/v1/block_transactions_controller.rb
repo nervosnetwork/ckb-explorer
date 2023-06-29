cc4d375add07de354c05a2f1cf175ee1fb835af1
NanZhang <zhangnan8710@gmail.com>
2023-06-29 16:34:58 +0800

    feat: filter transactions by address hash (#1319)

--- app/controllers/api/v1/block_transactions_controller.rb
+++ app/controllers/api/v1/block_transactions_controller.rb
@@ -1,31 +1,40 @@
 module Api
   module V1
     class BlockTransactionsController < ApplicationController
-      before_action :validate_query_params
-      before_action :validate_pagination_params, :pagination_params
-      include Pagy::Backend
+      before_action :validate_query_params, :validate_pagination_params, :pagination_params
+
       def show
         block = Block.find_by!(block_hash: params[:id])
-        temp_transactions = block.ckb_transactions
-          .select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at)
-          .where(block_timestamp: block.timestamp)
-        temp_transactions = temp_transactions.where(tx_hash: params[:tx_hash]) if params[:tx_hash].present?
-        temp_transactions = temp_transactions.order(id: :desc)
+        ckb_transactions = block.ckb_transactions.
+          select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at).
+          order(:id)
 
-        @pagy, ckb_transactions = pagy(
-          temp_transactions,
-          items: params[:page_size] || 10,
-          overflow: :empty_page
-        )
+        if params[:tx_hash].present?
+          ckb_transactions = ckb_transactions.where(tx_hash: params[:tx_hash])
+        end
 
-        json =
-          Rails.cache.realize(ckb_transactions.cache_key, version: ckb_transactions.cache_version) do
-            records_counter = RecordCounters::BlockTransactions.new(block)
-            options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: ckb_transactions, page: @pagy.page, page_size: @pagy.items, records_counter: records_counter).call
-            CkbTransactionsSerializer.new(ckb_transactions, options.merge(params: { previews: true })).serialized_json
-          end
+        if params[:address_hash].present?
+          address = Address.find_address!(params[:address_hash])
+          ckb_transactions = ckb_transactions.joins(:account_books).
+            where(account_books: { address_id: address.id })
+        end
 
-        render json: json
+        if stale?(ckb_transactions)
+          expires_in 10.seconds, public: true, must_revalidate: true, stale_while_revalidate: 5.seconds
+          records_counter = RecordCounters::BlockTransactions.new(ckb_transactions)
+          ckb_transactions = ckb_transactions.page(@page).per(@page_size).fast_page
+          options = FastJsonapi::PaginationMetaGenerator.new(
+            request: request,
+            records: ckb_transactions,
+            page: @page,
+            page_size: @page_size,
+            records_counter: records_counter
+          ).call
+          json = CkbTransactionsSerializer.new(ckb_transactions,
+                                               options.merge(params: { previews: true })).serialized_json
+
+          render json: json
+        end
       rescue ActiveRecord::RecordNotFound
         raise Api::V1::Exceptions::BlockTransactionsNotFoundError
       end
