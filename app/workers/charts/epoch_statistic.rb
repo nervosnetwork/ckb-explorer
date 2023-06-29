975621ac18dab408ae5a054744dd25cec4759c28
zhangnan <zhangnan8710@gmail.com>
2023-06-29 17:44:54 +0800

    refactor: backend epoch statistics

--- app/workers/charts/epoch_statistic.rb
+++ app/workers/charts/epoch_statistic.rb
@@ -6,10 +6,7 @@ module Charts
     def perform
       latest_epoch_number = ::EpochStatistic.order(epoch_number: :desc).pick(:epoch_number) || 0
       target_epoch_number = latest_epoch_number + 1
-      block_in_target_epoch = Block.where(epoch: target_epoch_number).first
-      if block_in_target_epoch.present? && Block.where(epoch: target_epoch_number).count == block_in_target_epoch.length
-        Charts::EpochStatisticGenerator.new(target_epoch_number).call
-      end
+      Charts::EpochStatisticGenerator.new(target_epoch_number).call
     end
   end
 end
