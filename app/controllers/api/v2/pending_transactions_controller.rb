512f5d5508adf5c27a28b984d4a6744d93b4a83a
zhangnan <zhangnan8710@gmail.com>
2023-07-04 17:13:23 +0800

    refactor: pending transactions sort

--- app/controllers/api/v2/pending_transactions_controller.rb
+++ app/controllers/api/v2/pending_transactions_controller.rb
@@ -1,53 +1,66 @@
-module Api::V2
-  class PendingTransactionsController < BaseController
-    before_action :set_page_and_page_size
-    def index
-      pending_transactions = CkbTransaction.tx_pending
+module Api
+  module V2
+    class PendingTransactionsController < BaseController
+      before_action :set_page_and_page_size
 
-      params[:sort] ||= "id.desc"
-      order_by, asc_or_desc = params[:sort].split(".", 2)
-      order_by =
-        case order_by
-             when "time"
-               "created_at"
-             when "fee"
-               "transaction_fee"
-             # current we don't support this in DB
-             # when 'capacity' then 'capacity_involved'
-             else order_by
+      def index
+        pending_transactions = CkbTransaction.tx_pending
+
+        if stale?(pending_transactions)
+          expires_in 10.seconds, public: true, must_revalidate: true, stale_while_revalidate: 5.seconds
+
+          total_count = pending_transactions.count
+          pending_transactions = sort_transactions(pending_transactions).
+            page(@page).per(@page_size).fast_page
+
+          json = {
+            data: pending_transactions.map do |tx|
+              {
+                transaction_hash: tx.tx_hash,
+                capacity_involved: tx.capacity_involved,
+                transaction_fee: tx.transaction_fee,
+                created_at: tx.created_at,
+                create_timestamp: (tx.created_at.to_f * 1000).to_i
+              }
+            end,
+            meta: {
+              total: total_count,
+              page_size: @page_size.to_i
+            }
+          }
+          render json: json
+        end
+      end
+
+      def count
+        render json: {
+          data: CkbTransaction.tx_pending.count
+        }
+      end
+
+      private
+
+      def set_page_and_page_size
+        @page = params[:page] || 1
+        @page_size = params[:page_size] || CkbTransaction.default_per_page
+      end
+
+      def sort_transactions(records)
+        sort, order = params.fetch(:sort, "id.desc").split(".", 2)
+        sort =
+          case sort
+          when "time" then "created_at"
+          when "fee" then  "transaction_fee"
+          when "capacity" then "capacity_involved"
+          else "id"
+          end
+
+        if order.nil? || !order.match?(/^(asc|desc)$/i)
+          order = "asc"
         end
 
-      head :not_found and return unless order_by.in? %w[id created_at transaction_fee]
-
-      pending_transactions = pending_transactions.order(order_by => asc_or_desc).
-        page(@page).per(@page_size).fast_page
-
-      render json: {
-        data: pending_transactions.map do |tx|
-          {
-            transaction_hash: tx.tx_hash,
-            capacity_involved: tx.capacity_involved,
-            transaction_fee: tx.transaction_fee,
-            created_at: tx.created_at,
-            create_timestamp: (tx.created_at.to_f * 1000).to_i
-          }
-        end,
-        meta: {
-          total: CkbTransaction.tx_pending.count,
-          page_size: @page_size.to_i
-        }
-      }
-    end
-
-    def count
-      render json: {
-        data: CkbTransaction.tx_pending.count
-      }
-    end
-
-    def set_page_and_page_size
-      @page = params[:page] || 1
-      @page_size = params[:page_size] || 10
+        records.order("#{sort} #{order}")
+      end
     end
   end
 end
