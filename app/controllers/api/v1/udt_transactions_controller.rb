7d0e80a037259cbd9814742077d4da17d7a3fd45
zhangnan <zhangnan8710@gmail.com>
2023-07-06 12:34:18 +0800

    refactor: udt transactions filter

--- app/controllers/api/v1/udt_transactions_controller.rb
+++ app/controllers/api/v1/udt_transactions_controller.rb
@@ -1,35 +1,51 @@
 module Api
   module V1
     class UdtTransactionsController < ApplicationController
-      before_action :validate_query_params
-      before_action :validate_pagination_params, :pagination_params
+      before_action :validate_query_params, :validate_pagination_params, :pagination_params
 
       def show
-        udt = Udt.find_by!(type_hash: params[:id], published: true)
-        ckb_transactions = udt.ckb_transactions.tx_committed
-          .select(:id, :tx_hash, :block_id, :block_number, :block_timestamp, :is_cellbase, :updated_at).recent
+        udt = Udt.find_by(type_hash: params[:id], published: true)
+        raise Api::V1::Exceptions::UdtNotFoundError if udt.blank?
 
-        # TODO minted? burn? transfer?
-        ckb_transactions = ckb_transactions.where(tx_hash: params[:tx_hash]) if params[:tx_hash].present?
-        ckb_transactions = ckb_transactions
-          .page(@page).per(@page_size).fast_page
+        ckb_transactions = udt.ckb_transactions.tx_committed.
+          select(:id, :tx_hash, :block_id, :block_number,
+                 :block_timestamp, :is_cellbase, :updated_at).
+          order("ckb_transactions.block_timestamp desc nulls last, ckb_transactions.id desc")
 
-        json =
-          Rails.cache.realize("#{udt.symbol}/#{ckb_transactions.cache_key}", version: ckb_transactions.cache_version) do
-            records_counter = RecordCounters::UdtTransactions.new(udt)
-            options = FastJsonapi::PaginationMetaGenerator.new(request: request, records: ckb_transactions, page: @page, page_size: @page_size, records_counter: records_counter).call
-            CkbTransactionsSerializer.new(ckb_transactions, options.merge(params: { previews: true })).serialized_json
-          end
+        if params[:tx_hash].present?
+          ckb_transactions = ckb_transactions.where(tx_hash: params[:tx_hash])
+        end
 
-        render json: json
-      rescue ActiveRecord::RecordNotFound
-        raise Api::V1::Exceptions::UdtNotFoundError
+        if params[:address_hash].present?
+          address = Address.find_address!(params[:address_hash])
+          raise Api::V1::Exceptions::AddressNotFoundError if address.is_a?(NullAddress)
+
+          ckb_transactions = ckb_transactions.includes(:contained_udt_addresses).
+            where(address_udt_transactions: { address_id: address.id })
+        end
+
+        if stale?(ckb_transactions)
+          expires_in 10.seconds, public: true, must_revalidate: true, stale_while_revalidate: 5.seconds
+
+          ckb_transactions = ckb_transactions.page(@page).per(@page_size).fast_page
+          options = FastJsonapi::PaginationMetaGenerator.new(
+            request: request,
+            records: ckb_transactions,
+            page: @page,
+            page_size: @page_size
+          ).call
+          json = CkbTransactionsSerializer.new(
+            ckb_transactions, options.merge(params: { previews: true })
+          ).serialized_json
+
+          render json: json
+        end
       end
 
       private
 
       def validate_query_params
-        validator = Validations::Udt.new(params)
+        validator = Validations::UdtTransaction.new(params)
 
         if validator.invalid?
           errors = validator.error_object[:errors]
--- test/factories/udt.rb
+++ test/factories/udt.rb
@@ -12,19 +12,23 @@ FactoryBot.define do
 
     trait :with_transactions do
       after(:create) do |udt, _evaluator|
+        full_udt_address_ids = []
         20.times do
           block = create(:block, :with_block_hash)
           transaction = create(:ckb_transaction, block: block, contained_udt_ids: [udt.id], tags: ["udt"])
           transaction1 = create(:ckb_transaction, block: block, contained_udt_ids: [udt.id], tags: ["udt"])
-          create(:cell_output, block: block,
-                               ckb_transaction: transaction,
-                               consumed_by: transaction1,
-                               status: "dead",
-                               type_hash: udt.type_hash,
-                               cell_type: "udt",
-                               data: "0x000050ad321ea12e0000000000000000")
+          cell_output = create(:cell_output, block: block,
+                                             ckb_transaction: transaction,
+                                             consumed_by: transaction1,
+                                             status: "dead",
+                                             type_hash: udt.type_hash,
+                                             cell_type: "udt",
+                                             data: "0x000050ad321ea12e0000000000000000")
+          full_udt_address_ids << { address_id: cell_output.address.id, ckb_transaction_id: transaction.id }
         end
         udt.update(ckb_transactions_count: 40)
+        AddressUdtTransaction.upsert_all full_udt_address_ids,
+                                         unique_by: [:address_id, :ckb_transaction_id]
       end
     end
   end
