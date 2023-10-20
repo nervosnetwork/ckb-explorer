36a936740b44e8f9f81e1bbcb07f24b9239320cb
Miles Zhang <mingchang555@hotmail.com>
2023-10-20 19:20:22 +0800

    feat: filter contract's referring cells of live status
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/workers/contract_statistic_worker.rb
+++ app/workers/contract_statistic_worker.rb
@@ -7,7 +7,7 @@ class ContractStatisticWorker
       contract.update(
         ckb_transactions_count: contract.cell_dependencies.count,
         deployed_cells_count: contract.deployed_cell_outputs&.live&.size,
-        referring_cells_count: contract.referring_cell_outputs&.size,
+        referring_cells_count: contract.referring_cell_outputs&.live&.size,
         total_deployed_cells_capacity: contract.deployed_cell_outputs&.live&.sum(:capacity),
         total_referring_cells_capacity: contract.referring_cell_outputs&.live&.sum(:capacity)
       )
