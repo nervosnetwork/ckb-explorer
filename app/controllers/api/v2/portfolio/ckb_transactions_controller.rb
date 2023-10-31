52fdec471c85bc7b2a35825161e0f9f528be3db1
zhangnan <zhangnan8710@gmail.com>
2023-10-31 15:01:03 +0800

    chore: refactor filter portfolio account books query

--- app/controllers/api/v2/portfolio/ckb_transactions_controller.rb
+++ app/controllers/api/v2/portfolio/ckb_transactions_controller.rb
@@ -8,13 +8,8 @@ module Api
         def index
           expires_in 15.minutes, public: true, stale_while_revalidate: 5.minutes, stale_if_error: 5.minutes
 
-          tx_ids = AccountBook.joins(:ckb_transaction).where(
-            account_books: { address_id: current_user.address_ids },
-            ckb_transactions: { tx_status: "committed" }
-          )
-          tx_ids = sort_account_books(tx_ids).page(@page).per(@page_size).fast_page
-
-          ckb_transactions = CkbTransaction.where(id: tx_ids.map(&:ckb_transaction_id)).
+          account_books = sort_account_books(filter_account_books).page(@page).per(@page_size).fast_page
+          ckb_transactions = CkbTransaction.where(id: account_books.map(&:ckb_transaction_id)).
             select(:id, :tx_hash, :block_id, :block_number, :block_timestamp,
                    :is_cellbase, :updated_at, :capacity_involved).
             order(id: :desc)
@@ -23,13 +18,13 @@ module Api
             records: ckb_transactions,
             page: @page,
             page_size: @page_size,
-            records_counter: tx_ids
+            records_counter: account_books
           ).call
           ckb_transaction_serializer = CkbTransactionsSerializer.new(
             ckb_transactions,
             options.merge(params: {
               previews: true,
-              address: @address })
+              address: current_user.addresses })
           )
           json = ckb_transaction_serializer.serialized_json
 
@@ -41,6 +36,26 @@ module Api
         def pagination_params
           @page = params[:page] || 1
           @page_size = params[:page_size] || CkbTransaction.default_per_page
+        end
+
+        def filter_account_books
+          address_ids =
+            if params[:address_hash].present?
+              address = Address.find_address!(params[:address_hash])
+              [address.id]
+            else
+              current_user.address_ids
+            end
+          scope = AccountBook.joins(:ckb_transaction).where(
+            account_books: { address_id: address_ids },
+            ckb_transactions: { tx_status: "committed" }
+          )
+
+          if params[:tx_hash].present?
+            scope = scope.where(ckb_transactions: { tx_hash: params[:tx_hash] })
+          end
+
+          scope
         end
 
         def sort_account_books(records)
