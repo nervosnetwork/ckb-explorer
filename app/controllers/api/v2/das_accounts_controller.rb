8ee1a354697481440e58a2094fe717df964a7ef0
Rabbit <zhangnan8710@gmail.com>
2023-11-29 11:30:38 +0800

    fix: filter out invalid addresses in das accounts query (#1518)

--- app/controllers/api/v2/das_accounts_controller.rb
+++ app/controllers/api/v2/das_accounts_controller.rb
@@ -1,20 +1,25 @@
-module Api::V2
-  class DasAccountsController < BaseController
-    def query
-      das = DasIndexerService.instance
+module Api
+  module V2
+    class DasAccountsController < BaseController
+      def query
+        das = DasIndexerService.instance
 
-      cache_keys = params[:addresses]
+        cache_keys = params[:addresses]
 
-      res = Rails.cache.read_multi(*cache_keys)
-      not_cached = cache_keys - res.keys
-      to_cache = {}
-      not_cached.each do |address|
-        name = das.reverse_record(address)
-        res[address] = name
-        to_cache[address] = name
+        res = Rails.cache.read_multi(*cache_keys)
+        not_cached = cache_keys - res.keys
+        to_cache = {}
+        not_cached.each do |address|
+          next unless QueryKeyUtils.valid_address?(address)
+
+          name = das.reverse_record(address)
+          res[address] = name
+          to_cache[address] = name
+        end
+        Rails.cache.write_multi(to_cache, expires_in: 1.hour)
+
+        render json: res
       end
-      Rails.cache.write_multi(to_cache, expires_in: 1.hour)
-      render json: res
     end
   end
 end
--- test/controllers/api/v2/das_accounts_controller_test.rb
+++ test/controllers/api/v2/das_accounts_controller_test.rb
@@ -7,16 +7,20 @@ module Api
         super
         SecureRandom.stubs(:uuid).returns("11111111-1111-1111-1111-111111111111")
       end
+
+      test "should return empty address alias for an invalid address" do
+        post api_v2_das_accounts_url, params: { addresses: ["test"] }
+
+        assert_response :success
+        assert_equal json, {}
+      end
+
       test "should return corresponding address alias" do
         DasIndexerService.any_instance.stubs(:reverse_record).returns("test")
-        
-        post api_v2_das_accounts_url, params: {addresses: ["test"]}
-        
+        post api_v2_das_accounts_url, params: { addresses: ["ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83"] }
+
         assert_response :success
-        data = JSON.parse response.body
-        assert_equal data["test"], "test"
-      # rescue => e
-      #   puts e.backtrace.join("\n")
+        assert_equal json["ckt1qyqrdsefa43s6m882pcj53m4gdnj4k440axqswmu83"], "test"
       end
     end
   end
