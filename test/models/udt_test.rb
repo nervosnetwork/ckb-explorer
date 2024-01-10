e789ecc8591ab7046ab854899924ce96c262bf00
Miles Zhang <mingchang555@hotmail.com>
2024-01-10 10:39:30 +0800

    feat: remove udt name and symbol validation (#1562)
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- test/models/udt_test.rb
+++ test/models/udt_test.rb
@@ -5,8 +5,6 @@ class UdtTest < ActiveSupport::TestCase
     should validate_presence_of(:total_amount)
     should validate_numericality_of(:decimal).allow_nil.is_greater_than_or_equal_to(0).is_less_than_or_equal_to(39)
     should validate_numericality_of(:total_amount).is_greater_than_or_equal_to(0)
-    should validate_length_of(:symbol).allow_nil.is_at_least(1).is_at_most(16)
-    should validate_length_of(:full_name).allow_nil.is_at_least(1).is_at_most(100)
   end
 
   test "should return node type script when the udt is published" do
@@ -14,7 +12,7 @@ class UdtTest < ActiveSupport::TestCase
     node_type_script = {
       args: udt.args,
       code_hash: udt.code_hash,
-      hash_type: udt.hash_type
+      hash_type: udt.hash_type,
     }
 
     assert_equal node_type_script, udt.type_script
@@ -32,19 +30,19 @@ class UdtTest < ActiveSupport::TestCase
     30.times do |number|
       block = create(:block, :with_block_hash)
       if number % 2 == 0
-        tx = create(:ckb_transaction, block: block, tags: ["udt"], contained_udt_ids: [udt.id],
+        tx = create(:ckb_transaction, block:, tags: ["udt"], contained_udt_ids: [udt.id],
                                       contained_address_ids: [address.id])
-        create(:cell_output, block: block, ckb_transaction: tx, cell_type: "udt", type_hash: udt.type_hash,
-                             address: address)
+        create(:cell_output, block:, ckb_transaction: tx, cell_type: "udt", type_hash: udt.type_hash,
+                             address:)
       else
-        tx = create(:ckb_transaction, block: block, tags: ["udt"], contained_udt_ids: [udt.id],
+        tx = create(:ckb_transaction, block:, tags: ["udt"], contained_udt_ids: [udt.id],
                                       contained_address_ids: [address.id])
-        tx1 = create(:ckb_transaction, block: block, tags: ["udt"], contained_udt_ids: [udt.id],
+        tx1 = create(:ckb_transaction, block:, tags: ["udt"], contained_udt_ids: [udt.id],
                                        contained_address_ids: [address.id])
-        create(:cell_output, block: block, ckb_transaction: tx1, cell_type: "udt", type_hash: udt.type_hash,
-                             address: address)
-        create(:cell_output, block: block, ckb_transaction: tx, cell_type: "udt", type_hash: udt.type_hash,
-                             consumed_by_id: tx1, address: address)
+        create(:cell_output, block:, ckb_transaction: tx1, cell_type: "udt", type_hash: udt.type_hash,
+                             address:)
+        create(:cell_output, block:, ckb_transaction: tx, cell_type: "udt", type_hash: udt.type_hash,
+                             consumed_by_id: tx1, address:)
       end
     end
 
