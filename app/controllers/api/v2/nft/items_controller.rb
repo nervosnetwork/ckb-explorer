93db6ca7f48b331bb98cab82c528a685e70b4d4a
Miles Zhang <mingchang555@hotmail.com>
2024-02-27 18:19:09 +0800

    feat: query items by hex token_id (#1653)
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/controllers/api/v2/nft/items_controller.rb
+++ app/controllers/api/v2/nft/items_controller.rb
@@ -9,18 +9,18 @@ module Api
           scope = scope.where(owner_id: @owner.id)
         end
         scope = scope.where(collection_id: @collection.id) if @collection
-        scope = scope.where(collection:{standard: params[:standard]}) if params[:standard]
+        scope = scope.where(collection: { standard: params[:standard] }) if params[:standard]
         scope = scope.where(token_id: params[:token_id]) if params[:token_id]
         scope = scope.order(token_id: :asc)
         pagy, items = pagy(scope)
         items = items.map do |i|
           j = i.as_json
-          j['collection'] = i.collection.as_json
+          j["collection"] = i.collection.as_json
           j
         end
         render json: {
           data: items,
-          pagination: pagy_metadata(pagy)
+          pagination: pagy_metadata(pagy),
         }
       end
 
@@ -29,7 +29,8 @@ module Api
           return head(:not_found)
         end
 
-        item = @collection.items.find_by token_id: params[:id]
+        token_id = parse_hex_token_id(params[:id])
+        item = @collection.items.find_by(token_id:)
         if item
           render json: item.as_json.merge(collection: item.collection.as_json)
         else
@@ -39,14 +40,21 @@ module Api
 
       protected
 
-
       def find_collection
         if params[:collection_id].present?
-          if /\A\d+\z/.match?(params[:collection_id])
-            @collection = TokenCollection.find params[:collection_id]
-          else
-            @collection = TokenCollection.find_by_sn params[:collection_id]
-          end
+          @collection = if /\A\d+\z/.match?(params[:collection_id])
+                          TokenCollection.find params[:collection_id]
+                        else
+                          TokenCollection.find_by_sn params[:collection_id]
+                        end
+        end
+      end
+
+      def parse_hex_token_id(hex_id)
+        if hex_id.start_with?("0x")
+          hex_id.hex
+        else
+          hex_id
         end
       end
     end
