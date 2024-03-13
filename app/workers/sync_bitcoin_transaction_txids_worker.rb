7bad790e5f8ebff3c6d4529b0483ee7817fad323
Rabbit <zhangnan8710@gmail.com>
2024-03-13 10:19:43 +0800

    chore: update bitcoin txids cache key

--- app/workers/sync_bitcoin_transaction_txids_worker.rb
+++ app/workers/sync_bitcoin_transaction_txids_worker.rb
@@ -3,21 +3,19 @@ class SyncBitcoinTransactionTxidsWorker
   sidekiq_options queue: "bitcoin"
 
   def perform
-    txids = Kredis.unique_list "bitcoin_#{tip_block_number}"
+    tip_block_number = StatisticInfo.default.tip_block_number
+    # The synchronization ratio of Bitcoin blocks
+    bitcoin_block_number = tip_block_number / 20
+    txids = Kredis.unique_list "bitcoin_#{bitcoin_block_number}"
     return if txids.elements.present?
 
-    block_hash = rpc.getblockhash(tip_block_number / 20)
+    block_hash = rpc.getblockhash(bitcoin_block_number)
     # verbose set to 1 for JSON object
     block = rpc.getblock(block_hash, 1)
     txids.append(block["tx"].slice(1..-1))
   end
 
   private
-
-  def tip_block_number
-    # The synchronization ratio of Bitcoin blocks
-    @number ||= StatisticInfo.default.tip_block_number
-  end
 
   def rpc
     @rpc ||= Bitcoin::Rpc.instance
