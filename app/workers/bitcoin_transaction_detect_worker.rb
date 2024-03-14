3dfdb12524ed1e71a9cae293f6b2663fe7f25477
Rabbit <zhangnan8710@gmail.com>
2024-03-14 16:32:18 +0800

    refactor: remove sync txids worker (#1678)

--- app/workers/bitcoin_transaction_detect_worker.rb
+++ app/workers/bitcoin_transaction_detect_worker.rb
@@ -2,6 +2,8 @@ class BitcoinTransactionDetectWorker
   include Sidekiq::Worker
   sidekiq_options queue: "bitcoin"
 
+  INIT_BITCOIN_BLOCK_HEIGHT = 100_000
+
   attr_accessor :block
 
   def perform(block_id)
@@ -9,77 +11,50 @@ class BitcoinTransactionDetectWorker
     return unless @block
 
     ApplicationRecord.transaction do
-      # build_vins!
-      build_vouts!
+      vout_attributes = []
+
+      transacitons = @block.ckb_transactions.limit(min_transactions_count)
+      transacitons.each_with_index do |transaction, index|
+        next if transaction.bitcoin_vouts.exists?
+
+        txid = bitcoin_txids[index]
+        raw_transaction = rpc.getrawtransaction(txid, 2)
+        bitcoin_transaction = build_tranaction!(raw_transaction)
+
+        cell_output = transaction.cell_outputs.first
+        vout_attribute = build_vout_attributes!(raw_transaction, bitcoin_transaction, cell_output)
+        next unless vout_attribute
+
+        vout_attributes << vout_attribute
+      end
+
+      return if vout_attributes.blank?
+
+      BitcoinVout.upsert_all(vout_attributes, unique_by: %i[bitcoin_transaction_id index])
     end
   end
 
   private
 
-  def build_vins!
-    vin_attributes = []
-    block.cell_inputs.each do |cell_input|
-      next unless cell_input.output
+  def bitcoin_txids
+    return @txids if @txids
 
-      # prev_txid, vout_index = CkbUtils.parse_rgb_args(cell_input.lock_script.args)
-      prev_txid, vout_index = get_txid(cell_input.output)
-      if prev_txid && vout_index
-        attributes = build_vin_attributes!(prev_txid, vout_index, cell_input)
-        vin_attributes << attributes
-      end
-    end
-
-    return if vin_attributes.blank?
-
-    BitcoinVin.upsert_all(vin_attributes, unique_by: %i[ckb_transaction_id cell_input_id])
+    block_hash = rpc.getblockhash(bitcoin_block_height)
+    # verbose set to 1 for JSON object
+    block = rpc.getblock(block_hash, 1)
+    @txids = block["tx"]
   end
 
-  def build_vin_attributes!(prev_txid, vout_index, cell_input)
-    prev_vout = BitcoinVout.includes(:bitcoin_transaction).find_by(
-      bitcoin_transactions: { txid: prev_txid },
-      bitcoin_vouts: { index: vout_index },
-    )
-    unless prev_vout
-      raise ArgumentError, "Missing previous vout txid: #{prev_txid} index: #{vout_index}"
-    end
-
-    {
-      previous_bitcoin_vout_id: prev_vout.id,
-      ckb_transaction_id: cell_input.ckb_transaction_id,
-      cell_input_id: cell_input.id,
-    }
+  def bitcoin_block_height
+    transaction = BitcoinTransaction.last
+    transaction ? transaction.block_height + 1 : 100_000
   end
 
-  def build_vouts!
-    utxos = []
-    block.cell_outputs.each do |cell_output|
-      txid, index = get_txid(cell_output)
-      # txid, index = CkbUtils.parse_rgb_args(cell_output.lock_script.args)
-      utxos << { txid:, index:, cell_output: } if txid && index
-    end
-
-    txids = utxos.map { _1[:txid] }.uniq
-    vout_attributes = txids.map do |txid|
-      # verbose set to 2 for JSON object
-      raw_tx = rpc.getrawtransaction(txid, 2)
-      tx = build_tx!(raw_tx)
-      build_vout_attributes!(raw_tx, tx)
-    end.flatten
-
-    vout_attributes.each do |v|
-      utxo = utxos.find { _1[:txid] == v[:txid] && _1[:index] == v[:index] }
-      v[:ckb_transaction_id] = utxo&.dig(:cell_output)&.ckb_transaction_id
-      v[:cell_output_id] = utxo&.dig(:cell_output)&.id
-      v[:address_id] = utxo&.dig(:cell_output)&.address_id
-      v.except!(:txid)
-    end
-
-    return if vout_attributes.blank?
-
-    BitcoinVout.upsert_all(vout_attributes, unique_by: %i[bitcoin_transaction_id index])
+  def min_transactions_count
+    [block.ckb_transactions_count, bitcoin_txids.count].min
   end
 
-  def build_tx!(raw_tx)
+  def build_tranaction!(raw_tx)
     tx = BitcoinTransaction.find_by(txid: raw_tx["txid"])
     return tx if tx
 
@@ -94,37 +69,26 @@ class BitcoinTransactionDetectWorker
     )
   end
 
-  def build_vout_attributes!(raw_tx, tx)
-    raw_tx["vout"].map do |v|
-      address_hash = v.dig("scriptPubKey", "address")
-      address = BitcoinAddress.find_or_create_by(address_hash:) if address_hash.present?
+  def build_vout_attributes!(raw_tx, tx, cell_output)
+    vout = raw_tx["vout"].find { _1["n"] == cell_output.cell_index }
+    vout ||= raw_tx["vout"][0]
 
-      data = v.dig("scriptPubKey", "hex")
-      script_pubkey = Bitcoin::Script.parse_from_payload(data.htb)
-      op_return = script_pubkey.op_return?
+    address_hash = vout.dig("scriptPubKey", "address")
+    return unless address_hash
 
-      {
-        txid: tx.txid,
-        bitcoin_transaction_id: tx.id,
-        bitcoin_address_id: address&.id,
-        data:,
-        index: v.dig("n"),
-        asm: v.dig("scriptPubKey", "asm"),
-        op_return:,
-      }
-    end
-  end
+    bitcoin_address = BitcoinAddress.find_or_create_by(address_hash:)
 
-  def get_txid(cell)
-    number = cell.ckb_transaction.block_number
-    txids = Kredis.unique_list "bitcoin_#{number / 20}"
-    txid = txids.elements[0]
-
-    raise ArgumentError, "Get rgb cell txid error" unless txid
-
-    txids.remove(txid)
-
-    [txid, cell.cell_index]
+    {
+      bitcoin_transaction_id: tx.id,
+      bitcoin_address_id: bitcoin_address.id,
+      data: "6a24aa21a9ed5e53af6963d02d7fcf87695798a0715951bd03fb05f524015d88324636141f42",
+      index: vout.dig("scriptPubKey", "n"),
+      asm: "OP_RETURN aa21a9ed5e53af6963d02d7fcf87695798a0715951bd03fb05f524015d88324636141f42",
+      op_return: true,
+      ckb_transaction_id: cell_output.ckb_transaction_id,
+      cell_output_id: cell_output.id,
+      address_id: cell_output.address_id,
+    }
   end
 
   def rpc
