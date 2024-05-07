cb3d708a07bbe42c91f9b54ec1454a3283ed9415
Miles Zhang <mingchang555@hotmail.com>
2024-05-07 14:28:09 +0800

    feat: generate h24_ckb_transactions_count of contract (#1831)
    * feat: generate h24_ckb_transactions_count of contract
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    * fix: stale? needs to pass active record
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    ---------
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/controllers/api/v2/statistics_controller.rb
+++ app/controllers/api/v2/statistics_controller.rb
@@ -1,31 +1,36 @@
 module Api::V2
   class StatisticsController < BaseController
     def transaction_fees
-      expires_in 15.seconds, public: true
       stats_info = StatisticInfo.default
+      if stale?(stats_info)
+        expires_in 15.seconds, public: true
 
-      render json: {
-        transaction_fee_rates: stats_info.transaction_fee_rates,
-        pending_transaction_fee_rates: stats_info.pending_transaction_fee_rates,
-        last_n_days_transaction_fee_rates: stats_info.last_n_days_transaction_fee_rates,
-      }
+        render json: {
+          transaction_fee_rates: stats_info.transaction_fee_rates,
+          pending_transaction_fee_rates: stats_info.pending_transaction_fee_rates,
+          last_n_days_transaction_fee_rates: stats_info.last_n_days_transaction_fee_rates,
+        }
+      end
     end
 
     def contract_resource_distributed
-      expires_in 30.minutes, public: true
+      contracts = Contract.filter_nil_hash_type
+      if stale?(contracts)
+        expires_in 30.minutes, public: true
+        json = contracts.map do |contract|
+          {
+            name: contract.name,
+            code_hash: contract.code_hash,
+            hash_type: contract.hash_type,
+            tx_count: contract.ckb_transactions_count,
+            h24_tx_count: contract.h24_ckb_transactions_count,
+            ckb_amount: (contract.total_referring_cells_capacity / 10**8).truncate(8),
+            address_count: contract.addresses_count,
+          }
+        end
 
-      json = Contract.filter_nil_hash_type.map do |contract|
-        {
-          name: contract.name,
-          code_hash: contract.code_hash,
-          hash_type: contract.hash_type,
-          tx_count: contract.ckb_transactions_count,
-          ckb_amount: (contract.total_referring_cells_capacity / 10**8).truncate(8),
-          address_count: contract.addresses_count,
-        }
+        render json:
       end
-
-      render json:
     end
   end
 end
