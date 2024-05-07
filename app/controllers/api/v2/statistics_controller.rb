c4628d4397e0002c6b7dc5011c75249e5a418ac6
Miles Zhang <mingchang555@hotmail.com>
2024-05-07 14:09:51 +0800

    fix: stale? needs to pass active record
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/controllers/api/v2/statistics_controller.rb
+++ app/controllers/api/v2/statistics_controller.rb
@@ -14,19 +14,20 @@ module Api::V2
     end
 
     def contract_resource_distributed
-      json = Contract.filter_nil_hash_type.map do |contract|
-        {
-          name: contract.name,
-          code_hash: contract.code_hash,
-          hash_type: contract.hash_type,
-          tx_count: contract.ckb_transactions_count,
-          h24_tx_count: contract.h24_ckb_transactions_count,
-          ckb_amount: (contract.total_referring_cells_capacity / 10**8).truncate(8),
-          address_count: contract.addresses_count,
-        }
-      end
-      if stale?(json)
+      contracts = Contract.filter_nil_hash_type
+      if stale?(contracts)
         expires_in 30.minutes, public: true
+        json = contracts.map do |contract|
+          {
+            name: contract.name,
+            code_hash: contract.code_hash,
+            hash_type: contract.hash_type,
+            tx_count: contract.ckb_transactions_count,
+            h24_tx_count: contract.h24_ckb_transactions_count,
+            ckb_amount: (contract.total_referring_cells_capacity / 10**8).truncate(8),
+            address_count: contract.addresses_count,
+          }
+        end
 
         render json:
       end
