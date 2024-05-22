5b1488a8ff62f9b943e556336f09d6f6e1000eb8
Rabbit <zhangnan8710@gmail.com>
2024-05-22 12:21:37 +0800

    Resolve conflicts with master (#1872)
    refactor: export xudt snapshot
    
    Co-authored-by: Miles Zhang <mingchang555@hotmail.com>

--- app/jobs/csv_exportable/export_udt_snapshot_job.rb
+++ app/jobs/csv_exportable/export_udt_snapshot_job.rb
@@ -12,12 +12,26 @@ module CsvExportable
         block_timestamp <= #{@block.timestamp} AND
         (consumed_block_timestamp > #{@block.timestamp} OR consumed_block_timestamp IS NULL)
       SQL
-      snapshot = CellOutput.where(condition).group(:address_id).sum(:udt_amount)
+      cell_outputs = CellOutput.where(condition).group(:address_id).sum(:udt_amount)
+      cell_outputs = cell_outputs.reject { |_, v| v.to_f.zero? }
+
+      data = []
+      cell_outputs.keys.each_slice(1000) do |address_ids|
+        addresses = Address.includes(bitcoin_address_mapping: [:bitcoin_address]).
+          where(id: address_ids).pluck("addresses.id", "addresses.address_hash", "bitcoin_addresses.address_hash")
+
+        addresses.each do |address|
+          data << {
+            address_hash: address[1],
+            bitcoin_address_hash: address[2],
+            udt_amount: cell_outputs[address[0]],
+          }
+        end
+      end
 
       rows = []
-      snapshot = snapshot.reject { |_, v| v.to_f.zero? }
-      snapshot.sort_by { |_k, v| -v }.each do |address_id, udt_amount|
-        row = generate_row(address_id, udt_amount)
+      data.sort_by { |item| -item[:udt_amount] }.each do |item|
+        row = generate_row(item)
         next if row.blank?
 
         rows << row
@@ -27,11 +41,7 @@ module CsvExportable
       generate_csv(header, rows)
     end
 
-    def generate_row(address_id, udt_amount)
-      address = Address.find_by(id: address_id)
-      return unless address
-
-      owner = address.bitcoin_address&.address_hash || address.address_hash
+    def generate_row(item)
       datetime = datetime_utc(@block.timestamp)
 
       if (decimal = @udt.decimal)
@@ -40,9 +50,9 @@ module CsvExportable
           @block.number,
           @block.timestamp,
           datetime,
-          owner,
-          address.address_hash,
-          parse_udt_amount(udt_amount.to_d, decimal),
+          item[:bitcoin_address_hash] || item[:address_hash],
+          item[:address_hash],
+          parse_udt_amount(item[:udt_amount].to_d, decimal),
         ]
       else
         [
@@ -50,9 +60,9 @@ module CsvExportable
           @block.number,
           @block.timestamp,
           datetime,
-          owner,
-          address.address_hash,
-          "#{udt_amount} (raw)",
+          item[:bitcoin_address_hash] || item[:address_hash],
+          item[:address_hash],
+          "#{item[:udt_amount]} (raw)",
         ]
       end
     end
