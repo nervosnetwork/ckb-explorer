8a47d2e123942fb32fd2cdcd0c82c0d6f05f5a91
Rabbit <zhangnan8710@gmail.com>
2024-05-23 11:40:44 +0800

    feat: add bound live cells count

--- app/controllers/api/v2/bitcoin_addresses_controller.rb
+++ app/controllers/api/v2/bitcoin_addresses_controller.rb
@@ -5,13 +5,17 @@ module Api
         expires_in 1.minute, public: true, must_revalidate: true, stale_while_revalidate: 10.seconds
 
         address = Addresses::Explore.run!(key: params[:id])
-        unbound_live_cells_count = BitcoinVout.includes(:ckb_address, :cell_output).where(
-          ckb_address: { id: address.map(&:id) },
-          bitcoin_vouts: { status: "unbound" },
-          cell_outputs: { status: "live" },
-        ).count
+        live_cells_count = ->(status) do
+          BitcoinVout.includes(:ckb_address, :cell_output).where(
+            ckb_address: { id: address.map(&:id) },
+            bitcoin_vouts: { status: },
+            cell_outputs: { status: "live" },
+          ).count
+        end
+        unbound_live_cells_count = live_cells_count.call("unbound")
+        bound_live_cells_count = live_cells_count.call("bound")
 
-        render json: { unbound_live_cells_count: }
+        render json: { unbound_live_cells_count:, bound_live_cells_count: }
       end
     end
   end
