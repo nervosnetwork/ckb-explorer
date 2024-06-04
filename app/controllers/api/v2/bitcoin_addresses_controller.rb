29b361d0e5ceaf9a02fed6e0f8350220d4c26887
Rabbit <zhangnan8710@gmail.com>
2024-06-04 18:13:51 +0800

    feat: query bitcoin address rgb cells

--- app/controllers/api/v2/bitcoin_addresses_controller.rb
+++ app/controllers/api/v2/bitcoin_addresses_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V2
     class BitcoinAddressesController < BaseController
+      before_action :set_pagination_params, only: %i[rgb_cells]
       def show
         expires_in 1.minute, public: true, must_revalidate: true, stale_while_revalidate: 10.seconds
 
@@ -16,6 +17,37 @@ module Api
         bound_live_cells_count = live_cells_count.call("bound")
 
         render json: { unbound_live_cells_count:, bound_live_cells_count: }
+      end
+
+      def rgb_cells
+        expires_in 1.minute, public: true, must_revalidate: true, stale_while_revalidate: 10.seconds
+
+        address = Addresses::Explore.run!(key: params[:id])
+        address_ids = address.map(&:id)
+
+        bitcoin_vouts = BitcoinVout.joins(:cell_output).
+          where(cell_outputs: { status: "live" }, bitcoin_vouts: { address_id: address_ids }).
+          select(:bitcoin_transaction_id, :index).
+          group(:bitcoin_transaction_id, :index).
+          page(1).per(10)
+
+        transaction_ids = bitcoin_vouts.map(&:bitcoin_transaction_id).uniq
+        transactions = BitcoinTransaction.where(id: transaction_ids).index_by(&:id)
+
+        cells = bitcoin_vouts.each_with_object({}) do |vout, hash|
+          tx = transactions[vout.bitcoin_transaction_id]
+          vouts = BitcoinVout.where(bitcoin_transaction_id: vout.bitcoin_transaction_id, index: vout.index).includes(:cell_output)
+          hash[[tx.tx_hash, vout.index]] = vouts.map { |v| CellOutputSerializer.new(v.cell_output).serialized_json }
+        end
+
+        render json: { data: { rgb_cells: cells }, meta: { total: bitcoin_vouts.total_count, page_size: @page_size } }
+      end
+
+      private
+
+      def set_pagination_params
+        @page = params.fetch(:page, 1)
+        @page_size = params.fetch(:page_size, CellOutput.default_per_page)
       end
     end
   end
