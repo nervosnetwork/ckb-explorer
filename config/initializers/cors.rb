72625c97d1bcbeb74f442fdd84e5a31efc034b0c
Miles Zhang <mingchang555@hotmail.com>
2024-06-18 09:15:24 +0800

    fix: typo
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -25,4 +25,3 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*", headers: :any, methods: %i[get post put head options]
   end
 end
-ckb - explorer - api - staging.magickbase.com
