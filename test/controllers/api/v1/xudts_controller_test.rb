64084d14cd9e74c996a7ee6bde1127c13ed12ab3
Miles Zhang <mingchang555@hotmail.com>
2024-07-01 16:49:51 +0800

    feat: adjust tags filter logic (#2017)
    * feat: adjust tags filter logic
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    * test: fix test
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    ---------
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- test/controllers/api/v1/xudts_controller_test.rb
+++ test/controllers/api/v1/xudts_controller_test.rb
@@ -34,7 +34,7 @@ module Api
         create(:xudt_tag, udt:, tags: ["duplicate", "layer-1-asset", "supply-limited"])
         udt2 = create(:udt, :xudt, symbol: "RPGG")
         create(:xudt_tag, udt: udt2, tags: ["duplicate", "layer-2-asset", "supply-limited"])
-        valid_get api_v1_xudts_url, params: { symbol: "CKBB", "tags": "layer-1-asset,supply-limited,NOT EXIST" }
+        valid_get api_v1_xudts_url, params: { symbol: "CKBB", "tags": "duplicate,supply-limited" }
         assert_response :success
         assert_equal "CKBB", json["data"].first["attributes"]["symbol"]
         assert_equal ["duplicate", "layer-1-asset", "supply-limited"], json["data"].first["attributes"]["xudt_tags"]
