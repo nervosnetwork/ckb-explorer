eee247b4bbb613ceede1ec4f42c9e2db5952050a
Rabbit <zhangnan8710@gmail.com>
2024-07-15 09:50:19 +0800

    refactor: export contract transactions

--- app/jobs/csv_exportable/export_contract_transactions_job.rb
+++ app/jobs/csv_exportable/export_contract_transactions_job.rb
@@ -1,33 +1,51 @@
 # frozen_string_literal: true
 
 module CsvExportable
-  class ExportContractTransactionsJob < BaseExporter
+  class ExportContractTransactionsJob2 < BaseExporter
     def perform(args)
-      dao_contract = DaoContract.default_contract
-      ckb_transactions = dao_contract.ckb_transactions.includes(dao_events: [:address]).tx_committed
+      start_date, end_date = extract_dates(args)
+      transaction_rows = fetch_transaction_rows(start_date, end_date)
+      header = [
+        "Txn hash", "Address", "Blockno", "UnixTimestamp", "Method",
+        "Amount", "Token", "TxnFee(CKB)", "date(UTC)"
+      ]
+      generate_csv(header, transaction_rows)
+    end
 
-      if args[:start_date].present?
-        start_date = BigDecimal(args[:start_date])
-        ckb_transactions = ckb_transactions.where("ckb_transactions.block_timestamp >= ?", start_date)
+    private
+
+    def extract_dates(args)
+      start_date = args[:start_date].present? ? BigDecimal(args[:start_date]) : nil
+      end_date = args[:end_date].present? ? BigDecimal(args[:end_date]) : nil
+      start_number = args[:start_number].presence
+      end_number = args[:end_number].presence
+
+      if start_number.present?
+        start_date = Block.find_by(number: start_number)&.timestamp
       end
 
-      if args[:end_date].present?
-        end_date = BigDecimal(args[:end_date])
-        ckb_transactions = ckb_transactions.where("ckb_transactions.block_timestamp <= ?", end_date)
+      if end_number.present?
+        end_date = Block.find_by(number: end_number)&.timestamp
       end
 
-      if args[:start_number].present?
-        ckb_transactions = ckb_transactions.where("ckb_transactions.block_number >= ?", args[:start_number])
-      end
+      [start_date, end_date]
+    end
 
-      if args[:end_number].present?
-        ckb_transactions = ckb_transactions.where("ckb_transactions.block_number <= ?", args[:end_number])
-      end
+    def build_sql_query(start_date, end_date)
+      sql = "".dup
+      sql << "block_timestamp >= #{start_date}" if start_date.present?
+      sql << " AND " if start_date.present? && end_date.present?
+      sql << "block_timestamp <= #{end_date}" if end_date.present?
+      sql
+    end
 
-      ckb_transactions = ckb_transactions.order("ckb_transactions.block_timestamp desc nulls last, ckb_transactions.id desc").limit(5000)
-
+    def fetch_transaction_rows(start_date, end_date)
+      sql = build_sql_query(start_date, end_date)
       rows = []
-      ckb_transactions.find_in_batches(batch_size: 1000) do |transactions|
+
+      dao_contract = DaoContract.default_contract
+      dao_contract.ckb_transactions.includes(dao_events: [:address]).tx_committed.where(sql).
+        order("block_timestamp desc nulls last, id desc").limit(5000).find_in_batches(batch_size: 500) do |transactions|
         transactions.each do |transaction|
           row = generate_row(transaction)
           next if row.blank?
@@ -36,27 +54,18 @@ module CsvExportable
         end
       end
 
-      header = [
-        "Txn hash", "Address", "Blockno", "UnixTimestamp", "Method",
-        "Amount", "Token", "TxnFee(CKB)", "date(UTC)"
-      ]
-
-      generate_csv(header, rows)
+      rows
     end
 
     def generate_row(transaction)
-      dao_events = transaction.dao_events
+      dao_events = transaction.dao_events.where(event_type: ["deposit_to_dao", "withdraw_from_dao", "issue_interest"])
 
       rows = []
       dao_events.each do |dao_event|
         datetime = datetime_utc(transaction.block_timestamp)
         fee = parse_transaction_fee(transaction.transaction_fee)
         amount = CkbUtils.shannon_to_byte(BigDecimal(dao_event.value))
-        method = {
-          "deposit_to_dao" => "Deposit",
-          "withdraw_from_dao" => "Withdraw Request",
-          "issue_interest" => "Withdraw Finalization",
-        }[dao_event.event_type]
+        method = map_event_type(dao_event.event_type)
 
         rows << [
           transaction.tx_hash,
@@ -72,6 +81,14 @@ module CsvExportable
       end
 
       rows
+    end
+
+    def map_event_type(event_type)
+      {
+        "deposit_to_dao" => "Deposit",
+        "withdraw_from_dao" => "Withdraw Request",
+        "issue_interest" => "Withdraw Finalization",
+      }[event_type]
     end
   end
 end
