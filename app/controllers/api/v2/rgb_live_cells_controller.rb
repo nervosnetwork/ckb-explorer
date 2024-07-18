bd3680b9483b7fd669a7fbddf9b905f12630eb90
Rabbit <zhangnan8710@gmail.com>
2024-07-18 18:31:40 +0800

    chore: add counter cache

--- app/controllers/api/v2/rgb_live_cells_controller.rb
+++ app/controllers/api/v2/rgb_live_cells_controller.rb
@@ -4,14 +4,17 @@ module Api
       before_action :set_pagination_params
       def index
         code_hash = params.fetch("code_hash", nil)
-        cell_outputs =
-          if CkbSync::Api.instance.rgbpp_code_hash.include?(code_hash)
-            CellOutput.live.includes(:lock_script).where(lock_scripts: { code_hash: }).page(@page).per(@page_size).fast_page
-          else
-            CellOutput.none
-          end
-        total_count = cell_outputs.present? ? cell_outputs.total_count : 0
-        outpoints = cell_outputs.pluck(:tx_hash, :cell_index)
+
+        if CkbSync::Api.instance.rgbpp_code_hash.include?(code_hash)
+          scope = CellOutput.live.includes(:lock_script).where(lock_scripts: { code_hash: })
+          # The average block time of BTC is 10 minutes longer
+          total_count = Rails.cache.fetch(scope.cache_key, expires_in: 5.minutes) { scope.count }
+          scope = scope.page(@page).per(@page_size, max_per_page: 1000).fast_page
+          outpoints = cell_outputs.pluck(:tx_hash, :cell_index)
+        else
+          total_count = 0
+          outpoints = CellOutput.none
+        end
 
         render json: { data: outpoints, meta: { total: total_count, page_size: @page_size } }
       end
