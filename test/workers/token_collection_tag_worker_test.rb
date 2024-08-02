892201287c44016ceb7b704d1e046fd4cd72cb93
Miles Zhang <mingchang555@hotmail.com>
2024-08-02 10:31:37 +0800

    feat: adjust xudt and nft tag logic (#2084)
    * feat: adjust xudt and nft tag logic
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    * fix: lp token issuer address logic
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    * feat: nft tag renmae rgbpp-compatible to rgb++
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    * fix: typo
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    * test: fix xudt_tag worker test
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    * feat:  nft name out-of-length tag max 60 characters
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>
    
    ---------
    
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- test/workers/token_collection_tag_worker_test.rb
+++ test/workers/token_collection_tag_worker_test.rb
@@ -14,23 +14,20 @@ class TokenCollectionTagWorkerTest < ActiveJob::TestCase
   end
 
   test "add suspicious tag to token_collection" do
-    create(:token_collection, name: "CK BB", cell_id: @cell.id, creator_id: @address.id)
+    create(:token_collection, name: "CK  BB", cell_id: @cell.id, creator_id: @address.id)
     TokenCollectionTagWorker.new.perform
     assert_equal ["suspicious"], TokenCollection.last.tags
   end
 
   test "add out-of-length-range tag to token_collection" do
-    create(:token_collection, name: "C" * 256, cell_id: @cell.id, creator_id: @address.id)
+    create(:token_collection, name: "C" * 66, cell_id: @cell.id, creator_id: @address.id)
     TokenCollectionTagWorker.new.perform
     assert_equal ["out-of-length-range"], TokenCollection.last.tags
   end
 
-  test "add duplicate tag to token_collection" do
-    create(:token_collection, name: "CKBNFT", cell_id: @cell.id, creator_id: @address.id, block_timestamp: 1.hour.ago.to_i, tags: ["rgbpp-compatible", "layer-1-asset"])
-    new_tx = create(:ckb_transaction)
-    new_cell = create(:cell_output, address_id: @address.id, ckb_transaction_id: new_tx.id, tx_hash: new_tx.tx_hash)
-    create(:token_collection, name: "CKBNFT", cell_id: new_cell.id, creator_id: @address.id, block_timestamp: Time.now.to_i)
+  test "add rgb++ tag to token_collection" do
+    create(:token_collection, name: "CKBNFT", cell_id: @cell.id, creator_id: @address.id, block_timestamp: 1.hour.ago.to_i)
     TokenCollectionTagWorker.new.perform
-    assert_equal ["duplicate", "layer-1-asset"], TokenCollection.last.tags
+    assert_equal ["rgb++", "layer-1-asset"], TokenCollection.last.tags
   end
 end
