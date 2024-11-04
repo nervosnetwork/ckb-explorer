9b8ea216d136ac582ed2fdf1afae3b5608288917
Rabbit <zhangnan8710@gmail.com>
2024-11-04 18:07:33 +0800

    chore: add filter to fiber graph channels

--- app/controllers/api/v2/fiber/graph_channels_controller.rb
+++ app/controllers/api/v2/fiber/graph_channels_controller.rb
@@ -5,7 +5,11 @@ module Api
         def index
           @page = params.fetch(:page, 1)
           @page_size = params.fetch(:page_size, FiberPeer.default_per_page)
-          @channels = FiberGraphChannel.all.page(@page).per(@page_size)
+          @channels = FiberGraphChannel.all
+          if params[:status] == "closed"
+            @channels = @channels.where.not(closed_transaction_id: nil)
+          end
+          @channels = @channels.page(@page).per(@page_size)
         end
       end
     end
