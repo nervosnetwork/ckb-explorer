93c11bc6150e87d7107ca26eef3bf88ed9499689
Rabbit <zhangnan8710@gmail.com>
2024-12-16 17:15:44 +0800

    chore: update rgbpp hourly statistic

--- app/workers/generate_udt_hourly_statistic_worker.rb
+++ app/workers/generate_udt_hourly_statistic_worker.rb
@@ -1,23 +1,10 @@
 class GenerateUdtHourlyStatisticWorker
   include Sidekiq::Job
 
-  def perform
-    udt_types = %i[xudt xudt_compatible spore_cell did_cell]
-    created_at_unixtimestamp = to_be_counted_date.beginning_of_day.to_i
+  def perform(datetime = nil)
     ActiveRecord::Base.connection.execute("SET statement_timeout = 0")
-    Udt.where(udt_type: udt_types, published: true).find_each do |udt|
-      puts "Generating statistics for #{udt.id}"
-      UdtHourlyStatistic.upsert(
-        {
-          udt_id: udt.id,
-          amount: calc_amount(udt),
-          ckb_transactions_count: calc_ckb_transactions_count(udt),
-          holders_count: calc_holders_count(udt),
-          created_at_unixtimestamp:,
-        },
-        unique_by: %i[udt_id created_at_unixtimestamp],
-      )
-    end
+    start_time = to_be_counted_date(datetime)
+    generate_statistics(start_time)
     ActiveRecord::Base.connection.execute("RESET statement_timeout")
   rescue StandardError => e
     Rails.logger.error "Error occurred during GenerateUdtHourlyStatistic error: #{e.message}"
@@ -25,12 +12,31 @@ class GenerateUdtHourlyStatisticWorker
 
   private
 
-  def to_be_counted_date
+  def to_be_counted_date(datetime)
     last_record = UdtHourlyStatistic.order(created_at_unixtimestamp: :desc).first
     if last_record
       Time.zone.at(last_record.created_at_unixtimestamp) + 1.day
     else
-      Time.current.yesterday
+      datetime.is_a?(String) ? Time.zone.parse(datetime) : Time.current.yesterday
+    end
+  end
+
+  def generate_statistics(start_time)
+    puts "Generating udt hourly statistics for #{start_time}"
+    statistic_attributes = []
+    udt_types = %i[xudt xudt_compatible spore_cell did_cell]
+    Udt.where(udt_type: udt_types, published: true).find_each do |udt|
+      statistic_attributes << {
+        udt_id: udt.id,
+        amount: calc_amount(udt),
+        ckb_transactions_count: calc_ckb_transactions_count(udt),
+        holders_count: calc_holders_count(udt),
+        created_at_unixtimestamp: start_time.beginning_of_day.to_i,
+      }
+    end
+
+    if statistic_attributes.present?
+      UdtHourlyStatistic.upsert_all(statistic_attributes, unique_by: %i[udt_id created_at_unixtimestamp])
     end
   end
 
@@ -38,9 +44,11 @@ class GenerateUdtHourlyStatisticWorker
     inputs_amount = 0
     outputs_amount = 0
     ckb_transaction_ids = udt.ckb_transactions.map(&:id)
-    ckb_transaction_ids.each_slice(1000) do |ids|
-      inputs_amount += CellOutput.where(consumed_by_id: ids).sum(:udt_amount)
-      outputs_amount += CellOutput.where(ckb_transaction_id: ids).sum(:udt_amount)
+    ckb_transaction_ids.each_slice(5000) do |ids|
+      batch_inputs_amount = CellOutput.select(:udt_amount).where(consumed_by_id: ids).map(&:udt_amount).compact
+      inputs_amount += batch_inputs_amount.sum
+      batch_outputs_amount = CellOutput.select(:udt_amount).where(ckb_transaction_id: ids).map(&:udt_amount).compact
+      outputs_amount += batch_outputs_amount.sum
     end
     [inputs_amount, outputs_amount].max
   end
