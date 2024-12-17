1e120e39eef04f5c961c6ff334ed247b07c43b50
Miles Zhang <mingchang555@hotmail.com>
2024-12-17 18:34:41 +0900

    fix: query default limit set 5000
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/lib/fast_jsonapi/pagination_meta_generator.rb
+++ app/lib/fast_jsonapi/pagination_meta_generator.rb
@@ -3,18 +3,18 @@ module FastJsonapi
     DEFAULT_PAGE = 1
     DEFAULT_PER_PAGE = 20
 
-    def initialize(request:, records:, page:, page_size:, records_counter: nil, total_count: nil)
+    def initialize(request:, records:, page:, page_size:, total_pages: nil, records_counter: nil, total_count: nil)
       @url = request.base_url + request.path + query_string(request.query_parameters)
       @page = page.to_i
       @page_size = limit_page_size(records, page_size.to_i)
       @records = records
       @records_counter = records_counter || records
       @total_count = total_count || @records_counter.total_count.to_i
-      @total_pages = total_pages
-      @hash = { links: {}, meta: { total: @total_count, page_size: @page_size } }
+      @total_pages = total_pages || calculated_total_pages
+      @hash = { links: {}, meta: { total: @total_count, page_size: @page_size, total_pages: @total_pages } }
     end
 
-    def total_pages
+    def calculated_total_pages
       (total_count / @page_size).ceil
     end
 
@@ -29,10 +29,6 @@ module FastJsonapi
 
     def current_page
       records.current_page
-    end
-
-    def last_page?
-      current_page == total_pages
     end
 
     def next_page
