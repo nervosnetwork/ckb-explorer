992b4f35e192425926e0d7bcb89695040a3cd36d
Rabbit <zhangnan8710@gmail.com>
2025-01-07 17:18:41 +0800

    refactor: address live cells (#2392)

--- app/interactions/addresses/live_cells.rb
+++ app/interactions/addresses/live_cells.rb
@@ -14,17 +14,16 @@ module Addresses
       raise AddressNotFoundError if address.is_a?(NullAddress)
 
       order_by, asc_or_desc = live_cells_ordering
-      cell_outputs = CellOutput.includes(:bitcoin_vout).where(
-        cell_outputs: { status: "live", address_id: address.map(&:id) },
-      )
-      cell_outputs = cell_outputs.where(bitcoin_vouts: { status: bound_status }) if bound_status
-      cell_output_ids = cell_outputs.pluck("cell_outputs.id")
-      records = CellOutput.where(id: cell_output_ids).order(order_by => asc_or_desc).
-        page(page).per(page_size).fast_page
-      options = FastJsonapi::PaginationMetaGenerator.new(
-        request:, records:, page:, page_size:,
-      ).call
+      if bound_status
+        bitcoin_vouts = BitcoinVout.where(address_id: address.map(&:id), status: bound_status)
+        records = CellOutput.live.where(id: bitcoin_vouts.map(&:cell_output_id)).order(order_by => asc_or_desc).
+          page(page).per(page_size).fast_page
+      else
+        records = CellOutput.live.where(address_id: address.map(&:id)).order(order_by => asc_or_desc).
+          page(page).per(page_size).fast_page
+      end
 
+      options = FastJsonapi::PaginationMetaGenerator.new(request:, records:, page:, page_size:).call
       CellOutputSerializer.new(records, options).serialized_json
     end
 
