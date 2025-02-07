c3b29ac5a2c11db89f74d3156d177b0aca396978
Rabbit <zhangnan8710@gmail.com>
2025-02-07 12:44:19 +0800

    chore: add cache for market data

--- app/controllers/api/v1/market_data_controller.rb
+++ app/controllers/api/v1/market_data_controller.rb
@@ -4,10 +4,12 @@ module Api
       skip_before_action :check_header_info
 
       def index
+        expires_in 30.minutes, public: true, stale_while_revalidate: 10.minutes, stale_if_error: 10.minutes
         render json: MarketData.new.indicators_json
       end
 
       def show
+        expires_in 30.minutes, public: true, stale_while_revalidate: 10.minutes, stale_if_error: 10.minutes
         render json: MarketData.new(indicator: params[:id]).call
       end
     end
