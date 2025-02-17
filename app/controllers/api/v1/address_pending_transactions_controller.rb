147a9d5dccbafc450f9d06f0324f09bdb9189beb
Miles Zhang <mingchang555@hotmail.com>
2025-02-17 16:39:07 +0800

    debug
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/controllers/api/v1/address_pending_transactions_controller.rb
+++ app/controllers/api/v1/address_pending_transactions_controller.rb
@@ -4,8 +4,6 @@ module Api
       before_action :validate_pagination_params
 
       def show
-        expires_in 10.seconds, public: true, must_revalidate: true, stale_while_revalidate: 10.seconds
-
         json = Addresses::PendingTransactions.run!(
           { request:,
             key: params[:id], sort: params[:sort],
