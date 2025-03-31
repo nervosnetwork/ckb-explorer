85dfa0686eb78da465f5b01b4eab2e2e8948677a
Miles Zhang <mingchang555@hotmail.com>
2025-03-31 13:45:14 +0800

    fix
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/interactions/addresses/ckb_transactions.rb
+++ app/interactions/addresses/ckb_transactions.rb
@@ -37,7 +37,7 @@ module Addresses
     end
 
     def paginate_options(records, address_id)
-      total_count = Address.where(address_id:).sum(:ckb_transactions_count)
+      total_count = Address.where(id: address_id).sum(:ckb_transactions_count)
       FastJsonapi::PaginationMetaGenerator.new(
         request:, records:, page:, page_size:, total_pages: records.total_pages, total_count:,
       ).call
