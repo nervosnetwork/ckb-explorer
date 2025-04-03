f19a3162adf650da4eaec7d18917db83f4978601
Rabbit <zhangnan8710@gmail.com>
2025-04-03 16:43:38 +0800

    chore: update fiber graph channel build attributes

--- app/workers/fiber_graph_detect_worker.rb
+++ app/workers/fiber_graph_detect_worker.rb
@@ -109,7 +109,8 @@ class FiberGraphDetectWorker
       capacity: channel["capacity"].to_i(16),
       chain_hash: channel["chain_hash"],
       open_transaction_id: open_transaction.id,
-      address_id: cell_output.address_id,
+      address_id: cell_output.address,
+      cell_output:,
       udt_id: udt&.id,
       deleted_at: nil,
     }
