d47160c0da2b80d37a49bc5d51200f33d05c00d7
Miles Zhang <mingchang555@hotmail.com>
2025-05-13 21:41:11 +0800

    feat: add ssri contract model
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

---
+++ app/models/ssri_contract.rb
@@ -0,0 +1,21 @@
+class SsriContract < ApplicationRecord
+  belongs_to :contract
+end
+
+# == Schema Information
+#
+# Table name: ssri_contracts
+#
+#  id          :bigint           not null, primary key
+#  contract_id :bigint
+#  methods     :string           default([]), is an Array
+#  is_udt      :boolean
+#  code_hash   :binary
+#  hash_type   :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+# Indexes
+#
+#  index_ssri_contracts_on_contract_id  (contract_id) UNIQUE
+#
