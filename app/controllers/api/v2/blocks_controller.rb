5458d975871e672aadfec6f2d4d0fc972438b569
Miles Zhang <mingchang555@hotmail.com>
2025-05-21 09:53:07 +0800

    feat: get block by epoch number and index
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- app/controllers/api/v2/blocks_controller.rb
+++ app/controllers/api/v2/blocks_controller.rb
@@ -4,14 +4,18 @@ module Api::V2
       result = Block.last_7_days_ckb_node_version
 
       render json: {
-        data: result.map { |k, v|
+        data: result.map do |k, v|
           {
-            version: (k || 'others'),
-            blocks_count: v
+            version: k || "others",
+            blocks_count: v,
           }
-        }
+        end,
       }
+    end
+
+    def by_epoch
+      block = Block.where(epoch: params[:epoch_number]).order("number asc").limit(1).offset(params[:epoch_index].to_i).first
+      render json: BlockSerializer.new(block)
     end
   end
 end
-
