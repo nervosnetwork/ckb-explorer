15ec4b426f5341eb722c48aea06610d4fd9d2cbb
Miles Zhang <mingchang555@hotmail.com>
2025-07-15 13:39:01 +0900

    feat: remove unused db FUNCTION and PROCEDURE
    Signed-off-by: Miles Zhang <mingchang555@hotmail.com>

--- db/migrate/20250715034316_drop_global_statistic.rb
+++ db/migrate/20250715034316_drop_global_statistic.rb
@@ -1,5 +1,15 @@
 class DropGlobalStatistic < ActiveRecord::Migration[7.0]
   def change
     drop_table :global_statistics, if_exists: true
+    sql = <<-SQL
+     DROP TRIGGER IF EXISTS after_delete_update_ckb_transactions_count ON ckb_transactions;
+     DROP TRIGGER IF EXISTS after_insert_update_ckb_transactions_count ON ckb_transactions;
+     DROP TRIGGER IF EXISTS after_update_ckb_transactions_count ON ckb_transactions;
+
+     DROP FUNCTION IF EXISTS public.decrease_ckb_transactions_count();
+     DROP FUNCTION IF EXISTS public.increase_ckb_transactions_count();
+     DROP FUNCTION IF EXISTS public.update_ckb_transactions_count();
+    SQL
+    ActiveRecord::Base.connection.execute(sql)
   end
 end
