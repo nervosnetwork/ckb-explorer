45f16213475baa8ca0ca7d1e5728e7c3d9b60b80
zhangyaning <zhangyaning1985@gmail.com>
2025-08-28 11:06:08 +0800

    check index exist before add_block_number_index_to_ckb_transactions

--- db/migrate/20250827065749_add_block_number_index_to_ckb_transactions.rb
+++ db/migrate/20250827065749_add_block_number_index_to_ckb_transactions.rb
@@ -1,5 +1,10 @@
 class AddBlockNumberIndexToCkbTransactions < ActiveRecord::Migration[7.0]
   def change
-    add_index :ckb_transactions, :block_number
+    table_name = :ckb_transactions
+    column_name = :block_number
+    index_name = "index_#{table_name}_on_#{column_name}".to_sym
+    unless index_exists?(table_name, column_name)
+      add_index table_name, column_name, name: index_name
+    end
   end
 end
